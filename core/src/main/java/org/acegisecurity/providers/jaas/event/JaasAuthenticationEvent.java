1cc46544ed120b0c81fbbb740ef181fbac13c38b
Ben Alex <ben.alex@springsource.com>
2004-07-29 05:04:49 +0000

    Javadoc removal of warning.

--- core/src/main/java/org/acegisecurity/providers/jaas/event/JaasAuthenticationEvent.java
+++ core/src/main/java/org/acegisecurity/providers/jaas/event/JaasAuthenticationEvent.java
@@ -1,15 +1,35 @@
+/* Copyright 2004 Acegi Technology Pty Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.sf.acegisecurity.providers.jaas.event;
 
 import net.sf.acegisecurity.Authentication;
+
 import org.springframework.context.ApplicationEvent;
 
+
 /**
- * Parent class for events fired by the {@link net.sf.acegisecurity.providers.jaas.JaasAuthenticationProvider JaasAuthenticationProvider}.
+ * Parent class for events fired by the {@link
+ * net.sf.acegisecurity.providers.jaas.JaasAuthenticationProvider
+ * JaasAuthenticationProvider}.
  *
  * @author Ray Krueger
  * @version $Id$
  */
 public abstract class JaasAuthenticationEvent extends ApplicationEvent {
+    //~ Constructors ===========================================================
 
     /**
      * The Authentication object is stored as the ApplicationEvent 'source'.
@@ -20,10 +40,12 @@ public abstract class JaasAuthenticationEvent extends ApplicationEvent {
         super(auth);
     }
 
+    //~ Methods ================================================================
+
     /**
      * Pre-casted method that returns the 'source' of the event.
      *
-     * @return
+     * @return the Authentication
      */
     public Authentication getAuthentication() {
         return (Authentication) source;
