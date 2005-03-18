918fc7c15a82ed8331d93da77928812c226be4ea
Luke Taylor <luke.taylor@springsource.com>
2005-03-18 01:00:36 +0000

    License header added.

--- core/src/main/java/org/acegisecurity/ui/x509/X509ProcessingFilter.java
+++ core/src/main/java/org/acegisecurity/ui/x509/X509ProcessingFilter.java
@@ -1,3 +1,18 @@
+/* Copyright 2004, 2005 Acegi Technology Pty Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.sf.acegisecurity.ui.x509;
 
 import net.sf.acegisecurity.ui.AbstractProcessingFilter;
@@ -19,6 +34,7 @@ import java.io.IOException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.util.Assert;
 
 /**
  * Processes the X.509 certificate submitted by a client browser
@@ -40,6 +56,7 @@ import org.springframework.beans.factory.InitializingBean;
  * </p>
  *
  * @author Luke Taylor
+ * @version $Id$
  */
 public class X509ProcessingFilter implements Filter, InitializingBean {
     //~ Static fields/initializers =============================================
@@ -57,8 +74,7 @@ public class X509ProcessingFilter implements Filter, InitializingBean {
     }
 
     public void afterPropertiesSet() throws Exception {
-        if(authenticationManager == null)
-            throw new IllegalArgumentException("An AuthenticationManager must be set");
+        Assert.notNull(authenticationManager, "An AuthenticationManager must be set");
     }
 
     /**
