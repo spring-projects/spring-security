781ed0f380c4b08e5ff978fb1ac50f189a4f9f54
Luke Taylor <luke.taylor@springsource.com>
2005-12-15 03:45:48 +0000

    Switch to local url.

--- core/src/test/java/org/acegisecurity/providers/ldap/AbstractLdapServerTestCase.java
+++ core/src/test/java/org/acegisecurity/providers/ldap/AbstractLdapServerTestCase.java
@@ -8,13 +8,12 @@ import junit.framework.TestCase;
  */
 public abstract class AbstractLdapServerTestCase extends TestCase {
     protected static final String ROOT_DN = "dc=acegisecurity,dc=org";
-    protected static final String PROVIDER_URL = "ldap://monkeymachine:389/"+ROOT_DN;
-    //protected static final String PROVIDER_URL = "ldap://localhost:10389/" + ROOT_DN;
+    //protected static final String PROVIDER_URL = "ldap://monkeymachine:389/"+ROOT_DN;
+    protected static final String PROVIDER_URL = "ldap://localhost:10389/" + ROOT_DN;
     protected static final String MANAGER_USER = "cn=manager," + ROOT_DN;
     protected static final String MANAGER_PASSWORD = "acegisecurity";
 
-
-//    protected static final LdapTestServer server = new LdapTestServer();
+    protected static final LdapTestServer server = new LdapTestServer();
 
     protected AbstractLdapServerTestCase() {
     }
