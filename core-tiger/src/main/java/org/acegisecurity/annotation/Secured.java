1ca02b6922ef47aac444c7ef0d1300217ae99b95
Mark St. Godard <markstg@users.sourceforge.net>
2006-01-30 03:27:18 +0000

    Include @Inherited and @Documented to Secured annotation see SEC-148

--- core-tiger/src/main/java/org/acegisecurity/annotation/Secured.java
+++ core-tiger/src/main/java/org/acegisecurity/annotation/Secured.java
@@ -15,7 +15,9 @@
 
 package org.acegisecurity.annotation;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -42,6 +44,8 @@ import java.lang.annotation.Target;
  */
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
 public @interface Secured {
     //~ Methods ================================================================
 
