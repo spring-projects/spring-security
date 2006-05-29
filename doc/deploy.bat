22cd1fd5ba1e8184f9c7d646cad88ebff2ca65af
Ben Alex <ben.alex@springsource.com>
2006-05-29 11:00:08 +0000

    Only require 768 Mb.

--- doc/deploy.bat
+++ doc/deploy.bat
@@ -1,4 +1,4 @@
-set MAVEN_OPTS=-Xmx1024m -XX:MaxPermSize=512m
+set MAVEN_OPTS=-Xmx768m -XX:MaxPermSize=512m
 call maven -Dmaven.jar.override=on -Dmaven.jar.clover-ant=1.3.3_01 clean
 call maven -Dmaven.jar.override=on -Dmaven.jar.clover-ant=1.3.3_01 multiproject:clean
 call maven -Dmaven.jar.override=on -Dmaven.jar.clover-ant=1.3.3_01 multiproject:artifact
