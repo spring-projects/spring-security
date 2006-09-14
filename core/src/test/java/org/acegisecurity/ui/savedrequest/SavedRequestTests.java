cf91104b69a7838152865684736e56831f484248
Ray Krueger <raykrueger@users.sourceforge.net>
2006-09-14 20:47:17 +0000

    Made parameters case-insensitive

--- core/src/test/java/org/acegisecurity/ui/savedrequest/SavedRequestTests.java
+++ core/src/test/java/org/acegisecurity/ui/savedrequest/SavedRequestTests.java
@@ -6,11 +6,17 @@ import org.springframework.mock.web.MockHttpServletRequest;
 
 public class SavedRequestTests extends TestCase {
 
-    public void testCaseInsensitve() throws Exception {
+    public void testCaseInsensitveHeaders() throws Exception {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.addHeader("USER-aGenT", "Mozilla");
         SavedRequest saved = new SavedRequest(request, new MockPortResolver(8080, 8443));
         assertEquals("Mozilla", saved.getHeaderValues("user-agent").next());
     }
 
+    public void testCaseInsensitveParameters() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addParameter("ThisIsATest", "Hi mom");
+        SavedRequest saved = new SavedRequest(request, new MockPortResolver(8080, 8443));
+        assertEquals("Hi mom", saved.getParameterValues("thisisatest")[0]);
+    }
 }
