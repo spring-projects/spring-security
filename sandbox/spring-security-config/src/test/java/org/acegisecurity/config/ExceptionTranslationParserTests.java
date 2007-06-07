66d7f754c1ac4de93e0d79d676da89695c100ff7
Vishal Puri <vishal.puri@springsource.com>
2007-06-07 02:21:52 +0000

    SEC-271: changed pom versions, added log4j.properties in test classpath, added another junit test in ExceptionTranslationParserTests

--- sandbox/spring-security-config/src/test/java/org/acegisecurity/config/ExceptionTranslationParserTests.java
+++ sandbox/spring-security-config/src/test/java/org/acegisecurity/config/ExceptionTranslationParserTests.java
@@ -1,9 +1,15 @@
 package org.acegisecurity.config;
 
+import java.beans.FeatureDescriptor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 import javax.servlet.Filter;
 
 import junit.framework.TestCase;
 
+import org.acegisecurity.ui.AccessDeniedHandler;
+import org.acegisecurity.ui.AccessDeniedHandlerImpl;
 import org.acegisecurity.ui.ExceptionTranslationFilter;
 import org.acegisecurity.ui.webapp.AuthenticationProcessingFilterEntryPoint;
 import org.springframework.beans.PropertyValue;
@@ -12,10 +18,11 @@ import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.util.ReflectionUtils;
 
 public class ExceptionTranslationParserTests extends TestCase {
 
-	public void testParsingBeanReferences() {
+	public void OFFtestParsingBeanReferences() {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"org/acegisecurity/config/exception-translation-beanref.xml");
 		ConfigurableListableBeanFactory factory = (ConfigurableListableBeanFactory) context
@@ -33,7 +40,7 @@ public class ExceptionTranslationParserTests extends TestCase {
 		assertEquals(entryPoint.getValue(), new RuntimeBeanReference("authenticationProcessingFilterEntryPoint"));
 	}
 
-	public void testRuntimeBeanDependencies() {
+	public void OFFtestRuntimeBeanDependencies() {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"org/acegisecurity/config/exception-translation-beanref.xml");
 		ExceptionTranslationFilter filter = (ExceptionTranslationFilter) context.getBean("exceptionTranslationFilter");
@@ -42,6 +49,31 @@ public class ExceptionTranslationParserTests extends TestCase {
 		assertEquals("/acegilogin.jsp", entryPoint.getLoginFormUrl());
 		assertFalse(entryPoint.getForceHttps());
 
+	}
+
+	public void testAutoDetectionOfDefaultDependencies() throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"org/acegisecurity/config/exception-translation-autodetect-handler.xml");
+		ExceptionTranslationFilter filter = (ExceptionTranslationFilter) context.getBean("exceptionTranslationFilter");
+		Field field = makeAccessibleAndGetFieldByName(filter.getClass().getDeclaredFields(), "accessDeniedHandler");
+		assertTrue(field.get(filter) instanceof AccessDeniedHandler);
+		AccessDeniedHandlerImpl accessDeniedHandler = (AccessDeniedHandlerImpl) field
+				.get(filter);
+		Field f = makeAccessibleAndGetFieldByName(accessDeniedHandler.getClass().getDeclaredFields(), "errorPage");
+		assertEquals("errorPage",f.getName());
+		String value = (String) f.get(accessDeniedHandler);
+		assertEquals("/accessDenied.jsp", value);
+	}
+
+	private Field makeAccessibleAndGetFieldByName(Field[] fields, String name) {
+		Field field = null;
+		for (int i = 0, n = fields.length; i < n; i++) {
+			ReflectionUtils.makeAccessible(fields[i]);
+			if (fields[i].getName().equals(name)) {
+				return fields[i];
+			}
+		}
+		return field;
 	}
 
 }
