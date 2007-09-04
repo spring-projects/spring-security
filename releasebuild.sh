d905160b5ba65d545bfbedee9a3829ad36d644e0
Luke Taylor <luke.taylor@springsource.com>
2007-09-04 01:12:31 +0000

    Extra patching of generated maven site files.

--- releasebuild.sh
+++ releasebuild.sh
@@ -1,8 +1,23 @@
 #! /bin/sh
 
 # This script must be run from the project root directory
+#
+# Release Process.
+#
+# 1. Do clean check out of source from svn.
+# 2. Set the version number in the pom.xml files of all the module
+# 3. Set the correct spring version number in the pom.xml.
+# 3a Set the same version number in this script
+# 4. Commit the source with the changed version numbers and note the revision number.
+# 5. Run this script to generate the artifacts and web site in the 'release' directory.
+#
+#
 
+
+
+#
 # Edit this release number before running. It is used to check jar names etc.
+#
 RELEASE_VERSION=1.0.5-SNAPSHOT
 
 PROJ_DIR=`pwd`;
@@ -15,6 +30,26 @@ SVN_REV=`svn info $PROJ_DIR | grep Revision | sed "s/Revision: //"`
 
 echo "** Building from revision $SVN_REV"
 
+#
+# Check the sandbox builds with the current configuration
+#
+
+pushd sandbox
+
+mvn clean test
+
+if [ "$?" -ne 0 ]
+then
+  echo "Failed to build sandbox with current configuration."
+  exit 1;
+fi
+
+popd
+
+
+#
+# Create the release directory if it doesn't already exist
+#
 
 if [[ -e $RELEASE_DIR ]]
 then
@@ -44,14 +79,19 @@ then
   exit 1;
 fi
 
-# Patch the module site files to point to the correct css file
+# Patch the module site files to point to the root css files, change names of oversized menus,
+# remove dodgy standard maven text etc.
+#
 
 pushd $RELEASE_DIR/site
 
-find . -name "*.html" -maxdepth 2 -mindepth 2 | xargs perl -i -p -e 's#\./css/site\.css#\.\./css/site\.css#'
+find . -name "*.html" -maxdepth 2 -mindepth 2 | xargs perl -i -p -e 's#\./css/#\.\./css/#;' \
+   -e 's/Maven Surefire Report/Unit Tests/;' \
+   -e 's/Cobertura Test Coverage/Test Coverage/;'
+   -e 's/A successful project.*greatly appreciated\.//;' 
+
 
 popd
-
 
 
 # Assemble the required jar files
