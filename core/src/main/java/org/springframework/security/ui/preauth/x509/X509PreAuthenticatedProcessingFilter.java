c7792458b428789ee2f4189db27844c1b52b79af
Luke Taylor <luke.taylor@springsource.com>
2008-01-27 11:12:50 +0000

    SEC-645: Reimplementation of X509 authentication.

---
+++ core/src/main/java/org/springframework/security/ui/preauth/x509/X509PreAuthenticatedProcessingFilter.java
@@ -0,0 +1,51 @@
+package org.springframework.security.ui.preauth.x509;
+
+import org.springframework.security.ui.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.ui.FilterChainOrder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.cert.X509Certificate;
+
+/**
+ * @author Luke Taylor
+ * @version $Id$
+ */
+public class X509PreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter {
+    private X509PrincipalExtractor principalExtractor = new SubjectDnX509PrincipalExtractor();
+
+    protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+        X509Certificate cert = extractClientCertificate(request);
+
+        if (cert == null) {
+            return null;
+        }
+
+        return principalExtractor.extractPrincipal(cert);
+    }
+
+    protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+        return extractClientCertificate(request);
+    }
+
+    private X509Certificate extractClientCertificate(HttpServletRequest request) {
+        X509Certificate[] certs = (X509Certificate[]) request.getAttribute("javax.servlet.request.X509Certificate");
+
+        if (certs != null && certs.length > 0) {
+            return certs[0];
+        }
+
+        if (logger.isDebugEnabled()) {
+            logger.debug("No client certificate found in request.");
+        }
+
+        return null;
+    }
+
+    public void setPrincipalExtractor(X509PrincipalExtractor principalExtractor) {
+        this.principalExtractor = principalExtractor;
+    }
+
+    public int getOrder() {
+        return FilterChainOrder.X509_FILTER;    
+    }
+}
---
+++ core/src/main/java/org/springframework/security/ui/preauth/x509/X509PrincipalExtractor.java
@@ -0,0 +1,17 @@
+package org.springframework.security.ui.preauth.x509;
+
+import java.security.cert.X509Certificate;
+
+/**
+ * Obtains the principal from an X509Certificate for use within the framework.
+ *
+ * @author Luke Taylor
+ * @version $Id$
+ */
+public interface X509PrincipalExtractor {
+
+    /**
+     * Returns the principal (usually a String) for the given certificate.
+     */
+    Object extractPrincipal(X509Certificate cert);
+}
