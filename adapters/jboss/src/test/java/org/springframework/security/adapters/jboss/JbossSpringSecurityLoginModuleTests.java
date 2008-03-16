072b76e516db2c3bb90a564053a6b668c7d255d5
Ben Alex <ben.alex@springsource.com>
2008-03-16 04:49:28 +0000

    SEC-717: Resolve UserDetails.getAuthorities() sort logic issue.

--- adapters/jboss/src/test/java/org/springframework/security/adapters/jboss/JbossSpringSecurityLoginModuleTests.java
+++ adapters/jboss/src/test/java/org/springframework/security/adapters/jboss/JbossSpringSecurityLoginModuleTests.java
@@ -240,8 +240,8 @@ public class JbossSpringSecurityLoginModuleTests extends TestCase {
         PrincipalSpringSecurityUserToken castResult = (PrincipalSpringSecurityUserToken) result;
         assertEquals("rod", castResult.getPrincipal());
         assertEquals("koala", castResult.getCredentials());
-        assertEquals("ROLE_TELLER", castResult.getAuthorities()[0].getAuthority());
-        assertEquals("ROLE_SUPERVISOR", castResult.getAuthorities()[1].getAuthority());
+        assertEquals("ROLE_TELLER", castResult.getAuthorities()[1].getAuthority());
+        assertEquals("ROLE_SUPERVISOR", castResult.getAuthorities()[0].getAuthority());
         assertEquals(ADAPTER_KEY.hashCode(), castResult.getKeyHash());
     }
 
