341455cde43ecea828ac3793dd72c4af85e89f80
Luke Taylor <luke.taylor@springsource.com>
2008-04-28 15:19:25 +0000

    SEC-799: Import cleaning following other changes.

--- core/src/main/java/org/springframework/security/config/LdapConfigUtils.java
+++ core/src/main/java/org/springframework/security/config/LdapConfigUtils.java
@@ -1,22 +1,13 @@
 package org.springframework.security.config;
 
-import org.springframework.security.ldap.SpringSecurityContextSource;
-import org.springframework.security.providers.ldap.LdapAuthenticationProvider;
-import org.springframework.security.providers.ldap.LdapAuthenticator;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
-import org.springframework.beans.factory.config.BeanReference;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
-import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.RootBeanDefinition;
-import org.springframework.beans.BeansException;
 import org.springframework.core.Ordered;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import org.springframework.security.ldap.SpringSecurityContextSource;
 
 /**
  * @author Luke Taylor
