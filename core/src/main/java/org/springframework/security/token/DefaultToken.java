7a2e1e13d371fcc77e992a5ad83bb44a5b6ed467
Ben Alex <ben.alex@springsource.com>
2008-05-02 10:38:56 +0000

    SEC-811: Provide a mechanism to allocate and rebuild cryptographically strong, randomised tokens.

---
+++ core/src/main/java/org/springframework/security/token/DefaultToken.java
@@ -0,0 +1,59 @@
+package org.springframework.security.token;
+
+import java.util.Date;
+
+import org.springframework.util.Assert;
+
+/**
+ * The default implementation of {@link Token}.
+ * 
+ * @author Ben Alex
+ * @since 2.0.1
+ */
+public class DefaultToken implements Token {
+	private String key;
+	private long keyCreationTime;
+	private String extendedInformation;
+	
+	public DefaultToken(String key, long keyCreationTime, String extendedInformation) {
+		Assert.hasText(key, "Key required");
+		Assert.notNull(extendedInformation, "Extended information cannot be null");
+		this.key = key;
+		this.keyCreationTime = keyCreationTime;
+		this.extendedInformation = extendedInformation;
+	}
+
+	public String getKey() {
+		return key;
+	}
+	
+	public long getKeyCreationTime() {
+		return keyCreationTime;
+	}
+
+	public String getExtendedInformation() {
+		return extendedInformation;
+	}
+
+	public boolean equals(Object obj) {
+		if (obj != null && obj instanceof DefaultToken) {
+			DefaultToken rhs = (DefaultToken) obj;
+			return this.key.equals(rhs.key) && this.keyCreationTime == rhs.keyCreationTime && this.extendedInformation.equals(rhs.extendedInformation);
+		}
+		return false;
+	}
+
+	public int hashCode() {
+        int code = 979;
+        code = code * key.hashCode();
+        code = code * new Long(keyCreationTime).hashCode();
+        code = code * extendedInformation.hashCode();
+        return code;
+	}
+
+	public String toString() {
+		return "DefaultToken[key=" + new String(key) + "; creation=" + new Date(keyCreationTime) + "; extended=" + extendedInformation + "]";
+	}
+	
+	
+}
---
+++ core/src/main/java/org/springframework/security/token/TokenService.java
@@ -0,0 +1,46 @@
+package org.springframework.security.token;
+
+
+/**
+ * Provides a mechanism to allocate and rebuild secure, randomised tokens.
+ * 
+ * <p>
+ * Implementations are solely concern with issuing a new {@link Token} on demand. The
+ * issued <code>Token</code> may contain user-specified extended information. The token also
+ * contains a cryptographically strong, byte array-based key. This permits the token to be
+ * used to identify a user session, if desired. The key can subsequently be re-presented
+ * to the <code>TokenService</code> for verification and reconstruction of a <code>Token</code>
+ * equal to the original <code>Token</code>.
+ * </p>
+ * 
+ * <p>
+ * Given the tightly-focused behaviour provided by this interface, it can serve as a building block
+ * for more sophisticated token-based solutions. For example, authentication systems that depend on
+ * stateless session keys. These could, for instance, place the username inside the user-specified
+ * extended information associated with the key). It is important to recognise that we do not intend
+ * for this interface to be expanded to provide such capabilities directly.
+ * </p>
+ * 
+ * @author Ben Alex
+ * @since 2.0.1
+ *
+ */
+public interface TokenService {
+	/**
+	 * Forces the allocation of a new {@link Token}.
+	 * 
+	 * @param the extended information desired in the token (cannot be <code>null</code>, but can be empty)
+	 * @return a new token that has not been issued previously, and is guaranteed to be recognised
+	 * by this implementation's {@link #verifyToken(String)} at any future time.
+	 */
+	Token allocateToken(String extendedInformation);
+	
+	/**
+	 * Permits verification the <{@link Token#getKey()} was issued by this <code>TokenService</code> and
+	 * reconstructs the corresponding <code>Token</code>.
+	 * 
+	 * @param key as obtained from {@link Token#getKey()} and created by this implementation
+	 * @return the token, or <code>null</code> if the token was not issued by this <code>TokenService</code>
+	 */
+	Token verifyToken(String key);
+}
