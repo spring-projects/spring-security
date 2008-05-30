0cfcc0e9f1548e92de798cde33d18cdf0feb0024
Luke Taylor <luke.taylor@springsource.com>
2008-05-30 17:51:23 +0000

    Tidying Javadoc

--- cas/src/main/java/org/springframework/security/ui/cas/ServiceProperties.java
+++ cas/src/main/java/org/springframework/security/ui/cas/ServiceProperties.java
@@ -21,9 +21,10 @@ import org.springframework.util.Assert;
 
 /**
  * Stores properties related to this CAS service.
- * <p>Each web application capable of processing CAS tickets is known as a service.
+ * <p>
+ * Each web application capable of processing CAS tickets is known as a service.
  * This class stores the properties that are relevant to the local CAS service, being the application
- * that is being secured by Spring Security.</p>
+ * that is being secured by Spring Security.
  *
  * @author Ben Alex
  * @version $Id$
@@ -42,7 +43,8 @@ public class ServiceProperties implements InitializingBean {
 
     /**
      * Represents the service the user is authenticating to.
-     * <p>This service is the callback URL belonging to the local Spring Security System for Spring secured application.
+     * <p>
+     * This service is the callback URL belonging to the local Spring Security System for Spring secured application.
      * For example,
      * <pre>
      * https://www.mycompany.com/application/j_spring_cas_security_check
@@ -56,10 +58,12 @@ public class ServiceProperties implements InitializingBean {
 
     /**
      * Indicates whether the <code>renew</code> parameter should be sent to the CAS login URL and CAS
-     * validation URL.<p>If <code>true</code>, it will force CAS to authenticate the user again (even if the
+     * validation URL.
+     * <p>
+     * If <code>true</code>, it will force CAS to authenticate the user again (even if the
      * user has previously authenticated). During ticket validation it will require the ticket was generated as a
      * consequence of an explicit login. High security applications would probably set this to <code>true</code>.
-     * Defaults to <code>false</code>, providing automated single sign on.</p>
+     * Defaults to <code>false</code>, providing automated single sign on.
      *
      * @return whether to send the <code>renew</code> parameter to CAS
      */
