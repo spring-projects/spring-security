d7926f3557df39d4ba8b3ddae3527b04c6c713dd
Luke Taylor <luke.taylor@springsource.com>
2008-07-31 20:30:56 +0000

    SEC-943: Forgot to commit tests.

--- core/src/test/java/org/springframework/security/ldap/DefaultSpringSecurityContextSourceTests.java
+++ core/src/test/java/org/springframework/security/ldap/DefaultSpringSecurityContextSourceTests.java
@@ -12,4 +12,10 @@ public class DefaultSpringSecurityContextSourceTests {
     public void instantiationSucceeds() {
         new DefaultSpringSecurityContextSource("ldap://blah:789/dc=springframework,dc=org");
     }
+
+    @Test
+    public void supportsSpacesInUrl() {
+    	new DefaultSpringSecurityContextSource("ldap://myhost:10389/dc=spring%20framework,dc=org");
+    }
+    
 }
