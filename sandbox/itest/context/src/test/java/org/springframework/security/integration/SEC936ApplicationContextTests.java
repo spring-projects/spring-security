959cdd833522f01e1df9b96b9e80ba02bdd5f1a3
Luke Taylor <luke.taylor@springsource.com>
2008-08-16 01:58:45 +0000

    SEC-936: Tests

---
+++ sandbox/itest/context/src/test/java/org/springframework/security/integration/SEC936ApplicationContextTests.java
@@ -0,0 +1,26 @@
+package org.springframework.security.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.AccessDeniedException;
+import org.springframework.security.concurrent.SessionRegistry;
+import org.springframework.security.context.SecurityContextHolder;
+import org.springframework.security.providers.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations={"/sec-936-app-context.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class SEC936ApplicationContextTests {
+    @Autowired
+    /** SessionRegistry is used as the test service interface (nothing to do with the test) */
+    private SessionRegistry sessionRegistry;
+
+    @Test(expected=AccessDeniedException.class)
+    public void securityInterceptorHandlesCallWithNoTargetObject() {
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken("bob","bobspassword"));
+        sessionRegistry.getAllPrincipals();
+    }
+
+}
