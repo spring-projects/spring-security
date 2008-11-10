fa6f57e3ddbca856b994d73762c3f49b48a796cd
Luke Taylor <luke.taylor@springsource.com>
2008-11-10 04:12:53 +0000

    Fixed TestVoter to match recent API updates

--- sandbox/heavyduty/src/main/java/sample/TestVoter.java
+++ sandbox/heavyduty/src/main/java/sample/TestVoter.java
@@ -1,30 +1,30 @@
 package sample;
 
 import java.lang.annotation.Annotation;
+import java.util.List;
 
 import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.security.Authentication;
 import org.springframework.security.ConfigAttribute;
-import org.springframework.security.ConfigAttributeDefinition;
 import org.springframework.security.vote.AccessDecisionVoter;
 
 public class TestVoter implements AccessDecisionVoter {
 
-	public boolean supports(ConfigAttribute attribute) {
-		return true;
-	}
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
 
-	public boolean supports(Class clazz) {
-		return MethodInvocation.class.isAssignableFrom(clazz);
-	}
+    public boolean supports(Class clazz) {
+        return MethodInvocation.class.isAssignableFrom(clazz);
+    }
 
-	public int vote(Authentication authentication, Object object, ConfigAttributeDefinition config) {
-		MethodInvocation mi = (MethodInvocation) object;
-		
-		Annotation[][] annotations = mi.getMethod().getParameterAnnotations();
-		
+    public int vote(Authentication authentication, Object object, List<ConfigAttribute> config) {
+        MethodInvocation mi = (MethodInvocation) object;
 
-		return ACCESS_GRANTED;
-	}
+        Annotation[][] annotations = mi.getMethod().getParameterAnnotations();
+
+
+        return ACCESS_GRANTED;
+    }
 
 }
