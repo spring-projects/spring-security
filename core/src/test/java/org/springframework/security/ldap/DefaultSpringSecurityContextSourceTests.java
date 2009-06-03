781c99f25789b665a6bc85de0e2c7a13316ca465
Luke Taylor <luke.taylor@springsource.com>
2009-06-03 16:57:33 +0000

    SEC-1145: Updated LDAP code to make sure pooling flag is removed when binding as a specific user (for real this time)

--- core/src/test/java/org/springframework/security/ldap/DefaultSpringSecurityContextSourceTests.java
+++ core/src/test/java/org/springframework/security/ldap/DefaultSpringSecurityContextSourceTests.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertNull;
 import javax.naming.directory.DirContext;
 
 import org.junit.Test;
+import org.springframework.security.BadCredentialsException;
 
 /**
  * @author Luke Taylor
@@ -29,5 +30,17 @@ public class DefaultSpringSecurityContextSourceTests extends AbstractLdapIntegra
         assertNull(ctx.getEnvironment().get("com.sun.jndi.ldap.connect.pool"));
         ctx.close();
     }
+
+    @Test(expected=BadCredentialsException.class)
+    public void poolingIsntUsedForSingleUser2() throws Exception {
+        DirContext ctx = getContextSource().getReadWriteContext("uid=Bob,ou=people,dc=springframework,dc=org", "bobspassword");
+        com.sun.jndi.ldap.LdapPoolManager.showStats(System.out);
+        ctx.close();
+        com.sun.jndi.ldap.LdapPoolManager.showStats(System.out);
+        // Now get it gain, with wrong password
+        ctx = getContextSource().getReadWriteContext("uid=Bob,ou=people,dc=springframework,dc=org", "wrongpassword");
+        com.sun.jndi.ldap.LdapPoolManager.showStats(System.out);
+    }
+
 
 }
