a67448c867bd18960d0e79e2575e58f0697b38e5
Luke Taylor <luke.taylor@springsource.com>
2009-08-08 18:51:15 +0000

    SEC-1216: Remove unused code.

--- config/src/main/java/org/springframework/security/config/method/MethodConfigUtils.java
+++ config/src/main/java/org/springframework/security/config/method/MethodConfigUtils.java
@@ -1,12 +1,10 @@
 package org.springframework.security.config.method;
 
-import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.ManagedList;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.ParserContext;
 import org.springframework.security.access.AccessDecisionVoter;
-import org.springframework.security.access.intercept.AfterInvocationProviderManager;
 import org.springframework.security.access.vote.AffirmativeBased;
 import org.springframework.security.access.vote.AuthenticatedVoter;
 import org.springframework.security.access.vote.RoleVoter;
@@ -39,24 +37,5 @@ abstract class MethodConfigUtils {
         BeanDefinitionBuilder accessMgrBuilder = BeanDefinitionBuilder.rootBeanDefinition(AffirmativeBased.class);
         accessMgrBuilder.addPropertyValue("decisionVoters", defaultVoters);
         return (RootBeanDefinition) accessMgrBuilder.getBeanDefinition();
-    }
-
-    @SuppressWarnings("unchecked")
-    static ManagedList getRegisteredAfterInvocationProviders(ParserContext parserContext) {
-        BeanDefinition manager = registerAfterInvocationProviderManagerIfNecessary(parserContext);
-        return (ManagedList) manager.getPropertyValues().getPropertyValue("providers").getValue();
-    }
-
-    @SuppressWarnings("unchecked")
-    static BeanDefinition registerAfterInvocationProviderManagerIfNecessary(ParserContext parserContext) {
-        if(parserContext.getRegistry().containsBeanDefinition(BeanIds.AFTER_INVOCATION_MANAGER)) {
-            return parserContext.getRegistry().getBeanDefinition(BeanIds.AFTER_INVOCATION_MANAGER);
-        }
-
-        BeanDefinition manager = new RootBeanDefinition(AfterInvocationProviderManager.class);
-        manager.getPropertyValues().addPropertyValue("providers", new ManagedList());
-        parserContext.getRegistry().registerBeanDefinition(BeanIds.AFTER_INVOCATION_MANAGER, manager);
-
-        return manager;
     }
 }
