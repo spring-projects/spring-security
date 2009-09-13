40cf50fc9863f5738901bce87452cdc3b110aeee
Luke Taylor <luke.taylor@springsource.com>
2009-09-13 21:51:54 +0000

    SEC-1148: Javadoc.

--- web/src/main/java/org/springframework/security/web/authentication/SimpleUrlAuthenticationSuccessHandler.java
+++ web/src/main/java/org/springframework/security/web/authentication/SimpleUrlAuthenticationSuccessHandler.java
@@ -8,11 +8,25 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 
+/**
+ * <tt>AuthenticationSuccessHandler</tt> which can be configured with a default URL which users should be
+ * sent to upon successful authentication.
+ * <p>
+ * The logic used is that of the {@link AbstractAuthenticationTargetUrlRequestHandler parent class}.
+ *
+ * @author Luke Taylor
+ * @version $Id$
+ * @since 3.0
+ */
 public class SimpleUrlAuthenticationSuccessHandler extends AbstractAuthenticationTargetUrlRequestHandler implements AuthenticationSuccessHandler {
 
     public SimpleUrlAuthenticationSuccessHandler() {
     }
 
+    /**
+     * Constructor which sets the <tt>defaultTargetUrl</tt> property of the base class.
+     * @param defaultTargetUrl the URL to which the user should be redirected on successful authentication.
+     */
     public SimpleUrlAuthenticationSuccessHandler(String defaultTargetUrl) {
         setDefaultTargetUrl(defaultTargetUrl);
     }
