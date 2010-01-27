b7fc5bc455996dbe59403e6c86e360277d956f4b
Luke Taylor <luke.taylor@springsource.com>
2010-01-27 01:49:05 +0000

    Update schema version to 3.1

--- config/convert_schema.sh
+++ config/convert_schema.sh
@@ -3,9 +3,9 @@
 pushd src/main/resources/org/springframework/security/config/
 
 echo "Converting rnc file to xsd ..."
-java -jar ~/bin/trang.jar spring-security-3.0.rnc spring-security-3.0.xsd
+java -jar ~/bin/trang.jar spring-security-3.1.rnc spring-security-3.1.xsd
 
 echo "Applying XSL transformation to xsd ..."
-xsltproc --output spring-security-3.0.xsd spring-security.xsl spring-security-3.0.xsd
+xsltproc --output spring-security-3.1.xsd spring-security.xsl spring-security-3.1.xsd
 
 popd
