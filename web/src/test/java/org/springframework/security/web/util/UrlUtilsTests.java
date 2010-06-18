ea8d37892cd9ee2e798d8d74431bb3c9c61ab52c
Luke Taylor <luke.taylor@springsource.com>
2010-06-18 03:33:06 +0100

    SEC-1496: Added support for use of any non-standard URL schemes in DefaultRedirectStrategy.

---
+++ web/src/test/java/org/springframework/security/web/util/UrlUtilsTests.java
@@ -0,0 +1,22 @@
+package org.springframework.security.web.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Luke Taylor
+ */
+public class UrlUtilsTests {
+
+    @Test
+    public void absoluteUrlsAreMatchedAsAbsolute() throws Exception {
+        assertTrue(UrlUtils.isAbsoluteUrl("http://something/"));
+        assertTrue(UrlUtils.isAbsoluteUrl("HTTP://something/"));
+        assertTrue(UrlUtils.isAbsoluteUrl("https://something/"));
+        assertTrue(UrlUtils.isAbsoluteUrl("a://something/"));
+        assertTrue(UrlUtils.isAbsoluteUrl("zz+zz.zz-zz://something/"));
+    }
+
+}
