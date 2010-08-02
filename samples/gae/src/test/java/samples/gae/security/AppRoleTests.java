d1279aeda2331e527dcd9e915398bbead3cd4cdd
Luke Taylor <luke.taylor@springsource.com>
2010-08-02 19:51:24 +0100

    Logging adjustments for gae sample.

--- samples/gae/src/test/java/samples/gae/security/AppRoleTests.java
+++ samples/gae/src/test/java/samples/gae/security/AppRoleTests.java
@@ -18,6 +18,11 @@ public class AppRoleTests {
         assertEquals("ADMIN", admin.getAuthority());
     }
 
-
-
+    @Test
+    public void bitsAreCorrect() throws Exception {
+        // If this fails, someone has modified the Enum and the Datastore is probably corrupt!
+        assertEquals(0, ADMIN.getBit());
+        assertEquals(1, NEW_USER.getBit());
+        assertEquals(2, USER.getBit());
+    }
 }
