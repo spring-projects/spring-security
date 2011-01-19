162cb64baacd44f90dda0ddfd96c6892bb3aece9
Luke Taylor <luke.taylor@springsource.com>
2011-01-19 18:19:58 +0000

    SEC-1659: Label crypto utils package as only for internal use.

--- crypto/src/main/java/org/springframework/security/crypto/util/package-info.java
+++ crypto/src/main/java/org/springframework/security/crypto/util/package-info.java
@@ -1,4 +1,5 @@
 /**
- * Shared crypto utilities.
+ * Shared crypto utilities for internal module use.
+ * These are liable to change or be refactored and should not be used by external code.
  */
 package org.springframework.security.crypto.util;
