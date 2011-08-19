d6b7b52a79d213481c60a50b7e140364f7237691
Luke Taylor <luke.taylor@springsource.com>
2011-08-19 14:37:56 -0700

    Update to Spring 3.0.6.

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -22,7 +22,7 @@ public class SpringSecurityCoreVersion {
 
     static final String SPRING_MAJOR_VERSION = "3";
 
-    static final String MIN_SPRING_VERSION = "3.0.5.RELEASE";
+    static final String MIN_SPRING_VERSION = "3.0.6.RELEASE";
 
     static {
         // Check Spring Compatibility
