d04cf5ea689eb1ce1708e8e9e86cd30eeee46823
Rob Winch <rwinch@vmware.com>
2012-12-07 09:27:15 -0600

    Remove unused FilterInvocation.DummyResponse

--- web/src/main/java/org/springframework/security/web/FilterInvocation.java
+++ web/src/main/java/org/springframework/security/web/FilterInvocation.java
@@ -17,19 +17,14 @@
 package org.springframework.security.web;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Collection;
-import java.util.Locale;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import javax.servlet.http.HttpServletResponse;
@@ -207,153 +202,6 @@ class DummyRequest extends HttpServletRequestWrapper {
 
     public void setQueryString(String queryString) {
         this.queryString = queryString;
-    }
-}
-
-class DummyResponse implements HttpServletResponse {
-    public void addCookie(Cookie cookie) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void addDateHeader(String name, long date) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void addHeader(String name, String value) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void addIntHeader(String name, int value) {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean containsHeader(String name) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String encodeRedirectURL(String url) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String encodeRedirectUrl(String url) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String encodeURL(String url) {
-        throw new UnsupportedOperationException();
-    }
-
-    public String encodeUrl(String url) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void sendError(int sc) throws IOException {
-        throw new UnsupportedOperationException();
-
-    }
-
-    public void sendError(int sc, String msg) throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public void sendRedirect(String location) throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setDateHeader(String name, long date) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setHeader(String name, String value) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setIntHeader(String name, int value) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setStatus(int sc) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setStatus(int sc, String sm) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void flushBuffer() throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getBufferSize() {
-        throw new UnsupportedOperationException();
-    }
-
-    public String getCharacterEncoding() {
-        throw new UnsupportedOperationException();
-    }
-
-    public String getContentType() {
-        throw new UnsupportedOperationException();
-    }
-
-    public Locale getLocale() {
-        throw new UnsupportedOperationException();
-    }
-
-    public ServletOutputStream getOutputStream() throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public PrintWriter getWriter() throws IOException {
-        throw new UnsupportedOperationException();
-    }
-
-    public boolean isCommitted() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void reset() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void resetBuffer() {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setBufferSize(int size) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setCharacterEncoding(String charset) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setContentLength(int len) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setContentType(String type) {
-        throw new UnsupportedOperationException();
-    }
-
-    public void setLocale(Locale loc) {
-        throw new UnsupportedOperationException();
-    }
-
-    public int getStatus() {
-        throw new UnsupportedOperationException();
-    }
-
-    public String getHeader(String name) {
-        throw new UnsupportedOperationException();
-    }
-
-    public Collection<String> getHeaders(String name) {
-        throw new UnsupportedOperationException();
-    }
-
-    public Collection<String> getHeaderNames() {
-        throw new UnsupportedOperationException();
     }
 }
 
