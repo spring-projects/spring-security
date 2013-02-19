b40b122988f724146f83b90310c8755ff4390a26
Nick Williams <nicholas@nicholaswilliams.net>
2013-02-19 13:58:01 -0600

    SEC-2002: Added events to notify of session ID change
    Session fixation protection, whether by clean new session or
    migrated session, now publishes an event when a session is
    migrated or its ID is changed. This enables application developers
    to keep track of the session ID of a particular authentication
    from the time the authentication is successful until the time
    of logout. Previously this was not possible since session
    migration changed the session ID and there was no way to
    reliably detect that.
    
    Revised changes per Rob Winch's suggestions.

--- web/src/test/java/org/springframework/security/web/session/DefaultSessionAuthenticationStrategyTests.java
+++ web/src/test/java/org/springframework/security/web/session/DefaultSessionAuthenticationStrategyTests.java
@@ -1,17 +1,22 @@
 package org.springframework.security.web.session;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.session.SessionFixationProtectionMigrationEvent;
+import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-import org.junit.Test;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.session.SessionFixationProtectionStrategy;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  *
@@ -33,11 +38,35 @@ public class DefaultSessionAuthenticationStrategyTests {
     public void newSessionIsCreatedIfSessionAlreadyExists() throws Exception {
         SessionFixationProtectionStrategy strategy = new SessionFixationProtectionStrategy();
         HttpServletRequest request = new MockHttpServletRequest();
-        String sessionId = request.getSession().getId();
+        HttpSession session = request.getSession();
+        session.setAttribute("blah", "blah");
+        session.setAttribute("SPRING_SECURITY_SAVED_REQUEST_KEY", "DefaultSavedRequest");
+        String oldSessionId = session.getId();
 
-        strategy.onAuthentication(mock(Authentication.class), request, new MockHttpServletResponse());
+        // See SEC-2002: Make sure SessionFixationProtectionMigrationEvent is published
+        ApplicationEventPublisher eventPublisher = mock(ApplicationEventPublisher.class);
+        strategy.setApplicationEventPublisher(eventPublisher);
 
-        assertFalse(sessionId.equals(request.getSession().getId()));
+        Authentication mockAuthentication = mock(Authentication.class);
+
+        strategy.onAuthentication(mockAuthentication, request, new MockHttpServletResponse());
+
+        ArgumentCaptor<ApplicationEvent> eventArgumentCaptor = ArgumentCaptor.forClass(ApplicationEvent.class);
+        verify(eventPublisher).publishEvent(eventArgumentCaptor.capture());
+
+        assertFalse(oldSessionId.equals(request.getSession().getId()));
+        assertNotNull(request.getSession().getAttribute("blah"));
+        assertNotNull(request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST_KEY"));
+
+        // See SEC-2002: Make sure SessionFixationProtectionMigrationEvent is published
+        assertNotNull(eventArgumentCaptor.getValue());
+        assertTrue(eventArgumentCaptor.getValue() instanceof SessionFixationProtectionMigrationEvent);
+        SessionFixationProtectionMigrationEvent event =
+                (SessionFixationProtectionMigrationEvent)eventArgumentCaptor.getValue();
+        assertEquals(oldSessionId, event.getOldSessionId());
+        assertEquals(request.getSession().getId(), event.getNewSessionId());
+        assertSame(mockAuthentication, event.getAuthentication());
+        assertTrue(event.sessionAttributesWereMigrated());
     }
 
     // See SEC-1077
@@ -49,11 +78,31 @@ public class DefaultSessionAuthenticationStrategyTests {
         HttpSession session = request.getSession();
         session.setAttribute("blah", "blah");
         session.setAttribute("SPRING_SECURITY_SAVED_REQUEST_KEY", "DefaultSavedRequest");
+        String oldSessionId = session.getId();
 
-        strategy.onAuthentication(mock(Authentication.class), request, new MockHttpServletResponse());
+        // See SEC-2002: Make sure SessionFixationProtectionMigrationEvent is published
+        ApplicationEventPublisher eventPublisher = mock(ApplicationEventPublisher.class);
+        strategy.setApplicationEventPublisher(eventPublisher);
+
+        Authentication mockAuthentication = mock(Authentication.class);
+
+        strategy.onAuthentication(mockAuthentication, request, new MockHttpServletResponse());
+
+        ArgumentCaptor<ApplicationEvent> eventArgumentCaptor = ArgumentCaptor.forClass(ApplicationEvent.class);
+        verify(eventPublisher).publishEvent(eventArgumentCaptor.capture());
 
         assertNull(request.getSession().getAttribute("blah"));
         assertNotNull(request.getSession().getAttribute("SPRING_SECURITY_SAVED_REQUEST_KEY"));
+
+        // See SEC-2002: Make sure SessionFixationProtectionMigrationEvent is published
+        assertNotNull(eventArgumentCaptor.getValue());
+        assertTrue(eventArgumentCaptor.getValue() instanceof SessionFixationProtectionMigrationEvent);
+        SessionFixationProtectionMigrationEvent event =
+                (SessionFixationProtectionMigrationEvent)eventArgumentCaptor.getValue();
+        assertEquals(oldSessionId, event.getOldSessionId());
+        assertEquals(request.getSession().getId(), event.getNewSessionId());
+        assertSame(mockAuthentication, event.getAuthentication());
+        assertFalse(event.sessionAttributesWereMigrated());
     }
 
     @Test
@@ -65,4 +114,83 @@ public class DefaultSessionAuthenticationStrategyTests {
         assertNotNull(request.getSession(false));
     }
 
+    // See SEC-2002
+    @Test
+    public void onSessionChangePublishesMigrationEventIfMigrateAttributesIsTrue() throws Exception {
+        SessionFixationProtectionStrategyWithPublicOnSessionChange strategy =
+                new SessionFixationProtectionStrategyWithPublicOnSessionChange();
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpSession session = request.getSession();
+
+        ApplicationEventPublisher eventPublisher = mock(ApplicationEventPublisher.class);
+        strategy.setApplicationEventPublisher(eventPublisher);
+
+        Authentication mockAuthentication = mock(Authentication.class);
+
+        strategy.onSessionChange("oldId01", session, mockAuthentication);
+
+        ArgumentCaptor<ApplicationEvent> eventArgumentCaptor = ArgumentCaptor.forClass(ApplicationEvent.class);
+        verify(eventPublisher).publishEvent(eventArgumentCaptor.capture());
+
+        assertNotNull(eventArgumentCaptor.getValue());
+        assertTrue(eventArgumentCaptor.getValue() instanceof SessionFixationProtectionMigrationEvent);
+        SessionFixationProtectionMigrationEvent event =
+                (SessionFixationProtectionMigrationEvent)eventArgumentCaptor.getValue();
+        assertEquals("oldId01", event.getOldSessionId());
+        assertEquals(request.getSession().getId(), event.getNewSessionId());
+        assertSame(mockAuthentication, event.getAuthentication());
+        assertTrue(event.sessionAttributesWereMigrated());
+    }
+
+    // See SEC-2002
+    @Test
+    public void onSessionChangePublishesIdChangeEventIfMigrateAttributesIsFalse() throws Exception {
+        SessionFixationProtectionStrategyWithPublicOnSessionChange strategy =
+                new SessionFixationProtectionStrategyWithPublicOnSessionChange();
+        strategy.setMigrateSessionAttributes(false);
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpSession session = request.getSession();
+
+        ApplicationEventPublisher eventPublisher = mock(ApplicationEventPublisher.class);
+        strategy.setApplicationEventPublisher(eventPublisher);
+
+        Authentication mockAuthentication = mock(Authentication.class);
+
+        strategy.onSessionChange("oldId02", session, mockAuthentication);
+
+        ArgumentCaptor<ApplicationEvent> eventArgumentCaptor = ArgumentCaptor.forClass(ApplicationEvent.class);
+        verify(eventPublisher).publishEvent(eventArgumentCaptor.capture());
+
+        assertNotNull(eventArgumentCaptor.getValue());
+        assertTrue(eventArgumentCaptor.getValue() instanceof SessionFixationProtectionMigrationEvent);
+        SessionFixationProtectionMigrationEvent event =
+                (SessionFixationProtectionMigrationEvent)eventArgumentCaptor.getValue();
+        assertEquals("oldId02", event.getOldSessionId());
+        assertEquals(request.getSession().getId(), event.getNewSessionId());
+        assertSame(mockAuthentication, event.getAuthentication());
+        assertFalse(event.sessionAttributesWereMigrated());
+    }
+
+    // See SEC-2002
+    @Test
+    public void onSessionChangeDoesNotThrowNullPointerExceptionIfEventPublisherNotSet() throws Exception {
+        SessionFixationProtectionStrategyWithPublicOnSessionChange strategy =
+                new SessionFixationProtectionStrategyWithPublicOnSessionChange();
+        strategy.setMigrateSessionAttributes(false);
+        HttpServletRequest request = new MockHttpServletRequest();
+        HttpSession session = request.getSession();
+
+        Authentication mockAuthentication = mock(Authentication.class);
+
+        strategy.onSessionChange("oldId03", session, mockAuthentication);
+    }
+
+    // See SEC-2002
+    private static class SessionFixationProtectionStrategyWithPublicOnSessionChange
+            extends SessionFixationProtectionStrategy {
+        @Override
+        public void onSessionChange(String originalSessionId, HttpSession newSession, Authentication auth) {
+            super.onSessionChange(originalSessionId, newSession , auth);
+        }
+    }
 }
