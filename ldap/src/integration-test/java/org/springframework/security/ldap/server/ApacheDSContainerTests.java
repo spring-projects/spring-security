407b08956b2c2bb2841097ffb1c89cf04c2448db
Rob Winch <rwinch@vmware.com>
2013-04-25 11:14:12 -0500

    SEC-2161: <ldap-server> creates unique dir for embedded LDAP

--- ldap/src/integration-test/java/org/springframework/security/ldap/server/ApacheDSContainerTests.java
+++ ldap/src/integration-test/java/org/springframework/security/ldap/server/ApacheDSContainerTests.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.security.ldap.server;
 
 import static junit.framework.Assert.fail;
@@ -9,6 +24,7 @@ import org.junit.Test;
  * Useful for debugging the container by itself.
  *
  * @author Luke Taylor
+ * @author Rob Winch
  * @since 3.0
  */
 public class ApacheDSContainerTests {
@@ -37,6 +53,24 @@ public class ApacheDSContainerTests {
                 server2.afterPropertiesSet();
                 fail("Expected Exception");
             } catch(RuntimeException success) {}
+        } finally {
+            try {
+                server1.destroy();
+            }catch(Throwable t) {}
+            try {
+                server2.destroy();
+            }catch(Throwable t) {}
+        }
+    }
+
+    // SEC-2161
+    @Test
+    public void multipleInstancesSimultanciously() throws Exception {
+        ApacheDSContainer server1 = new ApacheDSContainer("dc=springframework,dc=org", "classpath:test-server.ldif");
+        ApacheDSContainer server2 = new ApacheDSContainer("dc=springframework,dc=org", "classpath:test-server.ldif");
+        try {
+            server1.afterPropertiesSet();
+            server2.afterPropertiesSet();
         } finally {
             try {
                 server1.destroy();
