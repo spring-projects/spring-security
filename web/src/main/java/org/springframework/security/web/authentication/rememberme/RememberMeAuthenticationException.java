537d8f974fe02c0103abe9ede83287d2a80d8520
Julien Dubois <jdubois@ippon.fr>
2013-11-04 23:19:02 +0100

    SEC-2519: RememberMeAuthenticationException supports root cause
    Added a constructor which keeps the root cause of the exception, and
    added some documentation

--- web/src/main/java/org/springframework/security/web/authentication/rememberme/RememberMeAuthenticationException.java
+++ web/src/main/java/org/springframework/security/web/authentication/rememberme/RememberMeAuthenticationException.java
@@ -3,11 +3,31 @@ package org.springframework.security.web.authentication.rememberme;
 import org.springframework.security.core.AuthenticationException;
 
 /**
+ * This exception is thrown when an {@link org.springframework.security.core.Authentication} exception occurs while
+ * using the remember-me authentication.
+ *
  * @author Luke Taylor
  */
 public class RememberMeAuthenticationException extends AuthenticationException {
 
-    public RememberMeAuthenticationException(String message) {
-        super(message);
+    //~ Constructors ===================================================================================================
+
+    /**
+     * Constructs a {@code RememberMeAuthenticationException} with the specified message and root cause.
+     *
+     * @param msg the detail message
+     * @param t the root cause
+     */
+    public RememberMeAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    /**
+     * Constructs an {@code RememberMeAuthenticationException} with the specified message and no root cause.
+     *
+     * @param msg the detail message
+     */
+    public RememberMeAuthenticationException(String msg) {
+        super(msg);
     }
 }
