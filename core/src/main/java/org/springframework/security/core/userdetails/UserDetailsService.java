50637d445121e58effb33828996e9a5791bc6470
Alexander Kjäll <alexander.kjall@gmail.com>
2013-11-19 08:58:43 +0100

    SEC-2518: UserDetailsService javadoc repeats "insensitive"
    Typo in javadoc, "case insensitive" was repeated twice.

--- core/src/main/java/org/springframework/security/core/userdetails/UserDetailsService.java
+++ core/src/main/java/org/springframework/security/core/userdetails/UserDetailsService.java
@@ -35,7 +35,7 @@ public interface UserDetailsService {
 
     /**
      * Locates the user based on the username. In the actual implementation, the search may possibly be case
-     * insensitive, or case insensitive depending on how the implementation instance is configured. In this case, the
+     * sensitive, or case insensitive depending on how the implementation instance is configured. In this case, the
      * <code>UserDetails</code> object that comes back may have a username that is of a different case than what was
      * actually requested..
      *
