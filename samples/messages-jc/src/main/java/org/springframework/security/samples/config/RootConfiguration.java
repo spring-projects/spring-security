feeb380b51c40e81859d22e70d1f92060060a5f2
Rob Winch <rwinch@vmware.com>
2013-12-06 11:12:07 -0600

    Polish Guides

--- samples/messages-jc/src/main/java/org/springframework/security/samples/config/RootConfiguration.java
+++ samples/messages-jc/src/main/java/org/springframework/security/samples/config/RootConfiguration.java
@@ -1,12 +1,10 @@
 package org.springframework.security.samples.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 @Configuration
-@ComponentScan(value = "org.springframework.security.samples.config", excludeFilters = @Filter(type = FilterType.ASSIGNABLE_TYPE, value = RootConfiguration.class))
+@ComponentScan
 public class RootConfiguration {
 
 }
