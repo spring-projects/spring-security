20b4007160d8bf2cc52a7707573ffbb16ca45928
Alexander Müller <A.Mueller@db-central.com>
2013-12-06 14:06:41 +0100

    Replaced 8 spaces by 4 spaces.

--- ldap/src/main/java/org/springframework/security/ldap/authentication/PasswordComparisonAuthenticator.java
+++ ldap/src/main/java/org/springframework/security/ldap/authentication/PasswordComparisonAuthenticator.java
@@ -47,105 +47,105 @@ import java.util.Arrays;
  * @author Luke Taylor
  */
 public final class PasswordComparisonAuthenticator extends AbstractLdapAuthenticator {
-        //~ Static fields/initializers =====================================================================================
+    //~ Static fields/initializers =====================================================================================
 
-        private static final Log logger = LogFactory.getLog(PasswordComparisonAuthenticator.class);
+    private static final Log logger = LogFactory.getLog(PasswordComparisonAuthenticator.class);
 
-        private static final int SHA_HASH_LENGTH = 20;
+    private static final int SHA_HASH_LENGTH = 20;
 
-        //~ Instance fields ================================================================================================
+    //~ Instance fields ================================================================================================
 
-        private PasswordEncoder passwordEncoder = new LdapShaPasswordEncoder();
-        private String passwordAttributeName = "userPassword";
+    private PasswordEncoder passwordEncoder = new LdapShaPasswordEncoder();
+    private String passwordAttributeName = "userPassword";
 
-        //~ Constructors ===================================================================================================
+    //~ Constructors ===================================================================================================
 
-        public PasswordComparisonAuthenticator(BaseLdapPathContextSource contextSource) {
-                super(contextSource);
+    public PasswordComparisonAuthenticator(BaseLdapPathContextSource contextSource) {
+        super(contextSource);
+    }
+
+    //~ Methods ========================================================================================================
+
+    public DirContextOperations authenticate(final Authentication authentication) {
+        Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
+            "Can only process UsernamePasswordAuthenticationToken objects");
+        // locate the user and check the password
+
+        DirContextOperations user = null;
+        String username = authentication.getName();
+        String password = (String) authentication.getCredentials();
+
+        SpringSecurityLdapTemplate ldapTemplate = new SpringSecurityLdapTemplate(getContextSource());
+
+        for (String userDn : getUserDns(username)) {
+            try {
+                user = ldapTemplate.retrieveEntry(userDn, getUserAttributes());
+            } catch (NameNotFoundException ignore) {
+            }
+            if (user != null) {
+                break;
+            }
         }
 
-        //~ Methods ========================================================================================================
-
-        public DirContextOperations authenticate(final Authentication authentication) {
-                Assert.isInstanceOf(UsernamePasswordAuthenticationToken.class, authentication,
-                        "Can only process UsernamePasswordAuthenticationToken objects");
-                // locate the user and check the password
-
-                DirContextOperations user = null;
-                String username = authentication.getName();
-                String password = (String) authentication.getCredentials();
-
-                SpringSecurityLdapTemplate ldapTemplate = new SpringSecurityLdapTemplate(getContextSource());
-
-                for (String userDn : getUserDns(username)) {
-                        try {
-                                user = ldapTemplate.retrieveEntry(userDn, getUserAttributes());
-                        } catch (NameNotFoundException ignore) {
-                        }
-                        if (user != null) {
-                                break;
-                        }
-                }
-
-                if (user == null && getUserSearch() != null) {
-                        user = getUserSearch().searchForUser(username);
-                }
-
-                if (user == null) {
-                        throw new UsernameNotFoundException("User not found: " + username, username);
-                }
-
-                if (logger.isDebugEnabled()) {
-                        logger.debug("Performing LDAP compare of password attribute '" + passwordAttributeName + "' for user '" +
-                                user.getDn() + "'");
-                }
-
-                boolean useSshaSalt = passwordEncoder instanceof LdapShaPasswordEncoder;
-                String encodedPassword = passwordEncoder.encodePassword(password, useSshaSalt ? extractSalt(user) : null);
-                byte[] passwordBytes = Utf8.encode(encodedPassword);
-
-                if (!ldapTemplate.compare(user.getDn().toString(), passwordAttributeName, passwordBytes)) {
-                        throw new BadCredentialsException(messages.getMessage("PasswordComparisonAuthenticator.badCredentials",
-                                "Bad credentials"));
-                }
-
-                return user;
+        if (user == null && getUserSearch() != null) {
+            user = getUserSearch().searchForUser(username);
         }
 
-        private byte[] extractSalt(DirContextOperations user) {
-                String userPassword = new String((byte[]) user.getObjectAttribute(passwordAttributeName));
-                Assert.hasLength(userPassword, String.format("The %s attribute of the user is not set or empty.", passwordAttributeName));
-                return extractSalt(userPassword);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found: " + username, username);
         }
 
-        private byte[] extractSalt(String encodedPassword) {
-                // Strip off {hash} prefix. What's left is a Base64 encoded password with its salt.
-                String base64EncodedPasswordWithSalt = encodedPassword.substring(encodedPassword.lastIndexOf("}") + 1);
-
-                // Convert the Base64 encoded password with its salt to a byte array.
-                byte[] passwordWithSalt = Base64.decode(base64EncodedPasswordWithSalt.getBytes());
-
-                if (passwordWithSalt.length == SHA_HASH_LENGTH) {
-                        // Uh, there is no salt! This seems to be a SHA-1 encoded password.
-                        return null;
-                } else if (passwordWithSalt.length < SHA_HASH_LENGTH) {
-                        throw new IllegalArgumentException(
-                                "The supplied password SHA/SSHA hash is invalid. Its length is less than 20 bytes.");
-                }
-
-                // The SHA password has a length of 20 bytes. Everything beyond is the salt.
-                byte[] salt = Arrays.copyOfRange(passwordWithSalt, SHA_HASH_LENGTH, passwordWithSalt.length);
-
-                return salt;
+        if (logger.isDebugEnabled()) {
+            logger.debug("Performing LDAP compare of password attribute '" + passwordAttributeName + "' for user '" +
+                user.getDn() + "'");
         }
 
-        public void setPasswordAttributeName(String passwordAttribute) {
-                Assert.hasLength(passwordAttribute, "passwordAttributeName must not be empty or null");
-                this.passwordAttributeName = passwordAttribute;
+        boolean useSshaSalt = passwordEncoder instanceof LdapShaPasswordEncoder;
+        String encodedPassword = passwordEncoder.encodePassword(password, useSshaSalt ? extractSalt(user) : null);
+        byte[] passwordBytes = Utf8.encode(encodedPassword);
+
+        if (!ldapTemplate.compare(user.getDn().toString(), passwordAttributeName, passwordBytes)) {
+            throw new BadCredentialsException(messages.getMessage("PasswordComparisonAuthenticator.badCredentials",
+                "Bad credentials"));
         }
 
-        public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
-                Assert.notNull(passwordEncoder, "passwordEncoder must not be null.");
-                this.passwordEncoder = passwordEncoder;
+        return user;
+    }
+
+    private byte[] extractSalt(DirContextOperations user) {
+        String userPassword = new String((byte[]) user.getObjectAttribute(passwordAttributeName));
+        Assert.hasLength(userPassword, String.format("The %s attribute of the user is not set or empty.", passwordAttributeName));
+        return extractSalt(userPassword);
+    }
+
+    private byte[] extractSalt(String encodedPassword) {
+        // Strip off {hash} prefix. What's left is a Base64 encoded password with its salt.
+        String base64EncodedPasswordWithSalt = encodedPassword.substring(encodedPassword.lastIndexOf("}") + 1);
+
+        // Convert the Base64 encoded password with its salt to a byte array.
+        byte[] passwordWithSalt = Base64.decode(base64EncodedPasswordWithSalt.getBytes());
+
+        if (passwordWithSalt.length == SHA_HASH_LENGTH) {
+            // Uh, there is no salt! This seems to be a SHA-1 encoded password.
+            return null;
+        } else if (passwordWithSalt.length < SHA_HASH_LENGTH) {
+            throw new IllegalArgumentException(
+                "The supplied password SHA/SSHA hash is invalid. Its length is less than 20 bytes.");
         }
+
+        // The SHA password has a length of 20 bytes. Everything beyond is the salt.
+        byte[] salt = Arrays.copyOfRange(passwordWithSalt, SHA_HASH_LENGTH, passwordWithSalt.length);
+
+        return salt;
+    }
+
+    public void setPasswordAttributeName(String passwordAttribute) {
+        Assert.hasLength(passwordAttribute, "passwordAttributeName must not be empty or null");
+        this.passwordAttributeName = passwordAttribute;
+    }
+
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder) {
+        Assert.notNull(passwordEncoder, "passwordEncoder must not be null.");
+        this.passwordEncoder = passwordEncoder;
+    }
 }
