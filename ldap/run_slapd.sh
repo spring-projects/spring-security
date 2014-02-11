058b9debef4a25adf3e74b8710cc4b1623a14d21
Luke Taylor <luke.taylor@springsource.com>
2014-02-11 14:23:54 +0000

    Minor slapd config changes

--- ldap/run_slapd.sh
+++ ldap/run_slapd.sh
@@ -2,6 +2,6 @@
 
 rm -Rf build/openldap
 mkdir -p build/openldap
-/opt/local/libexec/slapd -h ldap://localhost:22389 -d -1 -f slapd.conf &
-sleep 2
+/usr/libexec/slapd -h ldap://localhost:22389 -d -1 -f slapd.conf &
+sleep 3
 ldapadd -h localhost -p 22389 -D cn=admin,dc=springsource,dc=com -w password -x -f openldaptest.ldif
