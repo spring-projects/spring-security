1bff58577c7eb279f36e54c7e4d74aea24eee1f5
Rob Winch <rwinch@gopivotal.com>
2014-06-12 10:54:23 -0500

    SEC-2631: Add inmemory-jc MockMvc integration tests

--- samples/inmemory-jc/src/test/java/org/springframework/security/samples/config/SecurityConfigTests.java
+++ samples/inmemory-jc/src/test/java/org/springframework/security/samples/config/SecurityConfigTests.java
@@ -15,19 +15,91 @@
  */
 package org.springframework.security.samples.config;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import javax.servlet.Filter;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.samples.mvc.config.WebMvcConfiguration;
+import org.springframework.security.test.context.DefaultSecurityTestExecutionListeners;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithSecurityContextTestExcecutionListener;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @author Rob Winch
  *
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=SecurityConfig.class)
+@ContextConfiguration(classes={RootConfiguration.class, WebMvcConfiguration.class})
+@WebAppConfiguration
+@DefaultSecurityTestExecutionListeners
 public class SecurityConfigTests {
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private Filter springSecurityFilterChain;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .addFilters(springSecurityFilterChain)
+                .defaultRequest(get("/").with(testSecurityContext()))
+                .build();
+    }
 
     @Test
-    public void securityConfigurationLoads() {}
+    public void requestProtectedResourceRequiresAuthentication() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void loginSuccess() throws Exception {
+        mvc.perform(formLogin())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void loginFailure() throws Exception {
+        mvc.perform(formLogin().password("invalid"))
+                .andExpect(redirectedUrl("/login?error"));
+    }
+
+    @Test
+    @WithMockUser
+    public void requestProtectedResourceWithUser() throws Exception {
+        mvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void logoutSuccess() throws Exception {
+        mvc.perform(logout())
+                .andExpect(redirectedUrl("/login?logout"))
+                .andExpect(unauthenticated());
+    }
 }
