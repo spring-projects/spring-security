10c4d8f1af79de890ec8962a3544d23b4b6e3ae1
Rob Winch <rwinch@gopivotal.com>
2014-06-12 16:48:04 -0500

    SEC-2651: Update Thymeleaf to Spring 4 variants in samples

--- samples/messages-jc/src/main/java/org/springframework/security/samples/mvc/config/WebMvcConfiguration.java
+++ samples/messages-jc/src/main/java/org/springframework/security/samples/mvc/config/WebMvcConfiguration.java
@@ -11,10 +11,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.extras.tiles2.dialect.TilesDialect;
-import org.thymeleaf.extras.tiles2.spring.web.configurer.ThymeleafTilesConfigurer;
-import org.thymeleaf.extras.tiles2.spring.web.view.ThymeleafTilesView;
-import org.thymeleaf.spring3.SpringTemplateEngine;
-import org.thymeleaf.spring3.view.ThymeleafViewResolver;
+import org.thymeleaf.extras.tiles2.spring4.web.configurer.ThymeleafTilesConfigurer;
+import org.thymeleaf.extras.tiles2.spring4.web.view.ThymeleafTilesView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @EnableWebMvc
