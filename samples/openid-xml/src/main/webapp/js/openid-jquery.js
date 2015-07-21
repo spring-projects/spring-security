54dabb6433b748508a581421452a71a238dcd8a9
Rob Winch <rwinch@gopivotal.com>
2015-07-21 10:44:54 -0500

    SEC-2993: OpenID Sample now uses me.yahoo.com

--- samples/openid-xml/src/main/webapp/js/openid-jquery.js
+++ samples/openid-xml/src/main/webapp/js/openid-jquery.js
@@ -12,7 +12,7 @@ var providers_large = {
     },
     yahoo: {
         name: 'Yahoo',      
-        url: 'http://yahoo.com/'
+        url: 'https://me.yahoo.com/'
     },    
     aol: {
         name: 'AOL',     
