26ab012b57af310d95ff79c196865894810c6e99
Rob Winch <rwinch@gopivotal.com>
2015-08-18 13:58:01 -0500

    Start 4.1.0

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -22,7 +22,7 @@ public class SpringSecurityCoreVersion {
 	 * N.B. Classes are not intended to be serializable between different versions. See
 	 * SEC-1709 for why we still need a serial version.
 	 */
-	public static final long SERIAL_VERSION_UID = 400L;
+	public static final long SERIAL_VERSION_UID = 410L;
 
 	static final String MIN_SPRING_VERSION = "4.1.6.RELEASE";
 
