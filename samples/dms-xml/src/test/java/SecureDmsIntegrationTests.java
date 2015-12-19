09c8fee88bb0f10ee6728accb4a804e1116a1c56
wkorando <wkorando@gmail.com>
2015-12-19 09:48:27 -0600

    Change Junit asserts to AssertJ

--- samples/dms-xml/src/test/java/SecureDmsIntegrationTests.java
+++ samples/dms-xml/src/test/java/SecureDmsIntegrationTests.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.ContextConfiguration;
 public class SecureDmsIntegrationTests extends DmsIntegrationTests {
 
 	@Test
-	public void testBasePopulation() {
+	public void testBasePopulation() { 
 		assertThat(jdbcTemplate.queryForObject("select count(id) from DIRECTORY", Integer.class)).isEqualTo(9);
 		assertThat(jdbcTemplate.queryForObject("select count(id) from FILE", Integer.class)).isEqualTo(90);
 		assertThat(jdbcTemplate.queryForObject("select count(id) from ACL_SID", Integer.class)).isEqualTo(4); // 3 users + 1 role
