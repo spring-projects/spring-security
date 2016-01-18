8e0e0288d70c3f3636749ecde4ca822e9db69edc
avri-schneider <avri.schneider@gmail.com>
2016-01-18 19:49:57 +0200

    Prevent array out of bounds
    Make sure no access of expectedBytes array is made if expectedBytes == null.

--- core/src/main/java/org/springframework/security/authentication/encoding/PasswordEncoderUtils.java
+++ core/src/main/java/org/springframework/security/authentication/encoding/PasswordEncoderUtils.java
@@ -20,10 +20,11 @@ class PasswordEncoderUtils {
 	static boolean equals(String expected, String actual) {
 		byte[] expectedBytes = bytesUtf8(expected);
 		byte[] actualBytes = bytesUtf8(actual);
-		int expectedLength = expectedBytes == null ? -1 : expectedBytes.length;
+		int expectedLength = expectedBytes == null ? 0 : expectedBytes.length;
 		int actualLength = actualBytes == null ? -1 : actualBytes.length;
 
 		int result = expectedLength != actualLength ? 1 : 0;
+		result |= ((expectedBytes == null && actualBytes != null) || (expectedBytes != null && actualBytes == null)) ? 1 : 0;
 		for (int i = 0; i < actualLength; i++) {
 			result |= expectedBytes[i % expectedLength] ^ actualBytes[i % actualLength];
 		}
