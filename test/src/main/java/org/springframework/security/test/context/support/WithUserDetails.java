df5e3ba6eefc5fab3464e7879570b068bd40ec73
Rob Winch <rwinch@gopivotal.com>
2016-03-09 16:24:50 -0600

    Polish Imports

--- test/src/main/java/org/springframework/security/test/context/support/WithUserDetails.java
+++ test/src/main/java/org/springframework/security/test/context/support/WithUserDetails.java
@@ -23,7 +23,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
