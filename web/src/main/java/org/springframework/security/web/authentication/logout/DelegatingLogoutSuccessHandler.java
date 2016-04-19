4093690322fc99d8b83a90748e92eeeadabb0051
Rob Winch <rwinch@gopivotal.com>
2016-04-19 12:00:06 -0500

    Polish Logout Content Negotiation
    * Rename to DelegatingLogoutSuccessHandler for consistency
    * Remove JavascriptOriginRequestMatcher in favor of
    RequestHeaderRequestMatcher
    
    Issue gh-3282

---
+++ web/src/main/java/org/springframework/security/web/authentication/logout/DelegatingLogoutSuccessHandler.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.web.authentication.logout;
+
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.Assert;
+
+/**
+ * Delegates to logout handlers based on matched request matchers
+ *
+ * @author Shazin Sadakath
+ * @author Rob Winch
+ * @since 4.1
+ */
+public class DelegatingLogoutSuccessHandler implements LogoutSuccessHandler {
+
+	private final LinkedHashMap<RequestMatcher, LogoutSuccessHandler> matcherToHandler;
+
+	private LogoutSuccessHandler defaultLogoutSuccessHandler;
+
+	public DelegatingLogoutSuccessHandler(
+			LinkedHashMap<RequestMatcher, LogoutSuccessHandler> matcherToHandler) {
+		Assert.notEmpty(matcherToHandler, "matcherToHandler cannot be null");
+		this.matcherToHandler = matcherToHandler;
+	}
+
+	@Override
+	public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		for (Map.Entry<RequestMatcher, LogoutSuccessHandler> entry : this.matcherToHandler
+				.entrySet()) {
+			RequestMatcher matcher = entry.getKey();
+			if (matcher.matches(request)) {
+				LogoutSuccessHandler handler = entry.getValue();
+				handler.onLogoutSuccess(request, response, authentication);
+				return;
+			}
+		}
+		if (this.defaultLogoutSuccessHandler != null) {
+			this.defaultLogoutSuccessHandler.onLogoutSuccess(request, response,
+					authentication);
+		}
+	}
+
+	/**
+	 * Sets the default {@link LogoutSuccessHandler} if no other handlers available
+	 *
+	 * @param defaultLogoutSuccessHandler the defaultLogoutSuccessHandler to set
+	 */
+	public void setDefaultLogoutSuccessHandler(
+			LogoutSuccessHandler defaultLogoutSuccessHandler) {
+		this.defaultLogoutSuccessHandler = defaultLogoutSuccessHandler;
+	}
+
+}
---
+++ web/src/test/java/org/springframework/security/web/authentication/logout/DelegatingLogoutSuccessHandlerTests.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.web.authentication.logout;
+
+import java.util.LinkedHashMap;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * DelegatingLogoutSuccessHandlerTests Tests
+ *
+ * @author Shazin Sadakath
+ * @author Rob Winch
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class DelegatingLogoutSuccessHandlerTests {
+
+	@Mock
+	RequestMatcher matcher;
+	@Mock
+	RequestMatcher matcher2;
+	@Mock
+	LogoutSuccessHandler handler;
+	@Mock
+	LogoutSuccessHandler handler2;
+	@Mock
+	LogoutSuccessHandler defaultHandler;
+	@Mock
+	HttpServletRequest request;
+	@Mock
+	MockHttpServletResponse response;
+	@Mock
+	Authentication authentication;
+
+	DelegatingLogoutSuccessHandler delegatingHandler;
+
+	@Before
+	public void setup() {
+		LinkedHashMap<RequestMatcher, LogoutSuccessHandler> matcherToHandler = new LinkedHashMap<RequestMatcher, LogoutSuccessHandler>();
+		matcherToHandler.put(this.matcher, this.handler);
+		matcherToHandler.put(this.matcher2, this.handler2);
+		this.delegatingHandler = new DelegatingLogoutSuccessHandler(matcherToHandler);
+	}
+
+	@Test
+	public void onLogoutSuccessFirstMatches() throws Exception {
+		this.delegatingHandler.setDefaultLogoutSuccessHandler(this.defaultHandler);
+		when(this.matcher.matches(this.request)).thenReturn(true);
+
+		this.delegatingHandler.onLogoutSuccess(this.request, this.response,
+				this.authentication);
+
+		verify(this.handler).onLogoutSuccess(this.request, this.response,
+				this.authentication);
+		verifyZeroInteractions(this.matcher2, this.handler2, this.defaultHandler);
+	}
+
+	@Test
+	public void onLogoutSuccessSecondMatches() throws Exception {
+		this.delegatingHandler.setDefaultLogoutSuccessHandler(this.defaultHandler);
+		when(this.matcher2.matches(this.request)).thenReturn(true);
+
+		this.delegatingHandler.onLogoutSuccess(this.request, this.response,
+				this.authentication);
+
+		verify(this.handler2).onLogoutSuccess(this.request, this.response,
+				this.authentication);
+		verifyZeroInteractions(this.handler, this.defaultHandler);
+	}
+
+	@Test
+	public void onLogoutSuccessDefault() throws Exception {
+		this.delegatingHandler.setDefaultLogoutSuccessHandler(this.defaultHandler);
+
+		this.delegatingHandler.onLogoutSuccess(this.request, this.response,
+				this.authentication);
+
+		verify(this.defaultHandler).onLogoutSuccess(this.request, this.response,
+				this.authentication);
+		verifyZeroInteractions(this.handler, this.handler2);
+	}
+
+	@Test
+	public void onLogoutSuccessNoMatchDefaultNull() throws Exception {
+
+		this.delegatingHandler.onLogoutSuccess(this.request, this.response,
+				this.authentication);
+
+		verifyZeroInteractions(this.handler, this.handler2, this.defaultHandler);
+	}
+}
