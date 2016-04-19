77ad2fda2888add561c564347e28492be0003635
Rob Winch <rwinch@gopivotal.com>
2016-04-19 11:38:20 -0500

    RoleHiearchy Bean used in GlobalMethodSecurity
    Previously it required quite a bit of extra work to use RoleHiearchy
    within Java Based Spring Security configuration.
    
    Now if a single RoleHiearchy Bean is defined it will automatically
    be picked up and used by method security.
    
    Fixes gh-3394

--- config/src/test/groovy/org/springframework/security/config/annotation/method/configuration/MethodSecurityServiceImpl.groovy
+++ config/src/test/groovy/org/springframework/security/config/annotation/method/configuration/MethodSecurityServiceImpl.groovy
@@ -56,6 +56,10 @@ public class MethodSecurityServiceImpl implements MethodSecurityService {
 	}
 
 	@Override
+	public void preAuthorizeAdmin() {
+	}
+
+	@Override
 	public String preAuthorizePermitAll() {
 		return null;
 	}
