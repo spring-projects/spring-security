9afb364116b034468ad79b312bf082bcc40d513a
Jitendra Singh Bisht <jitendra.singh@tothenew.com>
2016-04-22 13:00:57 +0530

    No need of constructor because Deserializer is already registered

--- core/src/main/java/org/springframework/security/jackson2/UserMixin.java
+++ core/src/main/java/org/springframework/security/jackson2/UserMixin.java
@@ -16,12 +16,8 @@
 
 package org.springframework.security.jackson2;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import org.springframework.security.core.GrantedAuthority;
-
-import java.util.Collection;
 
 /**
  * @author Jitendra Singh
@@ -29,11 +25,4 @@ import java.util.Collection;
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
 @JsonDeserialize(using = UserDeserializer.class)
 public abstract class UserMixin {
-
-	public UserMixin(@JsonProperty("username") String username, @JsonProperty("password") String password,
-					@JsonProperty("enabled") boolean enabled, @JsonProperty("accountNonExpired") boolean accountNonExpired,
-					@JsonProperty("credentialsNonExpired") boolean credentialsNonExpired,
-					@JsonProperty("accountNonLocked") boolean accountNonLocked,
-					@JsonProperty("authorities") Collection<? extends GrantedAuthority> authorities) {
-	}
 }
