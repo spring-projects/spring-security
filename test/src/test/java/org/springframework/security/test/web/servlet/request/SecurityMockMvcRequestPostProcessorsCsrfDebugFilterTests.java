18071af3e48f6230d533fce68a181c1d47e62433
Jens Goldhammer <j.goldhammer@fme.de>
2016-04-24 13:53:46 +0200

    chore(unit-test): better test for DebugFilter and CSRFTokenRepositories

--- test/src/test/java/org/springframework/security/test/web/servlet/request/SecurityMockMvcRequestPostProcessorsCsrfDebugFilterTests.java
+++ test/src/test/java/org/springframework/security/test/web/servlet/request/SecurityMockMvcRequestPostProcessorsCsrfDebugFilterTests.java
@@ -21,7 +21,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,31 +32,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequestWrapper;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @WebAppConfiguration
+
 /**
  * https://github.com/spring-projects/spring-security/pull/3836
  */
@@ -66,22 +53,14 @@ public class SecurityMockMvcRequestPostProcessorsCsrfDebugFilterTests {
     @Autowired
 	WebApplicationContext wac;
 
-	MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(wac)
-				.apply(springSecurity())
-				.build();
-	}
-
     @Test
-    public void csrfWithParam() throws Exception {
-        MvcResult result = mockMvc.perform(post("/").with(csrf()))
-                .andExpect(status().is2xxSuccessful()).andReturn();
+    public void shouldFindCookieCsrfTokenRepository() throws Exception {
 
-        CsrfTokenRepository csrfTokenRepository = WebTestUtils.getCsrfTokenRepository(result.getRequest());
+		// do
+		MockHttpServletRequest request = post("/").buildRequest(wac.getServletContext());
+		CsrfTokenRepository csrfTokenRepository = WebTestUtils.getCsrfTokenRepository(request);
+
+		// expect
         assertThat(csrfTokenRepository).isInstanceOf(CookieCsrfTokenRepository.class);
 
     }
