bd5f71bb0d236203a081df1e539a130458e49c3f
Rob Winch <rwinch@gopivotal.com>
2016-06-21 17:08:37 -0500

    Polish
    Fix checkstyle for LDAP JavaConfig Authority mapping
    
    Issue gh-2768

--- config/src/test/java/org/springframework/security/config/annotation/authentication/configurers/ldap/LdapAuthenticationProviderConfigurerTest.java
+++ config/src/test/java/org/springframework/security/config/annotation/authentication/configurers/ldap/LdapAuthenticationProviderConfigurerTest.java
@@ -1,39 +1,44 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2002-2016 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.springframework.security.config.annotation.authentication.configurers.ldap;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LdapAuthenticationProviderConfigurerTest {
 
-    private LdapAuthenticationProviderConfigurer configurer;
+	private LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder> configurer;
 
-    @Before
-    public void setUp() {
-        configurer = new LdapAuthenticationProviderConfigurer();
-    }
+	@Before
+	public void setUp() {
+		configurer = new LdapAuthenticationProviderConfigurer<AuthenticationManagerBuilder>();
+	}
 
-    // SEC-2557
-    @Test
-    public void getAuthoritiesMapper() throws Exception {
-        assertEquals(SimpleAuthorityMapper.class, configurer.getAuthoritiesMapper().getClass());
-        configurer.authoritiesMapper(new NullAuthoritiesMapper());
-        assertEquals(NullAuthoritiesMapper.class, configurer.getAuthoritiesMapper().getClass());
+	// SEC-2557
+	@Test
+	public void getAuthoritiesMapper() throws Exception {
+		assertThat(configurer.getAuthoritiesMapper()).isInstanceOf(SimpleAuthorityMapper.class);
+		configurer.authoritiesMapper(new NullAuthoritiesMapper());
+		assertThat(configurer.getAuthoritiesMapper()).isInstanceOf(NullAuthoritiesMapper.class);
 
-    }
+	}
 }
