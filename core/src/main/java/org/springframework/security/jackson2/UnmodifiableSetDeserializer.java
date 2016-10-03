4e83d274e74d00420822428f4a4afe02eed6f783
Jitendra Singh <jeet.mp3@gmail.com>
2016-10-04 01:24:57 +0530

    Space indentation converted to tabs

--- core/src/main/java/org/springframework/security/jackson2/UnmodifiableSetDeserializer.java
+++ core/src/main/java/org/springframework/security/jackson2/UnmodifiableSetDeserializer.java
@@ -39,23 +39,23 @@ import java.util.Set;
  */
 class UnmodifiableSetDeserializer extends JsonDeserializer<Set> {
 
-    @Override
-    public Set deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-        ObjectMapper mapper = (ObjectMapper) jp.getCodec();
-        JsonNode node = mapper.readTree(jp);
-        Set<Object> resultSet = new HashSet<Object>();
-        if (node != null) {
-            if (node instanceof ArrayNode) {
-                ArrayNode arrayNode = (ArrayNode) node;
-                Iterator<JsonNode> nodeIterator = arrayNode.iterator();
-                while (nodeIterator.hasNext()) {
-                    JsonNode elementNode = nodeIterator.next();
-                    resultSet.add(mapper.readValue(elementNode.toString(), Object.class));
-                }
-            } else {
-                resultSet.add(mapper.readValue(node.toString(), Object.class));
-            }
-        }
-        return Collections.unmodifiableSet(resultSet);
-    }
+	@Override
+	public Set deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+		ObjectMapper mapper = (ObjectMapper) jp.getCodec();
+		JsonNode node = mapper.readTree(jp);
+		Set<Object> resultSet = new HashSet<Object>();
+		if (node != null) {
+			if (node instanceof ArrayNode) {
+				ArrayNode arrayNode = (ArrayNode) node;
+				Iterator<JsonNode> nodeIterator = arrayNode.iterator();
+				while (nodeIterator.hasNext()) {
+					JsonNode elementNode = nodeIterator.next();
+					resultSet.add(mapper.readValue(elementNode.toString(), Object.class));
+				}
+			} else {
+				resultSet.add(mapper.readValue(node.toString(), Object.class));
+			}
+		}
+		return Collections.unmodifiableSet(resultSet);
+	}
 }
