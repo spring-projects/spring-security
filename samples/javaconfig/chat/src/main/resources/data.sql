46bb85573758d2409819ba1583588c7d41e5af93
Rob Winch <rwinch@gopivotal.com>
2016-10-17 10:55:49 -0500

    Remove chat sample
    Removed in favor of
    https://github.com/spring-projects/spring-session/tree/master/samples/websocket
    
    Fixes gh-4094

--- samples/javaconfig/chat/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into user(id,email,password,firstname,lastname) values (0,'rob@example.com','password','Rob','Winch');
-insert into user(id,email,password,firstname,lastname) values (1,'luke@example.com','password','Luke','Taylor');
-insert into user(id,email,password,firstname,lastname) values (2,'eve@example.com','password','Eve','Sdropper');
--- samples/javaconfig/chat/src/main/resources/password-encode.sql
+++
@@ -1,1 +0,0 @@
-update user set password = '$2a$10$FBAKClV1zBIOOC9XMXf3AO8RoGXYVYsfvUdoLxGkd/BnXEn4tqT3u';
