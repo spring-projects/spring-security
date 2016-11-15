a28e34d0c0bb2002984328d1949d860a0c3d5a7c
Eddú Meléndez <eddu.melendez@gmail.com>
2016-11-14 20:38:43 -0500

    Add HttpSessionRequestCache sessionAttrName property
    This commit allows to customize the session attribute name. Default is
    SPRING_SECURITY_SAVED_REQUEST.
    
    Fixes gh-4130

--- web/src/test/java/org/springframework/security/web/savedrequest/HttpSessionRequestCacheTests.java
+++ web/src/test/java/org/springframework/security/web/savedrequest/HttpSessionRequestCacheTests.java
@@ -35,6 +35,7 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 /**
  *
  * @author Luke Taylor
+ * @author Eddú Meléndez
  * @since 3.0
  */
 public class HttpSessionRequestCacheTests {
@@ -99,6 +100,21 @@ public class HttpSessionRequestCacheTests {
 		cache.saveRequest(request, response);
 		assertThat(cache.getRequest(request, response)).isInstanceOf(
 				CustomSavedRequest.class);
+	}
+
+	@Test
+	public void testCustomSessionAttrName() {
+		HttpSessionRequestCache cache = new HttpSessionRequestCache();
+		cache.setSessionAttrName("CUSTOM_SAVED_REQUEST");
+
+		MockHttpServletRequest request = new MockHttpServletRequest("GET",
+				"/destination");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		cache.saveRequest(request, response);
+
+		assertThat(request.getSession().getAttribute(HttpSessionRequestCache.SAVED_REQUEST)).isNull();
+		assertThat(request.getSession().getAttribute("CUSTOM_SAVED_REQUEST")).isNotNull();
+
 	}
 
 	private static final class CustomSavedRequest implements SavedRequest {
