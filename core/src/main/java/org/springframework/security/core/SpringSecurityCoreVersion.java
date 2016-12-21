cdc485d12136ee4f7c1eb27dfcd365d0df22551d
Rob Winch <rwinch@gopivotal.com>
2016-12-20 20:23:45 -0600

    Update to spring 3.2.17

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -23,7 +23,7 @@ public class SpringSecurityCoreVersion {
      */
     public static final long SERIAL_VERSION_UID = 320L;
 
-    static final String MIN_SPRING_VERSION = "3.2.13.RELEASE";
+    static final String MIN_SPRING_VERSION = "3.2.17.RELEASE";
 
     static {
         performVersionChecks();
