b5e0886bde488f86b167ef0718a72f9866c5c24b
Rob Winch <rwinch@gopivotal.com>
2017-01-04 09:51:16 -0600

    update to spring-io-plugin:0.0.6
    - Update spring-io-plugin
    - Update Spring Boot because spring-io-plugin now requires
      non passive dependency management plugin

--- samples/boot/helloworld/src/integration-test/java/org/springframework/security/samples/HelloWorldApplicationTests.java
+++ samples/boot/helloworld/src/integration-test/java/org/springframework/security/samples/HelloWorldApplicationTests.java
@@ -15,25 +15,22 @@
  */
 package org.springframework.security.samples;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,78 +40,79 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Joe Grandja
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(HelloWorldApplication.class)
-@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HelloWorldApplicationTests {
 
 	@Autowired
-	private WebApplicationContext context;
-
 	private MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(context)
-				.apply(springSecurity())
-				.build();
-	}
 
 	@Test
 	public void accessUnprotected() throws Exception {
-		this.mockMvc.perform(get("/index")).andExpect(status().isOk());
+		// @formatter:off
+		this.mockMvc.perform(get("/index"))
+				.andExpect(status().isOk());
+		// @formatter:on
 	}
 
 	@Test
 	public void accessProtectedRedirectsToLogin() throws Exception {
+		// @formatter:off
 		MvcResult mvcResult = this.mockMvc.perform(get("/user/index"))
 				.andExpect(status().is3xxRedirection())
 				.andReturn();
+		// @formatter:on
 
 		assertThat(mvcResult.getResponse().getRedirectedUrl()).endsWith("/login");
 	}
 
 	@Test
 	public void loginUser() throws Exception {
+		// @formatter:off
 		this.mockMvc.perform(formLogin().user("user").password("password"))
 				.andExpect(authenticated());
+		// @formatter:on
 	}
 
 	@Test
 	public void loginInvalidUser() throws Exception {
+		// @formatter:off
 		this.mockMvc.perform(formLogin().user("invalid").password("invalid"))
 				.andExpect(unauthenticated())
 				.andExpect(status().is3xxRedirection());
+		// @formatter:on
 	}
 
 	@Test
 	public void loginUserAccessProtected() throws Exception {
+		// @formatter:off
 		MvcResult mvcResult = this.mockMvc.perform(formLogin().user("user").password("password"))
-				.andExpect(authenticated())
-				.andReturn();
+				.andExpect(authenticated()).andReturn();
+		// @formatter:on
 
-		MockHttpSession httpSession = MockHttpSession.class.cast(mvcResult.getRequest().getSession(false));
+		MockHttpSession httpSession = (MockHttpSession) mvcResult.getRequest().getSession(false);
 
-		this.mockMvc.perform(get("/user/index")
-				.session(httpSession))
+		// @formatter:off
+		this.mockMvc.perform(get("/user/index").session(httpSession))
 				.andExpect(status().isOk());
+		// @formatter:on
 	}
 
 	@Test
 	public void loginUserValidateLogout() throws Exception {
+		// @formatter:off
 		MvcResult mvcResult = this.mockMvc.perform(formLogin().user("user").password("password"))
-				.andExpect(authenticated())
-				.andReturn();
+				.andExpect(authenticated()).andReturn();
+		// @formatter:on
 
-		MockHttpSession httpSession = MockHttpSession.class.cast(mvcResult.getRequest().getSession(false));
+		MockHttpSession httpSession = (MockHttpSession) mvcResult.getRequest().getSession(false);
 
-		this.mockMvc.perform(post("/logout").with(csrf())
-				.session(httpSession))
+		// @formatter:off
+		this.mockMvc.perform(post("/logout").with(csrf()).session(httpSession))
 				.andExpect(unauthenticated());
-
-		this.mockMvc.perform(get("/user/index")
-				.session(httpSession))
+		this.mockMvc.perform(get("/user/index").session(httpSession))
 				.andExpect(unauthenticated())
 				.andExpect(status().is3xxRedirection());
+		// @formatter:on
 	}
 }
--- samples/boot/insecure/src/integration-test/java/org/springframework/security/samples/InsecureApplicationTests.java
+++ samples/boot/insecure/src/integration-test/java/org/springframework/security/samples/InsecureApplicationTests.java
@@ -15,16 +15,14 @@
  */
 package org.springframework.security.samples;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -34,21 +32,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author Joe Grandja
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(InsecureApplication.class)
-@WebAppConfiguration
+@SpringBootTest
+@AutoConfigureMockMvc
 public class InsecureApplicationTests {
 
 	@Autowired
-	private WebApplicationContext context;
-
 	private MockMvc mockMvc;
-
-	@Before
-	public void setup() {
-		mockMvc = MockMvcBuilders
-				.webAppContextSetup(context)
-				.build();
-	}
 
 	@Test
 	public void accessUnprotected() throws Exception {
