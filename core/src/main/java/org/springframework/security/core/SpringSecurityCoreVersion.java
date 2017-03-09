3ca78df15fc7c864c6db5b29b0cadd138d6d8dc3
Rob Winch <rwinch@gopivotal.com>
2017-03-08 19:12:39 -0600

    Fix Security version Test

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -38,7 +38,7 @@ public class SpringSecurityCoreVersion {
 	 * N.B. Classes are not intended to be serializable between different versions. See
 	 * SEC-1709 for why we still need a serial version.
 	 */
-	public static final long SERIAL_VERSION_UID = 420L;
+	public static final long SERIAL_VERSION_UID = 500L;
 
 	static final String MIN_SPRING_VERSION = "4.3.5.RELEASE";
 
