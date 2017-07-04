c986b6f4b521efc05c1c099d361402a8c1580890
Joe Grandja <jgrandja@pivotal.io>
2017-07-04 17:30:54 -0400

    Add support for JWT/JWS
    Fixes gh-4434

---
+++ oauth2/jwt-jose/src/main/java/org/springframework/security/jwt/JwtClaimAccessor.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.jwt;
+
+import org.springframework.security.oauth2.core.ClaimAccessor;
+
+import java.net.URI;
+import java.time.Instant;
+
+/**
+ * A {@link ClaimAccessor} for the &quot;Registered Claim Names&quot;
+ * that may be contained in the JSON object <i>JWT Claims Set</i> of a <i>JSON Web Token (JWT)</i>.
+ *
+ * @author Joe Grandja
+ * @since 5.0
+ * @see ClaimAccessor
+ * @see JwtClaim
+ * @see Jwt
+ * @see <a target="_blank" href="https://tools.ietf.org/html/rfc7519#section-4.1">Registered Claim Names</a>
+ */
+public interface JwtClaimAccessor extends ClaimAccessor {
+
+	default URI getIssuer() {
+		return this.getClaimAsURI(JwtClaim.ISS);
+	}
+
+	default String getSubject() {
+		return this.getClaimAsString(JwtClaim.SUB);
+	}
+
+	default String getAudience() {
+		// FIXME Should return String[]
+		return this.getClaimAsString(JwtClaim.AUD);
+	}
+
+	default Instant getExpiresAt() {
+		return this.getClaimAsInstant(JwtClaim.EXP);
+	}
+
+	default Instant getNotBefore() {
+		return this.getClaimAsInstant(JwtClaim.NBF);
+	}
+
+	default Instant getIssuedAt() {
+		return this.getClaimAsInstant(JwtClaim.IAT);
+	}
+
+	default String getId() {
+		return this.getClaimAsString(JwtClaim.JTI);
+	}
+}
---
+++ oauth2/oauth2-core/src/main/java/org/springframework/security/oauth2/core/ClaimAccessor.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.oauth2.core;
+
+import org.springframework.util.Assert;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Instant;
+import java.util.Map;
+
+/**
+ * An &quot;accessor&quot; for a set of claims that may be used for assertions.
+ *
+ * @author Joe Grandja
+ * @since 5.0
+ */
+public interface ClaimAccessor {
+
+	Map<String, Object> getClaims();
+
+	default Boolean containsClaim(String claim) {
+		Assert.notNull(claim, "claim cannot be null");
+		return this.getClaims().containsKey(claim);
+	}
+
+	default String getClaimAsString(String claim) {
+		return (this.containsClaim(claim) ? this.getClaims().get(claim).toString() : null);
+	}
+
+	default Boolean getClaimAsBoolean(String claim) {
+		return (this.containsClaim(claim) ? Boolean.valueOf(this.getClaimAsString(claim)) : null);
+	}
+
+	default Instant getClaimAsInstant(String claim) {
+		if (!this.containsClaim(claim)) {
+			return null;
+		}
+		try {
+			return Instant.ofEpochSecond(Long.valueOf(this.getClaimAsString(claim)));
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Unable to convert claim '" + claim + "' to Instant: " + ex.getMessage(), ex);
+		}
+	}
+
+	default URI getClaimAsURI(String claim) {
+		if (!this.containsClaim(claim)) {
+			return null;
+		}
+		try {
+			return new URI(this.getClaimAsString(claim));
+		} catch (URISyntaxException ex) {
+			throw new IllegalArgumentException("Unable to convert claim '" + claim + "' to URI: " + ex.getMessage(), ex);
+		}
+	}
+}
