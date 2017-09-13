1fe414379c34ce3e22723f98df8c491f72dc1ce0
Rob Winch <rwinch@users.noreply.github.com>
2017-09-13 10:39:42 -0500

    Simplify hellowebfluxfn
    This sample should be absolute minimal example

--- samples/javaconfig/hellowebfluxfn/src/integration-test/java/sample/HelloWebfluxFnApplicationITests.java
+++ samples/javaconfig/hellowebfluxfn/src/integration-test/java/sample/HelloWebfluxFnApplicationITests.java
@@ -15,23 +15,20 @@
  */
 package sample;
 
+import java.time.Duration;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.web.server.header.ContentTypeOptionsHttpHeadersWriter;
+import org.springframework.http.ResponseCookie;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.ExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
-
-import java.nio.charset.Charset;
-import java.time.Duration;
-import java.util.Base64;
 
 import static org.springframework.web.reactive.function.client.ExchangeFilterFunctions.basicAuthentication;
 
@@ -57,161 +54,67 @@ public class HelloWebfluxFnApplicationITests {
 	}
 
 	@Test
-	public void basicRequired() throws Exception {
+	public void basicWhenNoCredentialsThenUnauthorized() throws Exception {
 		this.rest
 			.get()
-			.uri("/users")
+			.uri("/")
 			.exchange()
 			.expectStatus().isUnauthorized();
 	}
 
 	@Test
-	public void basicWorks() throws Exception {
+	public void basicWhenValidCredentialsThenOk() throws Exception {
 		this.rest
 			.mutate()
-			.filter(robsCredentials())
+			.filter(userCredentials())
 			.build()
 			.get()
-			.uri("/principal")
+			.uri("/")
 			.exchange()
 			.expectStatus().isOk()
-			.expectBody().json("{\"username\":\"rob\"}");
+			.expectBody().json("{\"message\":\"Hello user!\"}");
 	}
 
 	@Test
-	public void basicWhenPasswordInvalid401() throws Exception {
+	public void basicWhenInvalidCredentialsThenUnauthorized() throws Exception {
 		this.rest
 			.mutate()
 			.filter(invalidPassword())
 			.build()
 			.get()
-			.uri("/principal")
+			.uri("/")
 			.exchange()
 			.expectStatus().isUnauthorized()
 			.expectBody().isEmpty();
 	}
 
 	@Test
-	public void authorizationAdmin403() throws Exception {
-		this.rest
-			.mutate()
-			.filter(robsCredentials())
-			.build()
-			.get()
-			.uri("/admin")
-			.exchange()
-			.expectStatus().isEqualTo(HttpStatus.FORBIDDEN)
-			.expectBody().isEmpty();
-	}
-
-	@Test
-	public void authorizationAdmin200() throws Exception {
-		this.rest
-			.mutate()
-			.filter(adminCredentials())
-			.build()
-			.get()
-			.uri("/admin")
-			.exchange()
-			.expectStatus().isOk();
-	}
-
-	@Test
-	public void basicMissingUser401() throws Exception {
-		this.rest
-			.mutate()
-			.filter(basicAuthentication("missing-user", "password"))
-			.build()
-			.get()
-			.uri("/admin")
-			.exchange()
-			.expectStatus().isUnauthorized();
-	}
-
-	@Test
-	public void basicInvalidPassword401() throws Exception {
-		this.rest
-			.mutate()
-			.filter(invalidPassword())
-			.build()
-			.get()
-			.uri("/admin")
-			.exchange()
-			.expectStatus().isUnauthorized();
-	}
-
-	@Test
-	public void basicInvalidParts401() throws Exception {
-		this.rest
-			.get()
-			.uri("/admin")
-			.header("Authorization", "Basic " + base64Encode("no colon"))
-			.exchange()
-			.expectStatus().isUnauthorized();
-	}
-
-	@Test
 	public void sessionWorks() throws Exception {
 		ExchangeResult result = this.rest
 			.mutate()
-			.filter(robsCredentials())
+			.filter(userCredentials())
 			.build()
 			.get()
-			.uri("/principal")
+			.uri("/")
 			.exchange()
+			.expectStatus().isOk()
 			.returnResult(String.class);
 
-		String session = result.getResponseHeaders().getFirst("Set-Cookie");
+		ResponseCookie session = result.getResponseCookies().getFirst("SESSION");
 
 		this.rest
 			.get()
-			.uri("/principal")
-			.header("Cookie", session)
+			.uri("/")
+			.cookie(session.getName(), session.getValue())
 			.exchange()
 			.expectStatus().isOk();
 	}
 
-	@Test
-	public void principal() throws Exception {
-		this.rest
-			.mutate()
-			.filter(robsCredentials())
-			.build()
-			.get()
-			.uri("/principal")
-			.exchange()
-			.expectStatus().isOk()
-			.expectBody().json("{\"username\" : \"rob\"}");
-	}
-
-	@Test
-	public void headers() throws Exception {
-		this.rest
-			.mutate()
-			.filter(robsCredentials())
-			.build()
-			.get()
-			.uri("/principal")
-			.exchange()
-			.expectHeader().valueEquals(HttpHeaders.CACHE_CONTROL, "no-cache, no-store, max-age=0, must-revalidate")
-			.expectHeader().valueEquals(HttpHeaders.EXPIRES, "0")
-			.expectHeader().valueEquals(HttpHeaders.PRAGMA, "no-cache")
-			.expectHeader().valueEquals(ContentTypeOptionsHttpHeadersWriter.X_CONTENT_OPTIONS, ContentTypeOptionsHttpHeadersWriter.NOSNIFF);
-	}
-
-	private ExchangeFilterFunction robsCredentials() {
-		return basicAuthentication("rob","rob");
+	private ExchangeFilterFunction userCredentials() {
+		return basicAuthentication("user","user");
 	}
 
 	private ExchangeFilterFunction invalidPassword() {
-		return basicAuthentication("rob","INVALID");
-	}
-
-	private ExchangeFilterFunction adminCredentials() {
-		return basicAuthentication("admin","admin");
-	}
-
-	private String base64Encode(String value) {
-		return Base64.getEncoder().encodeToString(value.getBytes(Charset.defaultCharset()));
+		return basicAuthentication("user","INVALID");
 	}
 }
