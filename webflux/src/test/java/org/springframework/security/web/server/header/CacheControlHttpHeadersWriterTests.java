1f4082e7541d81fd1a1e4d817c15309dfec4bcf5
Rob Winch <rwinch@users.noreply.github.com>
2017-09-18 11:11:25 -0500

    Fix copyright lines

--- webflux/src/test/java/org/springframework/security/web/server/header/CacheControlHttpHeadersWriterTests.java
+++ webflux/src/test/java/org/springframework/security/web/server/header/CacheControlHttpHeadersWriterTests.java
@@ -1,5 +1,4 @@
 /*
- *
  * Copyright 2002-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 package org.springframework.security.web.server.header;
 
