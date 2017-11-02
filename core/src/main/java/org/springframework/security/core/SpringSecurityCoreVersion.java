0f546dcb07b135adaab5ec775cded77a8d349fb9
Rob Winch <rwinch@users.noreply.github.com>
2017-11-02 14:53:57 -0500

    Update to Spring 4.3.12
    Fixes gh-4776

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -40,7 +40,7 @@ public class SpringSecurityCoreVersion {
 	 */
 	public static final long SERIAL_VERSION_UID = 420L;
 
-	static final String MIN_SPRING_VERSION = "4.3.9.RELEASE";
+	static final String MIN_SPRING_VERSION = "4.3.12.RELEASE";
 
 	static {
 		performVersionChecks();
