1217547ebda0139126489af34d3ee34de19f1b96
Rob Winch <rwinch@users.noreply.github.com>
2018-02-06 11:51:38 -0600

    User groovy->java
    Issue: gh-4939

--- config/src/test/java/org/springframework/security/config/annotation/issue50/domain/User.java
+++ config/src/test/java/org/springframework/security/config/annotation/issue50/domain/User.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,4 +59,10 @@ public class User {
 		this.password = password;
 	}
 
+	public static User withUsernameAndPassword(String username, String password) {
+		User user = new User();
+		user.setUsername(username);
+		user.setPassword(password);
+		return  user;
+	}
 }
