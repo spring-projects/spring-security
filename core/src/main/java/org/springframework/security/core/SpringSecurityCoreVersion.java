831399be16c26a1013e8afa3f350c014eee36e0f
Rob Winch <rwinch@users.noreply.github.com>
2018-02-19 22:00:33 -0600

    Update to Spring Framework 5.0.4
    Fixes: gh-5027

--- core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
+++ core/src/main/java/org/springframework/security/core/SpringSecurityCoreVersion.java
@@ -40,7 +40,7 @@ public class SpringSecurityCoreVersion {
 	 */
 	public static final long SERIAL_VERSION_UID = 500L;
 
-	static final String MIN_SPRING_VERSION = "5.0.3.RELEASE";
+	static final String MIN_SPRING_VERSION = "5.0.4.RELEASE";
 
 	static {
 		performVersionChecks();
