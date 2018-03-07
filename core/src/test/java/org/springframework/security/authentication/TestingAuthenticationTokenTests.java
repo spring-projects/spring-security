a7f180b2c7f163637fe4a5597e2c93be55cc7f69
Josh Cummings <josh.cummings@gmail.com>
2018-03-07 14:20:14 -0700

    Authorities authenticate TestingAuthenticationToken
    In other extensions of `AbstractAuthenticationToken`, the constructors
    that include `authorities` call `setAuthenticated(true)`. This includes
    `PreAuthenticated`-, `UsernamePassword`-, and
    `RememberMeAuthenticationToken`.
    
    This change brings `TestingAuthenticationToken` in line with that
    convention.
    
    Note that this was done once already to one of the constructors
    (ee13be4) in `TestingAuthenticationToken` that takes an arity of
    `authorities`. It was not propagated to the constructor that takes a
    collection, which is what this commit remedies.
    
    Fixes: gh-5098

---
+++ core/src/test/java/org/springframework/security/authentication/TestingAuthenticationTokenTests.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.authentication;
+
+import org.junit.Test;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Josh Cummings
+ */
+public class TestingAuthenticationTokenTests {
+	@Test
+	public void constructorWhenNoAuthoritiesThenUnauthenticated() {
+		TestingAuthenticationToken unauthenticated =
+			new TestingAuthenticationToken("principal", "credentials");
+
+		assertThat(unauthenticated.isAuthenticated()).isFalse();
+	}
+
+	@Test
+	public void constructorWhenArityAuthoritiesThenAuthenticated() {
+		TestingAuthenticationToken authenticated =
+			new TestingAuthenticationToken("principal", "credentials", "authority");
+
+		assertThat(authenticated.isAuthenticated()).isTrue();
+	}
+
+	@Test
+	public void constructorWhenCollectionAuthoritiesThenAuthenticated() {
+		TestingAuthenticationToken authenticated =
+			new TestingAuthenticationToken("principal", "credentials",
+				Arrays.asList(new SimpleGrantedAuthority("authority")));
+
+		assertThat(authenticated.isAuthenticated()).isTrue();
+	}
+}
