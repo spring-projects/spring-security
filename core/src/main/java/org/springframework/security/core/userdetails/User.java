39cc8b305d8bc61a5e0a36d2985b48e92b6f72b0
Alter Ego <25475850+AlterEgoDev@users.noreply.github.com>
2018-03-22 09:58:12 +0100

    Update User.java
    fixed a typo; replaced "User.witUsername("user")" with "User.withUsername("user")"

--- core/src/main/java/org/springframework/security/core/userdetails/User.java
+++ core/src/main/java/org/springframework/security/core/userdetails/User.java
@@ -314,7 +314,7 @@ public class User implements UserDetails, CredentialsContainer {
 	 *
 	 * <pre>
 	 * <code>
-	 * UserDetails user = User.witUsername("user")
+	 * UserDetails user = User.withUsername("user")
 	 *     .password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG")
 	 *     .roles("USER")
 	 *     .build();
