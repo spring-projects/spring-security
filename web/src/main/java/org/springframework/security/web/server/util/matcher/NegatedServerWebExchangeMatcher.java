d83b67e4cb86a5639f3a6ec048a3ba14bea5b0a4
Tao Qian <tqian@vmware.com>
2018-03-28 12:28:09 -0700

    Add NegatedServerWebExchangeMatcher
    Fixes: gh-5170

---
+++ web/src/main/java/org/springframework/security/web/server/util/matcher/NegatedServerWebExchangeMatcher.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.web.server.util.matcher;
+
+import org.springframework.util.Assert;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author Tao Qian
+ * @since 5.0
+ */
+public class NegatedServerWebExchangeMatcher implements ServerWebExchangeMatcher {
+	private final ServerWebExchangeMatcher matcher;
+
+	public NegatedServerWebExchangeMatcher(ServerWebExchangeMatcher matcher) {
+		Assert.notNull(matcher, "matcher cannot be null");
+		this.matcher = matcher;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher#matches(org.springframework.web.server.ServerWebExchange)
+	 */
+	@Override
+	public Mono<MatchResult> matches(ServerWebExchange exchange) {
+		return matcher.matches(exchange)
+			.flatMap(m -> m.isMatch() ? MatchResult.notMatch() : MatchResult.match());
+	}
+
+	@Override
+	public String toString() {
+		return "NegatedServerWebExchangeMatcher{" +
+				"matcher=" + matcher +
+				'}';
+	}
+}
