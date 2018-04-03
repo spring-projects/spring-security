d4e459874a2d6c58f822dd8d907870954167641e
Joe Grandja <jgrandja@pivotal.io>
2018-04-03 11:16:50 -0400

    Fix SecuredAnnotationSecurityMetadataSourceTests -> Related SPR-16677

--- core/src/test/java/org/springframework/security/access/annotation/sec2150/MethodInvocationFactory.java
+++ core/src/test/java/org/springframework/security/access/annotation/sec2150/MethodInvocationFactory.java
@@ -30,7 +30,7 @@ public class MethodInvocationFactory {
 	public static MockMethodInvocation createSec2150MethodInvocation()
 			throws NoSuchMethodException {
 		ProxyFactory factory = new ProxyFactory(new Class[] { PersonRepository.class });
-		factory.setTargetClass(CrudRepository.class);
+		factory.setTargetClass(PersonRepositoryImpl.class);
 		PersonRepository repository = (PersonRepository) factory.getProxy();
 		return new MockMethodInvocation(repository, PersonRepository.class, "findAll");
 	}
