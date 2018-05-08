c35c1c0643131fc10553886e9e77d8f87bd8b0e6
Rob Winch <rwinch@users.noreply.github.com>
2018-05-08 09:39:21 -0500

    Update Dependencies

---
+++ scripts/update-dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -f build/updates.txt
+./gradlew dependencyUpdate -Drevision=release
+find -name report.txt | xargs cat > build/updates.txt
+echo "Updates...."
+cat build/updates.txt | fgrep ' ->' | sort | uniq
