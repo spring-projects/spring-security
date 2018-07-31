4b407189c2c9497e8d5548b2742c3b17fac7679a
Rob Winch <rwinch@users.noreply.github.com>
2018-07-31 09:05:04 -0500

    ServerHttpBasicAuthenticationConverter Validates Scheme Name
    Fixes: gh-5615

--- web/src/main/java/org/springframework/security/web/server/ServerHttpBasicAuthenticationConverter.java
+++ web/src/main/java/org/springframework/security/web/server/ServerHttpBasicAuthenticationConverter.java
@@ -41,7 +41,7 @@ public class ServerHttpBasicAuthenticationConverter implements Function<ServerWe
 		ServerHttpRequest request = exchange.getRequest();
 
 		String authorization = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
-		if(authorization == null) {
+		if(authorization == null || !authorization.toLowerCase().startsWith("basic ")) {
 			return Mono.empty();
 		}
 
