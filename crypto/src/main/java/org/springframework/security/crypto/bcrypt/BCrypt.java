f3c541cda8cfa38b5b584d16fe21f044b2173ddb
linfeng <lin199231@gmail.com>
2018-10-19 11:46:26 +0800

    fix index_64 out bound error when char value equals 128

--- crypto/src/main/java/org/springframework/security/crypto/bcrypt/BCrypt.java
+++ crypto/src/main/java/org/springframework/security/crypto/bcrypt/BCrypt.java
@@ -423,7 +423,7 @@ public class BCrypt {
 	 * @return	the decoded value of x
 	 */
 	private static byte char64(char x) {
-		if ((int) x < 0 || (int) x > index_64.length)
+		if ((int) x < 0 || (int) x >= index_64.length)
 			return -1;
 		return index_64[(int) x];
 	}
