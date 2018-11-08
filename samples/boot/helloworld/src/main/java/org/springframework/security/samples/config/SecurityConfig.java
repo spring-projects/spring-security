46989eeff91fbb71bd450d079569a5a319f238b3
Rob Winch <rwinch@users.noreply.github.com>
2018-11-08 16:35:28 -0600

    Polish Servlet Hello Java Config

--- samples/boot/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
+++ samples/boot/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -15,12 +15,14 @@
  */
 package org.springframework.security.samples.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Joe Grandja
@@ -41,11 +43,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// @formatter:on
 
 	// @formatter:off
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER"));
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("USER")
+			.build();
+		return new InMemoryUserDetailsManager(user);
 	}
 	// @formatter:on
 }
