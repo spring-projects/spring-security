56dd69d0cded59a468316c8e7cc13c102035fdc1
Rob Winch <rwinch@users.noreply.github.com>
2018-11-21 15:48:10 -0600

    Polish Boot hello world

--- samples/boot/helloworld/src/main/java/sample/hello/HelloWorldApplication.java
+++ samples/boot/helloworld/src/main/java/sample/hello/HelloWorldApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.security.samples;
+package sample.hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -27,6 +27,4 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-
-
 }
