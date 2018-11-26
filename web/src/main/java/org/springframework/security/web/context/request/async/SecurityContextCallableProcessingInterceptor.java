fd31e52fb169da5c06286d1b94afda08ffac5e15
lmagyar <magyarl05@gmail.com>
2018-11-26 21:29:41 +0100

    SecurityContextCallableProcessingInterceptor thread visibility fix
    Within class SecurityContextCallableProcessingInterceptor field securityContext should volatile.
    
    Fixes gh-6143

--- web/src/main/java/org/springframework/security/web/context/request/async/SecurityContextCallableProcessingInterceptor.java
+++ web/src/main/java/org/springframework/security/web/context/request/async/SecurityContextCallableProcessingInterceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -41,7 +41,7 @@ import org.springframework.web.context.request.async.CallableProcessingIntercept
  */
 public final class SecurityContextCallableProcessingInterceptor extends
 		CallableProcessingInterceptorAdapter {
-	private SecurityContext securityContext;
+	private volatile SecurityContext securityContext;
 
 	/**
 	 * Create a new {@link SecurityContextCallableProcessingInterceptor} that uses the
