3766958d1f1e57862e99c23bd29afc344ac98958
shabari <shabari004@gmail.com>
2018-12-19 12:12:33 -0500

    adding Junit test MethodSecurityEvaluationContext setVariable Fixes: gh-6223

---
+++ core/src/test/java/org/springframework/security/access/expression/method/MethodSecurityEvaluationContextTests.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2002-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.access.expression.method;
+
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.lang.reflect.Method;
+
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.core.ParameterNameDiscoverer;
+import org.springframework.lang.Nullable;
+import org.springframework.security.authentication.AuthenticationTrustResolver;
+import org.springframework.security.core.Authentication;
+
+/**
+ * @author shabarijonnalagadda
+ *
+ */
+public class MethodSecurityEvaluationContextTests{
+
+	@Mock
+	private Authentication authentication;
+	@Mock
+	private MethodInvocation methodInvocation;
+	@Mock
+	private AuthenticationTrustResolver trustResolver;
+	@Mock
+	private Method method;
+
+	public  static class  NotNullVariableMethodSecurityEvaluationContext extends MethodSecurityEvaluationContext {
+
+		public NotNullVariableMethodSecurityEvaluationContext(Authentication auth, MethodInvocation mi, ParameterNameDiscoverer parameterNameDiscoverer) {
+			super(auth, mi, parameterNameDiscoverer);
+
+		}
+		@Override
+		public void setVariable(String name, @Nullable Object value) {
+			if (name == null) {
+				throw new IllegalArgumentException("name  should not be null");
+			}
+			else {
+				setVariable(name, value);
+			}
+		}
+
+	}
+
+	@Test
+	public void setVariableTest() {
+		ParameterNameDiscoverer paramNameDiscoverer = mock(ParameterNameDiscoverer.class);
+		when(paramNameDiscoverer.getParameterNames(method)).thenReturn( new String[] {null});
+
+		NotNullVariableMethodSecurityEvaluationContext NNVMSEC= new NotNullVariableMethodSecurityEvaluationContext(mock(Authentication.class), mock(MethodInvocation.class), paramNameDiscoverer);
+		try {
+			NNVMSEC.lookupVariable("TESTVALUE");
+		}
+		catch (IllegalArgumentException unexpected) {
+			fail("name  should not be null");
+
+		}
+	}
+
+
+}
