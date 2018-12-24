9a67441507a02d54c78013944012308fd9c1b740
Alexey Nesterov <alexey.nesterov@live.com>
2018-12-24 13:33:29 +0300

    Add x509 support for Reactive Security
    [gh #5038]

---
+++ web/src/main/java/org/springframework/security/web/server/authentication/ReactivePreAuthenticatedAuthenticationManager.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2002-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.security.web.server.authentication;
+
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsChecker;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import reactor.core.publisher.Mono;
+
+/**
+ * Reactive version of {@link org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider}
+ *
+ * This manager receives a {@link PreAuthenticatedAuthenticationToken}, checks that associated account is not disabled,
+ * expired, or blocked, and returns new authenticated {@link PreAuthenticatedAuthenticationToken}.
+ *
+ * If no {@link UserDetailsChecker} is provided, a default {@link AccountStatusUserDetailsChecker} will be
+ * created.
+ *
+ * @author Alexey Nesterov
+ * @since 5.2
+ */
+public class ReactivePreAuthenticatedAuthenticationManager
+		implements ReactiveAuthenticationManager {
+
+	private final ReactiveUserDetailsService userDetailsService;
+	private final UserDetailsChecker userDetailsChecker;
+
+	public ReactivePreAuthenticatedAuthenticationManager(ReactiveUserDetailsService userDetailsService) {
+		this(userDetailsService, new AccountStatusUserDetailsChecker());
+	}
+
+	public ReactivePreAuthenticatedAuthenticationManager(
+			ReactiveUserDetailsService userDetailsService,
+			UserDetailsChecker userDetailsChecker) {
+		this.userDetailsService = userDetailsService;
+		this.userDetailsChecker = userDetailsChecker;
+	}
+
+	@Override
+	public Mono<Authentication> authenticate(Authentication authentication) {
+		return Mono.just(authentication)
+				.filter(this::supports)
+				.map(Authentication::getName)
+				.flatMap(userDetailsService::findByUsername)
+				.switchIfEmpty(Mono.error(() -> new UsernameNotFoundException("User not found")))
+				.doOnNext(userDetailsChecker::check)
+				.map(ud -> {
+					PreAuthenticatedAuthenticationToken result = new PreAuthenticatedAuthenticationToken(
+							ud, authentication.getCredentials(), ud.getAuthorities());
+					result.setDetails(authentication.getDetails());
+
+					return result;
+				});
+	}
+
+	private boolean supports(Authentication authentication) {
+		return PreAuthenticatedAuthenticationToken.class.isAssignableFrom(authentication.getClass());
+	}
+}
---
+++ web/src/main/java/org/springframework/security/web/server/authentication/ServerX509AuthenticationConverter.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.web.server.authentication;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.http.server.reactive.SslInfo;
+import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+import org.springframework.security.web.authentication.preauth.x509.X509PrincipalExtractor;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.security.cert.X509Certificate;
+
+/**
+ * Converts from a {@link SslInfo} provided by a request to an {@link PreAuthenticatedAuthenticationToken} that can be authenticated.
+ *
+ * @author Alexey Nesterov
+ * @since 5.2
+ */
+public class ServerX509AuthenticationConverter implements ServerAuthenticationConverter {
+
+	protected final Log logger = LogFactory.getLog(getClass());
+	private final X509PrincipalExtractor principalExtractor;
+
+	public ServerX509AuthenticationConverter(@NonNull X509PrincipalExtractor principalExtractor) {
+		this.principalExtractor = principalExtractor;
+	}
+
+	@Override
+	public Mono<Authentication> convert(ServerWebExchange exchange) {
+		SslInfo sslInfo = exchange.getRequest().getSslInfo();
+		if (sslInfo == null) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("No SslInfo provided with a request, skipping x509 authentication");
+			}
+
+			return Mono.empty();
+		}
+
+		if (sslInfo.getPeerCertificates() == null || sslInfo.getPeerCertificates().length == 0) {
+			if (logger.isDebugEnabled()) {
+				logger.debug("No peer certificates found in SslInfo, skipping x509 authentication");
+			}
+
+			return Mono.empty();
+		}
+
+		X509Certificate clientCertificate = sslInfo.getPeerCertificates()[0];
+		Object principal = this.principalExtractor.extractPrincipal(clientCertificate);
+
+		PreAuthenticatedAuthenticationToken authRequest = new PreAuthenticatedAuthenticationToken(
+				principal, clientCertificate);
+
+		return Mono.just(authRequest);
+	}
+}
