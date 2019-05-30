56f19912408893552678790552ce440431f17fb4
Daniel Meier <daniel.meier@adesso.de>
2019-05-30 14:22:18 +0200

    Update AssertJ to version 3.12.2
    Update the AssertJ dependency to version 3.12.2. Additionally fix
    some tests not compiling due to API changes of AssertJ.
    
    Fixes gh-6786

--- oauth2/oauth2-client/src/test/java/org/springframework/security/oauth2/client/registration/InMemoryReactiveClientRegistrationRepositoryTests.java
+++ oauth2/oauth2-client/src/test/java/org/springframework/security/oauth2/client/registration/InMemoryReactiveClientRegistrationRepositoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +16,17 @@
 
 package org.springframework.security.oauth2.client.registration;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.Before;
+import org.junit.Test;
+import reactor.test.StepVerifier;
 
-import java.util.HashMap;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
-import org.junit.Test;
-
-import reactor.test.StepVerifier;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
  * @author Rob Winch
@@ -107,7 +106,6 @@ public class InMemoryReactiveClientRegistrationRepositoryTests {
 
 	@Test
 	public void iteratorWhenContainsGithubThenContains() {
-		assertThat(this.repository.iterator())
-			.containsOnly(this.registration);
+		assertThat(this.repository).containsOnly(this.registration);
 	}
 }
