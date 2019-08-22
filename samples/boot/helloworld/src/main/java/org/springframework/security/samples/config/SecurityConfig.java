2ddab8b23e24be20202509410c4bf94bf772e741
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2019-08-22 10:06:56 -0400

    Use UserDetailsService bean in sample app
    Fixes: gh-7283

--- samples/boot/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
+++ samples/boot/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -15,12 +15,14 @@
  */
 package org.springframework.security.samples.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 /**
  * @author Joe Grandja
@@ -45,12 +47,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	// @formatter:on
 
-	// @formatter:off
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-			.inMemoryAuthentication()
-				.withUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER"));
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.withDefaultPasswordEncoder()
+				.username("user")
+				.password("password")
+				.roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(userDetails);
 	}
-	// @formatter:on
 }
