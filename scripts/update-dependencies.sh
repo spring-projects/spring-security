12f41f266aa58ae2ed4030299b42f1cd7dff8782
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2019-08-28 13:47:15 -0400

    Dependency update script compatible with mac
    Fixes: gh-7321

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -f build/updates.txt
 ./gradlew dependencyUpdate -Drevision=release
-find -name report.txt | xargs cat > build/updates.txt
+find . -name report.txt | xargs cat > build/updates.txt
 echo "Updates...."
 cat build/updates.txt | fgrep ' ->' | sort | uniq
