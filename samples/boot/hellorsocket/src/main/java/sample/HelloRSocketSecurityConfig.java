03e2efacf488fb97bff545c1aeb95baf6eb79cc1
Rob Winch <rwinch@users.noreply.github.com>
2019-09-30 13:57:52 -0500

    Add Hello RSocket Sample
    Fixes gh-7504

---
+++ samples/boot/hellorsocket/src/main/java/sample/HelloRSocketSecurityConfig.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2002-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sample;
+
+import org.springframework.boot.rsocket.server.ServerRSocketFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.rsocket.core.SecuritySocketAcceptorInterceptor;
+
+/**
+ * @author Rob Winch
+ * @since 5.2
+ */
+@Configuration
+@EnableRSocketSecurity
+public class HelloRSocketSecurityConfig {
+
+	@Bean
+	MapReactiveUserDetailsService userDetailsService() {
+		UserDetails user = User.withDefaultPasswordEncoder()
+			.username("user")
+			.password("password")
+			.roles("SETUP")
+			.build();
+		return new MapReactiveUserDetailsService(user);
+	}
+
+	@Bean
+	ServerRSocketFactoryCustomizer springSecurityRSocketSecurity(
+			SecuritySocketAcceptorInterceptor interceptor) {
+		return builder -> builder.addSocketAcceptorPlugin(interceptor);
+	}
+}
