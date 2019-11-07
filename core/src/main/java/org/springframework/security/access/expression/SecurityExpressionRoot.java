81352abdb56933cfc006f482f214ce5799fe8c0d
LeeHainie <30332409+LeeHainie@users.noreply.github.com>
2019-11-07 15:28:35 +0800

    Remove unused imports
    Remove unused imports

--- core/src/main/java/org/springframework/security/access/expression/SecurityExpressionRoot.java
+++ core/src/main/java/org/springframework/security/access/expression/SecurityExpressionRoot.java
@@ -17,7 +17,6 @@ package org.springframework.security.access.expression;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.security.access.PermissionEvaluator;
