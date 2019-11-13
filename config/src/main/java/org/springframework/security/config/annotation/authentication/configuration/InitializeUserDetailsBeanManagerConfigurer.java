ca8877c8c591c05388cc512749a912382382c877
Adrian Pena <rastadrian@gmail.com>
2019-11-12 22:37:55 -0500

    Updates javadoc for InitializeUserDetailsBeanManagerConfigurer

--- config/src/main/java/org/springframework/security/config/annotation/authentication/configuration/InitializeUserDetailsBeanManagerConfigurer.java
+++ config/src/main/java/org/springframework/security/config/annotation/authentication/configuration/InitializeUserDetailsBeanManagerConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2015 the original author or authors.
+ * Copyright 2002-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -82,7 +82,7 @@ class InitializeUserDetailsBeanManagerConfigurer
 		}
 
 		/**
-		 * @return
+		 * @return a bean of the requested class if there's just a single registered component, null otherwise.
 		 */
 		private <T> T getBeanOrNull(Class<T> type) {
 			String[] userDetailsBeanNames = InitializeUserDetailsBeanManagerConfigurer.this.context
