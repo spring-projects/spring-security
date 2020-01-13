78dedf12f8e396f894b12d8a1a36c92f572a278d
Josh Cummings <josh.cummings@gmail.com>
2020-01-13 16:16:13 -0700

    Add Mapping to Invalid Bearer Token
    Fixes gh-7793

--- core/src/main/java/org/springframework/security/authentication/DefaultAuthenticationEventPublisher.java
+++ core/src/main/java/org/springframework/security/authentication/DefaultAuthenticationEventPublisher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -92,6 +92,9 @@ public class DefaultAuthenticationEventPublisher implements AuthenticationEventP
 		addMapping(
 				"org.springframework.security.authentication.cas.ProxyUntrustedException",
 				AuthenticationFailureProxyUntrustedEvent.class);
+		addMapping(
+				"org.springframework.security.oauth2.server.resource.InvalidBearerTokenException",
+				AuthenticationFailureBadCredentialsEvent.class);
 	}
 
 	public void publishAuthenticationSuccess(Authentication authentication) {
