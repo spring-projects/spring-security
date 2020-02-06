ddc8e562b6e83c15bbe33c64d269ca8f3e5276c2
LeeHainie <30332409+LeeHainie@users.noreply.github.com>
2020-02-06 18:24:28 +0800

    Remove unwanted code
    Remove unwanted code

--- core/src/main/java/org/springframework/security/access/expression/SecurityExpressionRoot.java
+++ core/src/main/java/org/springframework/security/access/expression/SecurityExpressionRoot.java
@@ -17,7 +17,6 @@ package org.springframework.security.access.expression;
 
 import java.io.Serializable;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
 
 import org.springframework.security.access.PermissionEvaluator;
@@ -158,7 +157,6 @@ public abstract class SecurityExpressionRoot implements SecurityExpressionOperat
 
 	private Set<String> getAuthoritySet() {
 		if (roles == null) {
-			roles = new HashSet<>();
 			Collection<? extends GrantedAuthority> userAuthorities = authentication
 					.getAuthorities();
 
