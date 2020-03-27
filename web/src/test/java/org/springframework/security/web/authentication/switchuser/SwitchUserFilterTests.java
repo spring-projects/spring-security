f82e435aaf5695bc9af6afce226251171a77209e
Josh Cummings <josh.cummings@gmail.com>
2020-03-27 15:07:57 -0600

    Add Missing Import
    Restored an import that was lost during a backport
    
    Issue gh-4183

--- web/src/test/java/org/springframework/security/web/authentication/switchuser/SwitchUserFilterTests.java
+++ web/src/test/java/org/springframework/security/web/authentication/switchuser/SwitchUserFilterTests.java
@@ -17,6 +17,7 @@
 package org.springframework.security.web.authentication.switchuser;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import javax.servlet.FilterChain;
 
