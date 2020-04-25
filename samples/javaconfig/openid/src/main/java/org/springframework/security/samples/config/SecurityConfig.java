339d44b5a15a8c6ccccc5dbab408501834768f96
Dávid Kovács <dadikovi@gmail.com>
2020-04-25 15:27:47 +0200

    Deprecate openID 2.0 support
    This commit puts deprecation notice on docs, sample applications and configurations (java and xml)
    
    Fixes gh-7153

--- samples/javaconfig/openid/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
+++ samples/javaconfig/openid/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -20,6 +20,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.samples.security.CustomUserDetailsService;
 
+/**
+ * @deprecated The OpenID 1.0 and 2.0 protocols have been deprecated and users are
+ *  <a href="https://openid.net/specs/openid-connect-migration-1_0.html">encouraged to migrate</a>
+ *  to <a href="https://openid.net/connect/">OpenID Connect</a>, which is supported by <code>spring-security-oauth2</code>.
+ */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// @formatter:off
--- samples/xml/openid/src/main/webapp/index.jsp
+++ samples/xml/openid/src/main/webapp/index.jsp
@@ -6,6 +6,12 @@
 
 <h1>OpenID Sample Home Page</h1>
 
+<p><strong>
+NOTE: The OpenID 1.0 and 2.0 protocols have been deprecated and users are
+<a href="https://openid.net/specs/openid-connect-migration-1_0.html">encouraged to migrate</a>
+to <a href="https://openid.net/connect/">OpenID Connect</a>, which is supported by <code>spring-security-oauth2</code>.
+</strong></p>
+
 <sec:authentication property='principal.newUser' var='isNew' />
 <p>
 Welcome<c:if test="${!isNew}"> back,</c:if> <sec:authentication property='principal.name' />!
