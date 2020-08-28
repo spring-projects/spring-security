9b2ece9dbaf9c4347848c5b87c74385b54c4c9eb
Josh Cummings <josh.cummings@gmail.com>
2020-08-28 15:58:40 -0600

    Remove SAML 2.0 Framework Tests
    Closes gh-8989

--- samples/javaconfig/saml2login/src/test/java/org/springframework/security/samples/config/SecurityConfigTests.java
+++ samples/javaconfig/saml2login/src/test/java/org/springframework/security/samples/config/SecurityConfigTests.java
@@ -16,50 +16,17 @@
 
 package org.springframework.security.samples.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.security.saml2.provider.service.servlet.filter.Saml2WebSsoAuthenticationFilter;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.Arrays;
-import java.util.List;
-import javax.servlet.Filter;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SecurityConfig.class)
 public class SecurityConfigTests {
 
-	@Autowired
-	ApplicationContext context;
-
 	@Test
 	public void securityConfigurationLoads() {
-	}
-
-	@Test
-	public void filterWhenLoginProcessingUrlIsSetInJavaConfigThenTheFilterHasIt() {
-		FilterChainProxy filterChain = context.getBean(FilterChainProxy.class);
-		Assert.assertNotNull(filterChain);
-		final List<Filter> filters = filterChain.getFilters("/sample/jc/saml2/sso/test-id");
-		Assert.assertNotNull(filters);
-		Saml2WebSsoAuthenticationFilter filter = (Saml2WebSsoAuthenticationFilter) filters
-				.stream()
-				.filter(
-						(f) -> f instanceof Saml2WebSsoAuthenticationFilter
-				)
-				.findFirst()
-				.get();
-		for (String field : Arrays.asList("requiresAuthenticationRequestMatcher")) {
-			final Object matcher = ReflectionTestUtils.getField(filter, field);
-			final Object pattern = ReflectionTestUtils.getField(matcher, "pattern");
-			Assert.assertEquals("loginProcessingUrl mismatch", "/sample/jc/saml2/sso/{registrationId}", pattern);
-		}
 	}
 }
