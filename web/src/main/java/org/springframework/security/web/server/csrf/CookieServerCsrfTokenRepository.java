486722e5c0cc2d9c3edf6a48c15adf26bd99c11f
Josh Cummings <josh.cummings@gmail.com>
2020-09-24 13:58:10 -0600

    Revert "Create the CSRF token on the bounded elactic scheduler"
    This reverts commit 5bce912446eec508bb2c2fca1fb139b34809c86e.

--- web/src/main/java/org/springframework/security/web/server/csrf/CookieServerCsrfTokenRepository.java
+++ web/src/main/java/org/springframework/security/web/server/csrf/CookieServerCsrfTokenRepository.java
@@ -27,7 +27,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Schedulers;
 
 /**
  * A {@link ServerCsrfTokenRepository} that persists the CSRF token in a cookie named "XSRF-TOKEN" and
@@ -64,7 +63,7 @@ public final class CookieServerCsrfTokenRepository implements ServerCsrfTokenRep
 
 	@Override
 	public Mono<CsrfToken> generateToken(ServerWebExchange exchange) {
-		return Mono.fromCallable(this::createCsrfToken).subscribeOn(Schedulers.boundedElastic());
+		return Mono.fromCallable(this::createCsrfToken);
 	}
 
 	@Override
