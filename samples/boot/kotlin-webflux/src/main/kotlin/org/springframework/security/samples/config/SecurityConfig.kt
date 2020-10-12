9b66929358d3e59ad2410814dac85ec64ab34d57
Eleftheria Stein <eleftheria.kousathana@gmail.com>
2020-10-12 11:47:40 +0200

    Remove unused import in sample

--- samples/boot/kotlin-webflux/src/main/kotlin/org/springframework/security/samples/config/SecurityConfig.kt
+++ samples/boot/kotlin-webflux/src/main/kotlin/org/springframework/security/samples/config/SecurityConfig.kt
@@ -17,7 +17,6 @@
 package org.springframework.security.samples.config
 
 import org.springframework.context.annotation.Bean
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
