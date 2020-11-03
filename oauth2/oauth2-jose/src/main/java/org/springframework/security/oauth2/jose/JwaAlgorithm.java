0c25b8c1f9d38c943f6c13d73ac43026f67da59d
Joe Grandja <jgrandja@vmware.com>
2020-11-03 12:12:09 -0500

    Introduce JwaAlgorithm
    Closes gh-9182

--- oauth2/oauth2-jose/src/main/java/org/springframework/security/oauth2/jose/JwaAlgorithm.java
+++ oauth2/oauth2-jose/src/main/java/org/springframework/security/oauth2/jose/JwaAlgorithm.java
@@ -14,26 +14,28 @@
  * limitations under the License.
  */
 
-package org.springframework.security.oauth2.jose.jws;
-
-import org.springframework.security.oauth2.jose.JwaAlgorithm;
+package org.springframework.security.oauth2.jose;
 
 /**
  * Super interface for cryptographic algorithms defined by the JSON Web Algorithms (JWA)
  * specification and used by JSON Web Signature (JWS) to digitally sign or create a MAC of
- * the contents of the JWS Protected Header and JWS Payload.
+ * the contents and JSON Web Encryption (JWE) to encrypt the contents.
  *
  * @author Joe Grandja
- * @since 5.2
- * @see JwaAlgorithm
+ * @since 5.5
  * @see <a target="_blank" href="https://tools.ietf.org/html/rfc7518">JSON Web Algorithms
  * (JWA)</a>
  * @see <a target="_blank" href="https://tools.ietf.org/html/rfc7515">JSON Web Signature
  * (JWS)</a>
- * @see <a target="_blank" href=
- * "https://tools.ietf.org/html/rfc7518#section-3">Cryptographic Algorithms for Digital
- * Signatures and MACs</a>
+ * @see <a target="_blank" href="https://tools.ietf.org/html/rfc7516">JSON Web Encryption
+ * (JWE)</a>
  */
-public interface JwsAlgorithm extends JwaAlgorithm {
+public interface JwaAlgorithm {
+
+	/**
+	 * Returns the algorithm name.
+	 * @return the algorithm name
+	 */
+	String getName();
 
 }
