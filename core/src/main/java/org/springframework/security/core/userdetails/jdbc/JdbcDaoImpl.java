d7612e346e8f53e62cbe036b0d7e5a2fad0d2aa4
Angel Aguilera <angel.aguilera@gmail.com>
2020-11-11 12:48:22 +0100

    Fix typo in Javadoc

--- core/src/main/java/org/springframework/security/core/userdetails/jdbc/JdbcDaoImpl.java
+++ core/src/main/java/org/springframework/security/core/userdetails/jdbc/JdbcDaoImpl.java
@@ -38,8 +38,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.util.Assert;
 
 /**
- * <tt>UserDetailsServiceRetrieves</tt> implementation which retrieves the user details
- * (username, password, enabled flag, and authorities) from a database using JDBC queries.
+ * <tt>UserDetailsService</tt> implementation which retrieves the user details (username,
+ * password, enabled flag, and authorities) from a database using JDBC queries.
  *
  * <h3>Default Schema</h3> A default database schema is assumed, with two tables "users"
  * and "authorities".
