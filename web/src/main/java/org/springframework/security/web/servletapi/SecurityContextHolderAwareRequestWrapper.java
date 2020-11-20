ebdb398939b3d9d13666ea1d4423f7a79d4ca2f4
Stephen Joyner <stephen@rundeck.com>
2020-11-20 13:46:40 -0600

    getRemoteUser() returns principal name
    Closes gh-3357

--- web/src/main/java/org/springframework/security/web/servletapi/SecurityContextHolderAwareRequestWrapper.java
+++ web/src/main/java/org/springframework/security/web/servletapi/SecurityContextHolderAwareRequestWrapper.java
@@ -22,6 +22,7 @@ import java.util.Collection;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.core.Authentication;
@@ -105,6 +106,9 @@ public class SecurityContextHolderAwareRequestWrapper extends HttpServletRequest
 		}
 		if (auth.getPrincipal() instanceof UserDetails) {
 			return ((UserDetails) auth.getPrincipal()).getUsername();
+		}
+		if (auth.getPrincipal() instanceof AbstractAuthenticationToken) {
+			return ((AbstractAuthenticationToken) auth.getPrincipal()).getName();
 		}
 		return auth.getPrincipal().toString();
 	}
