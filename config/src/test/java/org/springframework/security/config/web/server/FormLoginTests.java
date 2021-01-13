43a071a89ebec8b1070d17db592dfc0a6a987ccc
Ihor Ilkevych <iilkev@softserveinc.com>
2021-01-13 15:03:28 +0200

    Add WebFlux oauth2Login with formLogin test
    Closes gh-9326

--- config/src/test/java/org/springframework/security/config/web/server/FormLoginTests.java
+++ config/src/test/java/org/springframework/security/config/web/server/FormLoginTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -431,6 +431,11 @@ public class FormLoginTests {
 
 		public DefaultLoginPage assertLoginFormNotPresent() {
 			assertThatExceptionOfType(NoSuchElementException.class).isThrownBy(() -> loginForm().username(""));
+			return this;
+		}
+
+		public DefaultLoginPage assertLoginFormPresent() {
+			loginForm().username("");
 			return this;
 		}
 
