f36e2fca5993dfcf1191babfb44c895be79c6036
Josh Cummings <josh.cummings@gmail.com>
2021-01-15 22:15:35 -0700

    Remove SingleKeyJWSKeySelector
    Closes gh-9348

--- oauth2/oauth2-jose/src/test/java/org/springframework/security/oauth2/jwt/NimbusJwtDecoderTests.java
+++ oauth2/oauth2-jose/src/test/java/org/springframework/security/oauth2/jwt/NimbusJwtDecoderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -503,8 +503,7 @@ public class NimbusJwtDecoderTests {
 				.macAlgorithm(MacAlgorithm.HS512)
 				.build();
 		assertThatExceptionOfType(BadJwtException.class)
-				.isThrownBy(() -> decoder.decode(signedJWT.serialize()))
-				.withMessageContaining("Unsupported algorithm of HS256");
+				.isThrownBy(() -> decoder.decode(signedJWT.serialize()));
 		// @formatter:on
 	}
 
