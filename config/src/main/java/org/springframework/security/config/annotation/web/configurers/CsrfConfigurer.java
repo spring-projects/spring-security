d19ff12813f0e5086eadc60386fcfdc6a655826b
Josh Cummings <josh.cummings@gmail.com>
2021-03-25 10:44:01 -0600

    Publish CsrfTokenRepository as shared object
    Closes gh-9595

--- config/src/main/java/org/springframework/security/config/annotation/web/configurers/CsrfConfigurer.java
+++ config/src/main/java/org/springframework/security/config/annotation/web/configurers/CsrfConfigurer.java
@@ -201,6 +201,7 @@ public final class CsrfConfigurer<H extends HttpSecurityBuilder<H>>
 	@SuppressWarnings("unchecked")
 	@Override
 	public void configure(H http) {
+		http.setSharedObject(CsrfTokenRepository.class, this.csrfTokenRepository);
 		CsrfFilter filter = new CsrfFilter(this.csrfTokenRepository);
 		RequestMatcher requireCsrfProtectionMatcher = getRequireCsrfProtectionMatcher();
 		if (requireCsrfProtectionMatcher != null) {
