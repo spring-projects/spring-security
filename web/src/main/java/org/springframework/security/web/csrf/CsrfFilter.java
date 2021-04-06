8dc702c80f46d678ace50ca464c5ce599ae765bc
佚名 <1694392889@qq.com>
2021-04-06 19:19:39 +0800

    Add null check in CsrfFilter and CsrfWebFilter
    Solve the problem that CsrfFilter and CsrfWebFilter
    throws NPE exception when comparing two byte array
    is equal in low JDK version.
    
    When JDK version is lower than 1.8.0_45, method
    java.security.MessageDigest#isEqual does not verify
    whether the two arrays are null. And the above two
    class call this method without null judgment.
    
    ZiQiang Zhao<1694392889@qq.com>
    
    Closes gh-9561

--- web/src/main/java/org/springframework/security/web/csrf/CsrfFilter.java
+++ web/src/main/java/org/springframework/security/web/csrf/CsrfFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2013 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -174,15 +174,16 @@ public final class CsrfFilter extends OncePerRequestFilter {
 	 * @return
 	 */
 	private static boolean equalsConstantTime(String expected, String actual) {
-		byte[] expectedBytes = bytesUtf8(expected);
-		byte[] actualBytes = bytesUtf8(actual);
+		if (expected == actual) {
+			return true;
+		}
+		if (expected == null || actual == null) {
+			return false;
+		}
+		// Encode after ensure that the string is not null
+		byte[] expectedBytes = Utf8.encode(expected);
+		byte[] actualBytes = Utf8.encode(actual);
 		return MessageDigest.isEqual(expectedBytes, actualBytes);
-	}
-
-	private static byte[] bytesUtf8(String s) {
-		// need to check if Utf8.encode() runs in constant time (probably not).
-		// This may leak length of string.
-		return (s != null) ? Utf8.encode(s) : null;
 	}
 
 	private static final class DefaultRequiresCsrfMatcher implements RequestMatcher {
