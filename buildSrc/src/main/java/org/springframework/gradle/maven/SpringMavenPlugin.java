01bddedb950d0bdc7962e012b94286e8b5da569a
Rob Winch <rwinch@users.noreply.github.com>
2021-05-17 16:21:07 -0500

    apply publishplugin

--- buildSrc/src/main/java/org/springframework/gradle/maven/SpringMavenPlugin.java
+++ buildSrc/src/main/java/org/springframework/gradle/maven/SpringMavenPlugin.java
@@ -4,13 +4,14 @@ import io.spring.gradle.convention.ArtifactoryPlugin;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.PluginManager;
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin;
 
 public class SpringMavenPlugin implements Plugin<Project> {
 	@Override
 	public void apply(Project project) {
 		PluginManager pluginManager = project.getPluginManager();
+		pluginManager.apply(MavenPublishPlugin.class);
 		pluginManager.apply(SpringSigningPlugin.class);
-		pluginManager.apply(SpringMavenPlugin.class);
 		pluginManager.apply(MavenPublishingConventionsPlugin.class);
 		pluginManager.apply(PublishAllJavaComponentsPlugin.class);
 		pluginManager.apply(PublishLocalPlugin.class);
