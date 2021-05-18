be903b8e25f6f2d8735427dbfbc8174c9e8f6038
Eleftheria Stein <estein@vmware.com>
2021-05-18 15:16:28 +0200

    Cleanup unused import

--- config/src/main/kotlin/org/springframework/security/config/web/server/ServerPermissionsPolicyDsl.kt
+++ config/src/main/kotlin/org/springframework/security/config/web/server/ServerPermissionsPolicyDsl.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2020 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,8 +15,6 @@
  */
 
 package org.springframework.security.config.web.server
-
-import org.springframework.security.web.server.header.ReferrerPolicyServerHttpHeadersWriter
 
 /**
  * A Kotlin DSL to configure the [ServerHttpSecurity] permissions policy header using
