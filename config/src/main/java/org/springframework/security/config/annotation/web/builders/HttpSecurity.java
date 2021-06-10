7451fafd3542e424167a006bc7b2fd0dd886c40a
ch4mpy <ch4mp@c4-soft.com>
2021-06-10 11:20:05 -1000

    Fix proposal for gh-9787

--- config/src/main/java/org/springframework/security/config/annotation/web/builders/HttpSecurity.java
+++ config/src/main/java/org/springframework/security/config/annotation/web/builders/HttpSecurity.java
@@ -18,6 +18,7 @@ package org.springframework.security.config.annotation.web.builders;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -96,18 +97,20 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 /**
- * A {@link HttpSecurity} is similar to Spring Security's XML &lt;http&gt; element in the
- * namespace configuration. It allows configuring web based security for specific http
- * requests. By default it will be applied to all requests, but can be restricted using
- * {@link #requestMatcher(RequestMatcher)} or other similar methods.
+ * A {@link HttpSecurity} is similar to Spring Security's XML &lt;http&gt;
+ * element in the namespace configuration. It allows configuring web based
+ * security for specific http requests. By default it will be applied to all
+ * requests, but can be restricted using {@link #requestMatcher(RequestMatcher)}
+ * or other similar methods.
  *
  * <h2>Example Usage</h2>
  *
- * The most basic form based configuration can be seen below. The configuration will
- * require that any URL that is requested will require a User with the role "ROLE_USER".
- * It also defines an in memory authentication scheme with a user that has the username
- * "user", the password "password", and the role "ROLE_USER". For additional examples,
- * refer to the Java Doc of individual methods on {@link HttpSecurity}.
+ * The most basic form based configuration can be seen below. The configuration
+ * will require that any URL that is requested will require a User with the role
+ * "ROLE_USER". It also defines an in memory authentication scheme with a user
+ * that has the username "user", the password "password", and the role
+ * "ROLE_USER". For additional examples, refer to the Java Doc of individual
+ * methods on {@link HttpSecurity}.
  *
  * <pre>
  * &#064;Configuration
@@ -128,6 +131,7 @@ import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
  *
  * @author Rob Winch
  * @author Joe Grandja
+ * @author ch4mpy ch4mp@c4-soft.com
  * @since 3.2
  * @see EnableWebSecurity
  */
@@ -141,6 +145,10 @@ public final class HttpSecurity extends AbstractConfiguredSecurityBuilder<Defaul
 	private RequestMatcher requestMatcher = AnyRequestMatcher.INSTANCE;
 
 	private FilterOrderRegistration filterOrders = new FilterOrderRegistration();
+
+	private Map<Class<? extends Filter>, Class<? extends Filter>> filtersRegistredBefore = new HashMap<>();
+
+	private Map<Class<? extends Filter>, Class<? extends Filter>> filtersRegistredAfter = new HashMap<>();
 
 	/**
 	 * Creates a new instance
@@ -2642,12 +2650,48 @@ public final class HttpSecurity extends AbstractConfiguredSecurityBuilder<Defaul
 
 	@Override
 	public HttpSecurity addFilterAfter(Filter filter, Class<? extends Filter> afterFilter) {
-		return addFilterAtOffsetOf(filter, 1, afterFilter);
+		if (this.filterOrders.getOrder(afterFilter) != null) {
+			this.filtersRegistredAfter.put(filter.getClass(), afterFilter);
+			return addFilterAtOffsetOf(filter, 1, afterFilter);
+
+		}
+		else if (this.filtersRegistredAfter.containsKey(afterFilter)) {
+			final Class<? extends Filter> registeredFilter = this.filtersRegistredAfter.get(afterFilter);
+			this.filtersRegistredAfter.put(filter.getClass(), registeredFilter);
+			return addFilterAtOffsetOf(filter, 1, registeredFilter);
+
+		}
+		else if (this.filtersRegistredBefore.containsKey(afterFilter)) {
+			final Class<? extends Filter> registeredFilter = this.filtersRegistredBefore.get(afterFilter);
+			this.filtersRegistredBefore.put(filter.getClass(), registeredFilter);
+			return addFilterAtOffsetOf(filter, -1, registeredFilter);
+		}
+
+		throw new IllegalArgumentException(afterFilter.getClass().getName()
+				+ " is not registered in filter chain. Consider adding your filter after a registered filter.");
 	}
 
 	@Override
 	public HttpSecurity addFilterBefore(Filter filter, Class<? extends Filter> beforeFilter) {
-		return addFilterAtOffsetOf(filter, -1, beforeFilter);
+		if (this.filterOrders.getOrder(beforeFilter) != null) {
+			this.filtersRegistredBefore.put(filter.getClass(), beforeFilter);
+			return addFilterAtOffsetOf(filter, -1, beforeFilter);
+
+		}
+		else if (this.filtersRegistredAfter.containsKey(beforeFilter)) {
+			final Class<? extends Filter> registeredFilter = this.filtersRegistredAfter.get(beforeFilter);
+			this.filtersRegistredAfter.put(filter.getClass(), registeredFilter);
+			return addFilterAtOffsetOf(filter, -1, registeredFilter);
+
+		}
+		else if (this.filtersRegistredBefore.containsKey(beforeFilter)) {
+			final Class<? extends Filter> registeredFilter = this.filtersRegistredBefore.get(beforeFilter);
+			this.filtersRegistredBefore.put(filter.getClass(), registeredFilter);
+			return addFilterAtOffsetOf(filter, -2, registeredFilter);
+		}
+
+		throw new IllegalArgumentException(beforeFilter.getClass().getName()
+				+ " is not registered in filter chain. Consider adding your filter before a registered filter.");
 	}
 
 	private HttpSecurity addFilterAtOffsetOf(Filter filter, int offset, Class<? extends Filter> registeredFilter) {
