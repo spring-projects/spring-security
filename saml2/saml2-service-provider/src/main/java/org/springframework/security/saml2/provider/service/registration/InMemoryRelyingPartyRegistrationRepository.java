80a6dac76e2ebe6cdc888eec21b222b9ff5cfa14
Russell Allen <allenru@gmail.com>
2021-07-08 18:05:54 -0400

    Fix Assertion
    Closes gh-10055

--- saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/InMemoryRelyingPartyRegistrationRepository.java
+++ saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/InMemoryRelyingPartyRegistrationRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,7 +48,7 @@ public class InMemoryRelyingPartyRegistrationRepository
 		for (RelyingPartyRegistration rp : rps) {
 			Assert.notNull(rp, "relying party collection cannot contain null values");
 			String key = rp.getRegistrationId();
-			Assert.notNull(rp, "relying party identifier cannot be null");
+			Assert.notNull(key, "relying party identifier cannot be null");
 			Assert.isNull(result.get(key), () -> "relying party duplicate identifier '" + key + "' detected.");
 			result.put(key, rp);
 		}
