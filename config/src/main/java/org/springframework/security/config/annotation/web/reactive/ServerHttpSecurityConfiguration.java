869a783900b50b3d612d40fc42d4447c91f1e3fb
Eleftheria Stein <estein@vmware.com>
2021-08-11 13:26:47 +0200

    Make method static to prevent circular dependency error
    Workaround for circular dependency between ServerHttpSecurityConfiguration and WebFluxConfigurationSupport.
    
    Closes gh-10076

--- config/src/main/java/org/springframework/security/config/annotation/web/reactive/ServerHttpSecurityConfiguration.java
+++ config/src/main/java/org/springframework/security/config/annotation/web/reactive/ServerHttpSecurityConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,7 +89,7 @@ class ServerHttpSecurityConfiguration {
 	}
 
 	@Bean
-	WebFluxConfigurer authenticationPrincipalArgumentResolverConfigurer(
+	static WebFluxConfigurer authenticationPrincipalArgumentResolverConfigurer(
 			ObjectProvider<AuthenticationPrincipalArgumentResolver> authenticationPrincipalArgumentResolver) {
 		return new WebFluxConfigurer() {
 
