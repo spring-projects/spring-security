dd43d9198b5df655b41ceb7d00abb0ad920f7bfc
Ashley Scopes <73482956+ascopes@users.noreply.github.com>
2021-08-15 14:38:13 +0100

    Amended treatment of OAuth2 'iss' claim
    Prior to this commit, the OAuth2 resource server code is failing any issuer
    that is not a valid URL. This does not correspond to
    https://datatracker.ietf.org/doc/html/rfc7662#page-7 which redirects to
    https://datatracker.ietf.org/doc/html/rfc7519#section-4.1.1, defining an
    issuer as being a "StringOrURI", which is defined at
    https://datatracker.ietf.org/doc/html/rfc7519#page-5 as being
    an "arbitrary string value" that "MUST be a URI" only for
    "any value containing a ':'".
    
    The issue currently is that an issuer that is not a valid URL may be
    provided, which will automatically result in the request being aborted
    due to being invalid.
    
    I have removed the check entirely, since while the claim could be invalid,
    it is still a response that the OAuth2 introspection endpoint has provided.
    In the liklihood that interpretations of this behaviour are different for
    the OAuth2 server implementation in use, this currently stops Spring
    Security from being able to be used at all without implementing a custom
    introspector from scratch.
    
    It is also worth noting that the spec does not specify whether it is
    valid to normalize issuers or not if they are valid URLs. This may cause
    other unintended side effects as a result of this change, so it is
    safer to disable it entirely.

--- oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/introspection/SpringOpaqueTokenIntrospector.java
+++ oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/introspection/SpringOpaqueTokenIntrospector.java
@@ -17,7 +17,6 @@
 package org.springframework.security.oauth2.server.resource.introspection;
 
 import java.net.URI;
-import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -187,7 +186,25 @@ public class SpringOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
 				(k, v) -> Instant.ofEpochSecond(((Number) v).longValue()));
 		claims.computeIfPresent(OAuth2TokenIntrospectionClaimNames.IAT,
 				(k, v) -> Instant.ofEpochSecond(((Number) v).longValue()));
-		claims.computeIfPresent(OAuth2TokenIntrospectionClaimNames.ISS, (k, v) -> issuer(v.toString()));
+		// RFC-7662 page 7 directs users to RFC-7519 for defining the values of these
+		// issuer fields.
+		// https://datatracker.ietf.org/doc/html/rfc7662#page-7
+		//
+		// RFC-7519 page 9 defines issuer fields as being 'case-sensitive' strings
+		// containing
+		// a 'StringOrURI', which is defined on page 5 as being any string, but strings
+		// containing ':'
+		// should be treated as valid URIs.
+		// https://datatracker.ietf.org/doc/html/rfc7519#section-2
+		//
+		// It is not defined however as to whether-or-not normalized URIs should be
+		// treated as the same literal
+		// value. It only defines validation itself, so to avoid potential ambiguity or
+		// unwanted side effects that
+		// may be awkward to debug, we do not want to manipulate this value. Previous
+		// versions of Spring Security
+		// would *only* allow valid URLs, which is not what we wish to achieve here.
+		claims.computeIfPresent(OAuth2TokenIntrospectionClaimNames.ISS, (k, v) -> v.toString());
 		claims.computeIfPresent(OAuth2TokenIntrospectionClaimNames.NBF,
 				(k, v) -> Instant.ofEpochSecond(((Number) v).longValue()));
 		Collection<GrantedAuthority> authorities = new ArrayList<>();
@@ -202,16 +219,6 @@ public class SpringOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
 			return v;
 		});
 		return new OAuth2IntrospectionAuthenticatedPrincipal(claims, authorities);
-	}
-
-	private URL issuer(String uri) {
-		try {
-			return new URL(uri);
-		}
-		catch (Exception ex) {
-			throw new OAuth2IntrospectionException(
-					"Invalid " + OAuth2TokenIntrospectionClaimNames.ISS + " value: " + uri);
-		}
 	}
 
 }
