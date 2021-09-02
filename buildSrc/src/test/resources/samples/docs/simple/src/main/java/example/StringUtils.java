be9ff16583e3b171992490df1858d64e99e36d64
Rob Winch <rwinch@users.noreply.github.com>
2021-09-02 14:38:19 -0500

    Remove Asciidoctor from the build in favor of Antora

--- buildSrc/src/test/resources/samples/docs/simple/src/main/java/example/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package example;
-
-public class StringUtils {
-    // tag::contains[]
-    public boolean contains(String haystack, String needle) {
-        return haystack.contains(needle);
-    }
-    // end::contains[]
-}
