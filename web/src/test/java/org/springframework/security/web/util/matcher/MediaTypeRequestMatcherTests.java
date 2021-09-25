84d173c3103342109868e58b21d43ff53ea42c51
heowc <heowc1992@gmail.com>
2021-09-25 16:25:05 +0900

    Fix typo

--- web/src/test/java/org/springframework/security/web/util/matcher/MediaTypeRequestMatcherTests.java
+++ web/src/test/java/org/springframework/security/web/util/matcher/MediaTypeRequestMatcherTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -81,7 +81,7 @@ public class MediaTypeRequestMatcherTests {
 	}
 
 	@Test
-	public void constructorEmtpyMediaTypes() {
+	public void constructorEmptyMediaTypes() {
 		assertThatIllegalArgumentException().isThrownBy(
 				() -> new MediaTypeRequestMatcher(this.negotiationStrategy, Collections.<MediaType>emptyList()));
 	}
