4c2401a576b7885f8b3f7b9abe53505b8e7ede87
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-02-02 10:09:45 -0300

    Revert "Make source code compatible with JDK 8"
    This reverts commit 60ed3602f6281d1a34c643484dfcb3440e2243d5.

--- config/src/main/java/org/springframework/security/config/annotation/web/configurers/saml2/Saml2LoginConfigurer.java
+++ config/src/main/java/org/springframework/security/config/annotation/web/configurers/saml2/Saml2LoginConfigurer.java
@@ -359,7 +359,8 @@ public final class Saml2LoginConfigurer<B extends HttpSecurityBuilder<B>>
 		if (version != null) {
 			return version;
 		}
-		return Version.getVersion();
+		return Version.class.getModule().getDescriptor().version().map(Object::toString)
+				.orElseThrow(() -> new IllegalStateException("cannot determine OpenSAML version"));
 	}
 
 	private void registerDefaultAuthenticationProvider(B http) {
