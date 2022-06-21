5357cb8c954bbf35ae167d0311b82547545c3068
Josh Cummings <josh.cummings@gmail.com>
2022-06-21 16:47:39 -0600

    Use SecurityContextHolderStrategy for NullSecurityContextRepository
    Issue gh-11060

--- web/src/main/java/org/springframework/security/web/context/NullSecurityContextRepository.java
+++ web/src/main/java/org/springframework/security/web/context/NullSecurityContextRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,6 +22,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.context.SecurityContextHolderStrategy;
+import org.springframework.util.Assert;
 
 /**
  * @author Luke Taylor
@@ -53,6 +54,7 @@ public final class NullSecurityContextRepository implements SecurityContextRepos
 	 * @since 5.8
 	 */
 	public void setSecurityContextHolderStrategy(SecurityContextHolderStrategy strategy) {
+		Assert.notNull(this.securityContextHolderStrategy, "securityContextHolderStrategy cannot be null");
 		this.securityContextHolderStrategy = strategy;
 	}
 
