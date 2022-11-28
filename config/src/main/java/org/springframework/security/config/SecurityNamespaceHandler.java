e6173f9e5b552bb72939930de819e7cea7300770
Marcus Da Coregio <marcusdacoregio@gmail.com>
2022-11-28 15:47:10 -0300

    Prepare for Spring Security 6.1

--- config/src/main/java/org/springframework/security/config/SecurityNamespaceHandler.java
+++ config/src/main/java/org/springframework/security/config/SecurityNamespaceHandler.java
@@ -95,7 +95,7 @@ public final class SecurityNamespaceHandler implements NamespaceHandler {
 		if (!namespaceMatchesVersion(element)) {
 			pc.getReaderContext().fatal("You cannot use a spring-security-2.0.xsd or spring-security-3.0.xsd or "
 					+ "spring-security-3.1.xsd schema or spring-security-3.2.xsd schema or spring-security-4.0.xsd schema "
-					+ "with Spring Security 6.0. Please update your schema declarations to the 6.0 schema.", element);
+					+ "with Spring Security 6.1. Please update your schema declarations to the 6.1 schema.", element);
 		}
 		String name = pc.getDelegate().getLocalName(element);
 		BeanDefinitionParser parser = this.parsers.get(name);
@@ -218,7 +218,7 @@ public final class SecurityNamespaceHandler implements NamespaceHandler {
 
 	private boolean matchesVersionInternal(Element element) {
 		String schemaLocation = element.getAttributeNS("http://www.w3.org/2001/XMLSchema-instance", "schemaLocation");
-		return schemaLocation.matches("(?m).*spring-security-6\\.0.*.xsd.*")
+		return schemaLocation.matches("(?m).*spring-security-6\\.1.*.xsd.*")
 				|| schemaLocation.matches("(?m).*spring-security.xsd.*")
 				|| !schemaLocation.matches("(?m).*spring-security.*");
 	}
