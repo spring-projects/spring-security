1bbbd046c33a24272bf30ab3b13e5f5f2fa6b3a1
Evgeniy Cheban <mister.cheban@gmail.com>
2023-01-04 20:52:46 +0100

    Polish gh-12231
    - Update copyright header
    - Use Set.of instead of HashSet in AuthorityAuthorizationManager
    - Align roleHierarchy test name with other tests in AuthoritiesAuthorizationManagerTests

--- core/src/main/java/org/springframework/security/authorization/AuthorityAuthorizationManager.java
+++ core/src/main/java/org/springframework/security/authorization/AuthorityAuthorizationManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2022 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,8 +16,6 @@
 
 package org.springframework.security.authorization;
 
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.function.Supplier;
 
@@ -43,7 +41,7 @@ public final class AuthorityAuthorizationManager<T> implements AuthorizationMana
 	private final Set<String> authorities;
 
 	private AuthorityAuthorizationManager(String... authorities) {
-		this.authorities = new HashSet<>(Arrays.asList(authorities));
+		this.authorities = Set.of(authorities);
 	}
 
 	/**
