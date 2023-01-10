5b6b3d585fdccd5d774067f69c6e59b25f4600b5
Josh Cummings <josh.cummings@gmail.com>
2023-01-10 08:30:52 -0700

    Change EnableReactiveMethodSecurity Defaults
    Closes gh-12506

--- config/src/main/java/org/springframework/security/config/annotation/method/configuration/EnableReactiveMethodSecurity.java
+++ config/src/main/java/org/springframework/security/config/annotation/method/configuration/EnableReactiveMethodSecurity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 the original author or authors.
+ * Copyright 2002-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -73,6 +73,6 @@ public @interface EnableReactiveMethodSecurity {
 	 * used.
 	 * @since 5.8
 	 */
-	boolean useAuthorizationManager() default false;
+	boolean useAuthorizationManager() default true;
 
 }
