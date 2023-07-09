ea19f82b8a526d920ff1e1c2830d5e99d8124472
Seongguk Jeong <seongguk.dev@gmail.com>
2023-07-09 23:53:53 +0900

    Using pattern matching for instanceof

--- core/src/main/java/org/springframework/security/authentication/AbstractAuthenticationToken.java
+++ core/src/main/java/org/springframework/security/authentication/AbstractAuthenticationToken.java
@@ -68,14 +68,14 @@ public abstract class AbstractAuthenticationToken implements Authentication, Cre
 
 	@Override
 	public String getName() {
-		if (this.getPrincipal() instanceof UserDetails) {
-			return ((UserDetails) this.getPrincipal()).getUsername();
+		if (this.getPrincipal() instanceof UserDetails userDetails) {
+			return userDetails.getUsername();
 		}
-		if (this.getPrincipal() instanceof AuthenticatedPrincipal) {
-			return ((AuthenticatedPrincipal) this.getPrincipal()).getName();
+		if (this.getPrincipal() instanceof AuthenticatedPrincipal authenticatedPrincipal) {
+			return authenticatedPrincipal.getName();
 		}
-		if (this.getPrincipal() instanceof Principal) {
-			return ((Principal) this.getPrincipal()).getName();
+		if (this.getPrincipal() instanceof Principal principal) {
+			return principal.getName();
 		}
 		return (this.getPrincipal() == null) ? "" : this.getPrincipal().toString();
 	}
@@ -119,10 +119,9 @@ public abstract class AbstractAuthenticationToken implements Authentication, Cre
 
 	@Override
 	public boolean equals(Object obj) {
-		if (!(obj instanceof AbstractAuthenticationToken)) {
+		if (!(obj instanceof AbstractAuthenticationToken test)) {
 			return false;
 		}
-		AbstractAuthenticationToken test = (AbstractAuthenticationToken) obj;
 		if (!this.authorities.equals(test.authorities)) {
 			return false;
 		}
