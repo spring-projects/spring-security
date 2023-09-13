e1db108cb9cb6c957271c8693435baec2c42cc30
Mario Petrovski <marivce12@gmail.com>
2023-09-13 14:41:07 +0200

    Improvements and unit tests.

---
+++ oauth2/oauth2-core/src/test/java/org/springframework/security/oauth2/core/OAuth2AuthorizationManagersTests.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2002-2023 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.oauth2.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Tests for {@link OAuth2AuthorizationManagers}
+ *
+ * @author Mario Petrovski
+ */
+public class OAuth2AuthorizationManagersTests {
+
+	@Test
+	void hasScope_withInvalidScope_shouldThrowIllegalArgumentException() {
+		String scope = "SCOPE_invalid";
+		assertThrows(IllegalArgumentException.class, () -> OAuth2AuthorizationManagers.hasScope(scope));
+	}
+
+	@Test
+	void hasScopes_withInvalidScope_shouldThrowIllegalArgumentException() {
+		String[] scopes = { "read", "write", "SCOPE_invalid" };
+		assertThrows(IllegalArgumentException.class, () -> OAuth2AuthorizationManagers.hasAnyScope(scopes));
+	}
+
+	@Test
+	void hasScope_withValidScope_shouldPass() {
+		String scope = "read";
+		AuthorityAuthorizationManager<Object> authorizationManager = OAuth2AuthorizationManagers.hasScope(scope);
+		assertNotNull(authorizationManager);
+	}
+
+	@Test
+	void hasScope_withValidScopes_shouldPass() {
+		String[] scopes = { "read", "write" };
+		AuthorityAuthorizationManager<Object> authorizationManager = OAuth2AuthorizationManagers.hasAnyScope(scopes);
+		assertNotNull(authorizationManager);
+	}
+
+}
