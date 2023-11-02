14f61d3f5a0be9b857694040dfe5a872001ba45a
chenhui <gaischen@163.com>
2023-11-02 20:41:27 +0800

    fix #14089 Bugs occur when roles and authorities are built at the same time

--- core/src/main/java/org/springframework/security/core/userdetails/User.java
+++ core/src/main/java/org/springframework/security/core/userdetails/User.java
@@ -441,7 +441,7 @@ public class User implements UserDetails, CredentialsContainer {
 		 */
 		public UserBuilder authorities(Collection<? extends GrantedAuthority> authorities) {
 			Assert.notNull(authorities, "authorities cannot be null");
-			this.authorities = new ArrayList<>(authorities);
+			this.authorities.addAll(authorities);
 			return this;
 		}
 
