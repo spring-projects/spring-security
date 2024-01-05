2b7d2969945b899a51d241575ddae2886d433e21
Sam Brannen <104798+sbrannen@users.noreply.github.com>
2024-01-05 11:56:03 +0100

    Revise AuthorizationAnnotationUtils
    This commit revises AuthorizationAnnotationUtils as follows.
    
    - Removes code duplication by treating both Class and Method as
      AnnotatedElement.
    
    - Avoids duplicated annotation searches by processing merged
      annotations in a single Stream instead of first using the
      MergedAnnotations API to find possible duplicates and then again
      searching for a single annotation via AnnotationUtils (which
      effectively performs the same search using the MergedAnnotations API
      internally).
    
    - Uses `.distinct()` within the Stream to avoid the need for the
      workaround introduced in gh-13625. Note that the semantics here
      result in duplicate "equivalent" annotations being ignored. In other
      words, if @⁠PreAuthorize("hasRole('someRole')") is present multiple
      times as a meta-annotation, no exception will be thrown and the first
      such annotation found will be used.
    
    - Improves the error message when competing annotations are found by
      including the competing annotations in the error message.
    
    - Updates AuthorizationAnnotationUtilsTests to cover all known,
      supported use cases.
    
    - Configures correct role in @⁠RequireUserRole.
    
    Please note this commit uses
    `.map(MergedAnnotation::withNonMergedAttributes)` to retain backward
    compatibility with previous versions of Spring Security. However, that
    line can be deleted if the Spring Security team decides that it wishes
    to support merged annotation attributes via custom composed
    annotations. If that decision is made, the
    composedMergedAnnotationsAreNotSupported() test should be renamed and
    updated as explained in the comment in that method.
    
    See gh-13625
    See https://github.com/spring-projects/spring-framework/issues/31803

--- core/src/test/java/org/springframework/security/access/annotation/RequireUserRole.java
+++ core/src/test/java/org/springframework/security/access/annotation/RequireUserRole.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 
 @Retention(RetentionPolicy.RUNTIME)
 @PreAuthorize("hasRole('USER')")
-@RolesAllowed("ADMIN")
+@RolesAllowed("USER")
 @Secured("USER")
 public @interface RequireUserRole {
 
