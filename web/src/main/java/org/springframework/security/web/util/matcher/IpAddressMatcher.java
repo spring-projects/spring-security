a808c139add8ae5d99acac381daf7eee439282a2
ahmd-nabil <ahm3dnabil99@gmail.com>
2024-01-27 01:02:11 +0200

    Enhance IpAddressMatcher performance
    Closes gh-14493
    
    Signed-off-by: ahmd-nabil <ahm3dnabil99@gmail.com>

--- web/src/main/java/org/springframework/security/web/util/matcher/IpAddressMatcher.java
+++ web/src/main/java/org/springframework/security/web/util/matcher/IpAddressMatcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,8 +56,9 @@ public final class IpAddressMatcher implements RequestMatcher {
 			this.nMaskBits = -1;
 		}
 		this.requiredAddress = parseAddress(ipAddress);
-		Assert.isTrue(this.requiredAddress.getAddress().length * 8 >= this.nMaskBits,
-				String.format("IP address %s is too short for bitmask of length %d", ipAddress, this.nMaskBits));
+		String finalIpAddress = ipAddress;
+		Assert.isTrue(this.requiredAddress.getAddress().length * 8 >= this.nMaskBits, () -> String
+			.format("IP address %s is too short for bitmask of length %d", finalIpAddress, this.nMaskBits));
 	}
 
 	@Override
