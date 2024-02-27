b69939c1e55c9dd477e55e91f6d783ae720c18ec
ch4mpy <ch4mp@c4-soft.com>
2024-02-27 03:51:59 -1000

    Getters for OAuth2AuthorizedClientId properties
    Expose getters for principalName and clientRegistrationId which compose an OAuth2AuthorizedClientId

--- oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/OAuth2AuthorizedClientId.java
+++ oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/OAuth2AuthorizedClientId.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2019 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,22 @@ public final class OAuth2AuthorizedClientId implements Serializable {
 		Assert.hasText(principalName, "principalName cannot be empty");
 		this.clientRegistrationId = clientRegistrationId;
 		this.principalName = principalName;
+	}
+
+	/**
+	 * @return the registration-id part of this authorized client ID
+	 * @since 6.2.3
+	 */
+	public String getClientRegistrationId() {
+		return this.clientRegistrationId;
+	}
+
+	/**
+	 * @return the principalName part of this authorized client ID
+	 * @since 6.2.3
+	 */
+	public String getPrincipalName() {
+		return this.principalName;
 	}
 
 	@Override
