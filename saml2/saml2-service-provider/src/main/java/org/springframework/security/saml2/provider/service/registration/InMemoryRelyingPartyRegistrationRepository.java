1e2900328b05b6164d29aa1148096cf7a651a3db
Josh Cummings <josh.cummings@gmail.com>
2024-07-01 17:20:35 -0600

    Add IterableRelyingPartyRegistrationRepository
    Closes gh-15027

--- saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/InMemoryRelyingPartyRegistrationRepository.java
+++ saml2/saml2-service-provider/src/main/java/org/springframework/security/saml2/provider/service/registration/InMemoryRelyingPartyRegistrationRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,8 +36,7 @@ import org.springframework.util.MultiValueMap;
  * @author Josh Cummings
  * @since 5.2
  */
-public class InMemoryRelyingPartyRegistrationRepository
-		implements RelyingPartyRegistrationRepository, Iterable<RelyingPartyRegistration> {
+public class InMemoryRelyingPartyRegistrationRepository implements IterableRelyingPartyRegistrationRepository {
 
 	private final Map<String, RelyingPartyRegistration> byRegistrationId;
 
