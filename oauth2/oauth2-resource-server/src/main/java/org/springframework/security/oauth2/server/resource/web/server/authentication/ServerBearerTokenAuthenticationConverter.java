afa19eb040d3591e7b0e5d998da7a072f1605a3e
Steve Riesenberg <5248162+sjohnr@users.noreply.github.com>
2024-11-04 13:08:59 -0600

    Polish gh-15940

--- oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/web/server/authentication/ServerBearerTokenAuthenticationConverter.java
+++ oauth2/oauth2-resource-server/src/main/java/org/springframework/security/oauth2/server/resource/web/server/authentication/ServerBearerTokenAuthenticationConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -79,7 +79,7 @@ public class ServerBearerTokenAuthenticationConverter implements ServerAuthentic
 		}
 		if (parameterToken != null && isParameterTokenSupportedForRequest(request)) {
 			if (!StringUtils.hasText(parameterToken)) {
-				final BearerTokenError error = BearerTokenErrors
+				BearerTokenError error = BearerTokenErrors
 					.invalidRequest("The requested token parameter is an empty string");
 				throw new OAuth2AuthenticationException(error);
 			}
