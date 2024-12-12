7e83fd54aaa993db64e3fcbbde6a39dc90907e3a
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2024-12-12 12:24:28 -0700

    Add Test Saml 2.0 Test Objects

---
+++ saml2/saml2-service-provider/src/test/java/org/springframework/security/saml2/provider/service/authentication/TestSaml2LogoutRequests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2002-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.saml2.provider.service.authentication;
+
+import org.springframework.security.saml2.provider.service.authentication.logout.Saml2LogoutRequest;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.Saml2MessageBinding;
+import org.springframework.security.saml2.provider.service.registration.TestRelyingPartyRegistrations;
+
+public final class TestSaml2LogoutRequests {
+
+	private TestSaml2LogoutRequests() {
+
+	}
+
+	public static Saml2LogoutRequest create() {
+		RelyingPartyRegistration registration = TestRelyingPartyRegistrations.relyingPartyRegistration().build();
+		return Saml2LogoutRequest.withRelyingPartyRegistration(registration)
+			.samlRequest("samlRequest")
+			.binding(Saml2MessageBinding.POST)
+			.location("location")
+			.relayState("relayState")
+			.id("id")
+			.build();
+	}
+
+}
