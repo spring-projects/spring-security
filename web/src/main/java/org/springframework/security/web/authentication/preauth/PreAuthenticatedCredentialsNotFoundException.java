27c2a8ad11afa3ac9ae88a67d03d32ec6b3c1e9d
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2024-12-17 13:05:23 -0700

    Add Serializable Compatibility to Web Authentication Exceptions
    Issue gh-16276

--- web/src/main/java/org/springframework/security/web/authentication/preauth/PreAuthenticatedCredentialsNotFoundException.java
+++ web/src/main/java/org/springframework/security/web/authentication/preauth/PreAuthenticatedCredentialsNotFoundException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2016 the original author or authors.
+ * Copyright 2002-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,14 @@
 
 package org.springframework.security.web.authentication.preauth;
 
+import java.io.Serial;
+
 import org.springframework.security.core.AuthenticationException;
 
 public class PreAuthenticatedCredentialsNotFoundException extends AuthenticationException {
+
+	@Serial
+	private static final long serialVersionUID = 2026209817833032728L;
 
 	public PreAuthenticatedCredentialsNotFoundException(String msg) {
 		super(msg);
