7838079daa5d1193400969c5c0b9120996920e08
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2025-01-18 16:26:40 +0700

    Implement Serializable for PublicKeyCredentialUserEntity
    Closes gh-16273
    
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- web/src/main/java/org/springframework/security/web/webauthn/api/ImmutablePublicKeyCredentialUserEntity.java
+++ web/src/main/java/org/springframework/security/web/webauthn/api/ImmutablePublicKeyCredentialUserEntity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,8 @@ package org.springframework.security.web.webauthn.api;
  * @since 6.4
  */
 public final class ImmutablePublicKeyCredentialUserEntity implements PublicKeyCredentialUserEntity {
+
+	private static final long serialVersionUID = -3438693960347279759L;
 
 	/**
 	 * When inherited by PublicKeyCredentialUserEntity, it is a human-palatable identifier
