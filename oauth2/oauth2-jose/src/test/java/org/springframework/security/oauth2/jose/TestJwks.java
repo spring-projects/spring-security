d799c132e62374b07573af62825950e3ed2e0465
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-02-14 09:35:21 -0700

    Polish setJwkSelector
    Make so that it runs only when selection is needed.
    Require the provided selector be non-null.
    Add Tests.
    
    Issue gh-16170

--- oauth2/oauth2-jose/src/test/java/org/springframework/security/oauth2/jose/TestJwks.java
+++ oauth2/oauth2-jose/src/test/java/org/springframework/security/oauth2/jose/TestJwks.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,6 +57,10 @@ public final class TestJwks {
 	// @formatter:on
 
 	private TestJwks() {
+	}
+
+	public static RSAKey.Builder rsa() {
+		return jwk(TestKeys.DEFAULT_PUBLIC_KEY, TestKeys.DEFAULT_PRIVATE_KEY);
 	}
 
 	public static RSAKey.Builder jwk(RSAPublicKey publicKey, RSAPrivateKey privateKey) {
