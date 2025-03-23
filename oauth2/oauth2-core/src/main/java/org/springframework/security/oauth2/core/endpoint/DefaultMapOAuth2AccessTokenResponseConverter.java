2dc57718ed32879e1a2e2dd13712ecfe0afe29eb
hammadirshad <hammadirshad046@gmail.com>
2025-03-23 14:34:01 +0100

    Added a mapping for DPOP in DefaultMapOAuth2AccessTokenResponseConverter
    Signed-off-by: muha <muha@kreftregisteret.no>

--- oauth2/oauth2-core/src/main/java/org/springframework/security/oauth2/core/endpoint/DefaultMapOAuth2AccessTokenResponseConverter.java
+++ oauth2/oauth2-core/src/main/java/org/springframework/security/oauth2/core/endpoint/DefaultMapOAuth2AccessTokenResponseConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2021 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,6 +69,10 @@ public final class DefaultMapOAuth2AccessTokenResponseConverter
 		if (OAuth2AccessToken.TokenType.BEARER.getValue()
 			.equalsIgnoreCase(getParameterValue(tokenResponseParameters, OAuth2ParameterNames.TOKEN_TYPE))) {
 			return OAuth2AccessToken.TokenType.BEARER;
+		}
+		else if (OAuth2AccessToken.TokenType.DPOP.getValue()
+				.equalsIgnoreCase(getParameterValue(tokenResponseParameters, OAuth2ParameterNames.TOKEN_TYPE))) {
+			return OAuth2AccessToken.TokenType.DPOP;
 		}
 		return null;
 	}
