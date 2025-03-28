9a897d0b62ceed60d38f367ebc79753cae04b81d
Max Batischev <mblancer@mail.ru>
2025-03-28 13:10:35 +0300

    Add Support Postgres To JdbcUserCredentialRepository
    Closes gh-16832
    
    Signed-off-by: Max Batischev <mblancer@mail.ru>

--- web/src/main/java/org/springframework/security/web/aot/hint/UserCredentialRuntimeHints.java
+++ web/src/main/java/org/springframework/security/web/aot/hint/UserCredentialRuntimeHints.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,9 @@ class UserCredentialRuntimeHints implements RuntimeHintsRegistrar {
 
 	@Override
 	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
-		hints.resources().registerPattern("org/springframework/security/user-credentials-schema.sql");
+		hints.resources()
+			.registerPattern("org/springframework/security/user-credentials-schema.sql")
+			.registerPattern("org/springframework/security/user-credentials-schema-postgres.sql");
 	}
 
 }
--- web/src/test/java/org/springframework/security/web/aot/hint/UserCredentialRuntimeHintsTests.java
+++ web/src/test/java/org/springframework/security/web/aot/hint/UserCredentialRuntimeHintsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -53,7 +53,8 @@ public class UserCredentialRuntimeHintsTests {
 	}
 
 	private static Stream<String> getClientRecordsSqlFiles() {
-		return Stream.of("org/springframework/security/user-credentials-schema.sql");
+		return Stream.of("org/springframework/security/user-credentials-schema.sql",
+				"org/springframework/security/user-credentials-schema-postgres.sql");
 	}
 
 }
