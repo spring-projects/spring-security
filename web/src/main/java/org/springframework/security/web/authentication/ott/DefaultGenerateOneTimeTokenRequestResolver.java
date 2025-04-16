9dcce6162eaf9489ebb1fc66acf3fb5c785577b8
Max Batischev <mblancer@mail.ru>
2025-04-16 15:34:13 +0300

    Add support OneTimeToken customization
    Closes gh-16939
    
    Signed-off-by: Max Batischev <mblancer@mail.ru>

--- web/src/main/java/org/springframework/security/web/authentication/ott/DefaultGenerateOneTimeTokenRequestResolver.java
+++ web/src/main/java/org/springframework/security/web/authentication/ott/DefaultGenerateOneTimeTokenRequestResolver.java
@@ -17,6 +17,8 @@
 package org.springframework.security.web.authentication.ott;
 
 import java.time.Duration;
+import java.util.UUID;
+import java.util.function.Supplier;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -37,13 +39,15 @@ public final class DefaultGenerateOneTimeTokenRequestResolver implements Generat
 
 	private Duration expiresIn = DEFAULT_EXPIRES_IN;
 
+	private Supplier<String> tokenValueFactory = () -> UUID.randomUUID().toString();
+
 	@Override
 	public GenerateOneTimeTokenRequest resolve(HttpServletRequest request) {
 		String username = request.getParameter("username");
 		if (!StringUtils.hasText(username)) {
 			return null;
 		}
-		return new GenerateOneTimeTokenRequest(username, this.expiresIn);
+		return new GenerateOneTimeTokenRequest(username, this.expiresIn, this.tokenValueFactory.get());
 	}
 
 	/**
@@ -53,6 +57,16 @@ public final class DefaultGenerateOneTimeTokenRequestResolver implements Generat
 	public void setExpiresIn(Duration expiresIn) {
 		Assert.notNull(expiresIn, "expiresIn cannot be null");
 		this.expiresIn = expiresIn;
+	}
+
+	/**
+	 * Sets factory for token value generation
+	 * @param tokenValueFactory factory for token value generation
+	 * @since 6.5
+	 */
+	public void setTokenValueFactory(Supplier<String> tokenValueFactory) {
+		Assert.notNull(tokenValueFactory, "tokenValueFactory cannot be null");
+		this.tokenValueFactory = tokenValueFactory;
 	}
 
 }
