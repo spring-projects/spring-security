1172581703acfc3bd85208d4dd73cb9a74fc49aa
Evgeniy Cheban <mister.cheban@gmail.com>
2025-04-24 23:54:37 +0300

    ReactiveAuthorizationManager replace deprecated #check calls with #authorize
    Closes gh-16936
    
    Signed-off-by: Evgeniy Cheban <mister.cheban@gmail.com>

--- core/src/main/java/org/springframework/security/authorization/ReactiveAuthorizationManager.java
+++ core/src/main/java/org/springframework/security/authorization/ReactiveAuthorizationManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -50,8 +50,8 @@ public interface ReactiveAuthorizationManager<T> {
 	 */
 	default Mono<Void> verify(Mono<Authentication> authentication, T object) {
 		// @formatter:off
-		return check(authentication, object)
-				.filter(AuthorizationDecision::isGranted)
+		return authorize(authentication, object)
+				.filter(AuthorizationResult::isGranted)
 				.switchIfEmpty(Mono.defer(() -> Mono.error(new AccessDeniedException("Access Denied"))))
 				.flatMap((decision) -> Mono.empty());
 		// @formatter:on
