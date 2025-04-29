c972de5369a1261ab674a3f5e3a80e8ce3e8cdfb
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-04-29 15:50:08 -0600

    Use .equals to Compare Methods
    Closes gh-17143

--- core/src/main/java/org/springframework/security/core/annotation/UniqueSecurityAnnotationScanner.java
+++ core/src/main/java/org/springframework/security/core/annotation/UniqueSecurityAnnotationScanner.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -267,7 +267,7 @@ final class UniqueSecurityAnnotationScanner<A extends Annotation> extends Abstra
 
 	private static Method findMethod(Method method, Class<?> targetClass) {
 		for (Method candidate : targetClass.getDeclaredMethods()) {
-			if (candidate == method) {
+			if (candidate.equals(method)) {
 				return candidate;
 			}
 			if (isOverride(method, candidate)) {
