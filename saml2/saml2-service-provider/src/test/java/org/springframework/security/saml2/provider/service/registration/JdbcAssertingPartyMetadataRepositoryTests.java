bb1473ecea6287075278922192b0b24ba5d51f80
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-06-05 14:45:51 -0600

    Fix Checkstyle
    Issue gh-16012

--- saml2/saml2-service-provider/src/test/java/org/springframework/security/saml2/provider/service/registration/JdbcAssertingPartyMetadataRepositoryTests.java
+++ saml2/saml2-service-provider/src/test/java/org/springframework/security/saml2/provider/service/registration/JdbcAssertingPartyMetadataRepositoryTests.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.security.saml2.provider.service.registration;
 
 import java.io.IOException;
@@ -62,7 +78,7 @@ class JdbcAssertingPartyMetadataRepositoryTests {
 	private final Serializer<Object> serializer = new DefaultSerializer();
 
 	@BeforeEach
-	public void setUp() throws Exception {
+	void setUp() {
 		this.db = createDb();
 		this.jdbcOperations = new JdbcTemplate(this.db);
 		this.repository = new JdbcAssertingPartyMetadataRepository(this.jdbcOperations);
@@ -70,7 +86,7 @@ class JdbcAssertingPartyMetadataRepositoryTests {
 	}
 
 	@AfterEach
-	public void tearDown() {
+	void tearDown() {
 		this.db.shutdown();
 	}
 
