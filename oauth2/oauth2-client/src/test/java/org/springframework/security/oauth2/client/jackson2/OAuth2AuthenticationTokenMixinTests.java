b5478410e44f4851768bedfaa9ddd36efe873249
yybmion <yunyubin54@gmail.com>
2025-06-11 19:50:33 +0900

    Allow injecting the principal name into DefaultOAuth2User
    - Add username field to DefaultOAuth2User for direct name injection
    - Add Builder pattern with DefaultOAuth2User.withUsername(String) static factory method
    - Deprecate constructor that uses nameAttributeKey lookup in favor of Builder pattern
    - Update Jackson mixins to support username field serialization/deserialization
    
    This change prepares for SpEL support in the next commit.
    
    Signed-off-by: yybmion <yunyubin54@gmail.com>

--- oauth2/oauth2-client/src/test/java/org/springframework/security/oauth2/client/jackson2/OAuth2AuthenticationTokenMixinTests.java
+++ oauth2/oauth2-client/src/test/java/org/springframework/security/oauth2/client/jackson2/OAuth2AuthenticationTokenMixinTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2024 the original author or authors.
+ * Copyright 2002-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -194,7 +194,8 @@ public class OAuth2AuthenticationTokenMixinTests {
 				"      \"@class\": \"java.util.Collections$UnmodifiableMap\",\n" +
 				"      \"username\": \"user\"\n" +
 				"    },\n" +
-				"    \"nameAttributeKey\": \"username\"\n" +
+				"    \"nameAttributeKey\": \"username\",\n" +
+				"    \"username\": \"" + oauth2User.getName() + "\"\n" +
 				"  }";
 		// @formatter:on
 	}
