0179a811c76ca4b7c452b77127c60c5950485d0e
Joe Kuhel <4983938+jkuhel@users.noreply.github.com>
2025-06-10 23:32:15 -0400

    Update servlet test method docs to use include-code
    References gh-16226
    
    Signed-off-by: Joe Kuhel <4983938+jkuhel@users.noreply.github.com>

---
+++ docs/src/test/kotlin/org/springframework/security/kt/docs/servlet/test/testmethodwithanonymoususer/WithUserClassLevelAuthenticationTests.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.kt.docs.servlet.test.testmethodwithanonymoususer
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.security.test.context.support.WithAnonymousUser
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+
+// tag::snippet[]
+@ExtendWith(SpringExtension::class)
+@WithMockUser
+class WithUserClassLevelAuthenticationTests {
+
+	@Test
+	fun withMockUser1() {
+	}
+
+	@Test
+	fun withMockUser2() {
+	}
+
+	@Test
+	@WithAnonymousUser
+	@Throws(Exception::class)
+	fun anonymous() {
+		// override default to run as anonymous user
+	}
+
+}
+// end::snippet[]
---
+++ docs/src/test/kotlin/org/springframework/security/kt/docs/servlet/test/testmethodwithmockuser/WithMockUserTests.kt
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2002-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.kt.docs.servlet.test.testmethodwithmockuser
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.core.MessageService
+import org.springframework.security.kt.docs.servlet.test.testmethod.HelloMessageService
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@ExtendWith(SpringExtension::class)
+@ContextConfiguration
+class WithMockUserTests {
+
+    @Autowired
+    lateinit var messageService: MessageService
+
+    // tag::mock-user[]
+    @Test
+    @WithMockUser
+    fun getMessageWithMockUser() {
+        val message = messageService.message
+        Assertions.assertThat(message).contains("user")
+    }
+    // end::mock-user[]
+
+    // tag::custom-user[]
+    @Test
+    @WithMockUser("customUser")
+    fun getMessageWithMockUserCustomUsername() {
+        val message = messageService.message
+        Assertions.assertThat(message).contains("customUser")
+    }
+    // end::custom-user[]
+
+    // tag::custom-roles[]
+    @Test
+    @WithMockUser(username = "admin", roles = ["USER", "ADMIN"])
+    fun getMessageWithMockUserCustomRoles() {
+        val message = messageService.message
+        Assertions.assertThat(message)
+            .contains("admin")
+            .contains("ROLE_ADMIN")
+            .contains("ROLE_USER")
+    }
+    // end::custom-roles[]
+
+    // tag::custom-authorities[]
+    @Test
+    @WithMockUser(username = "admin", authorities = ["ADMIN", "USER"])
+    fun getMessageWithMockUserCustomAuthorities() {
+        val message = messageService.message
+        Assertions.assertThat(message)
+            .contains("admin")
+            .contains("ADMIN")
+            .contains("USER")
+            .doesNotContain("ROLE_")
+    }
+    // end::custom-authorities[]
+
+    @EnableMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
+    @Configuration
+    open class Config {
+        @Bean
+        open fun messageService(): MessageService {
+            return HelloMessageService()
+        }
+    }
+}
