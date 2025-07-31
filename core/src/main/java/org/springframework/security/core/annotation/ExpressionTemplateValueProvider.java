6c3ac8cc530edac44c3eaba6891e09889030938d
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-07-31 11:09:04 -0600

    Address Checkstyle
    Issue gh-17447
    
    Signed-off-by: Josh Cummings <3627351+jzheaux@users.noreply.github.com>

--- core/src/main/java/org/springframework/security/core/annotation/ExpressionTemplateValueProvider.java
+++ core/src/main/java/org/springframework/security/core/annotation/ExpressionTemplateValueProvider.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.security.core.annotation;
 
 /**
@@ -20,14 +36,13 @@ package org.springframework.security.core.annotation;
  * }
  * </pre>
  *
- * @since 6.5
  * @author Mike Heath
+ * @since 7.0
  */
 public interface ExpressionTemplateValueProvider {
 
 	/**
 	 * Returns the value to be used in an expression template.
-	 *
 	 * @return the value to be used in an expression template
 	 */
 	String getExpressionTemplateValue();
