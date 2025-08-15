a04b0260009ad1b927354bbafa893503de6c4205
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-08-15 14:51:42 -0600

    Disable Test
    Issue gh-17747
    PR gh-17724

--- config/src/test/java/org/springframework/security/config/annotation/web/configurers/NamespaceHttpInterceptUrlTests.java
+++ config/src/test/java/org/springframework/security/config/annotation/web/configurers/NamespaceHttpInterceptUrlTests.java
@@ -16,6 +16,7 @@
 
 package org.springframework.security.config.annotation.web.configurers;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -85,6 +86,7 @@ public class NamespaceHttpInterceptUrlTests {
 	}
 
 	@Test
+	@Disabled // see https://github.com/spring-projects/spring-security/issues/17747
 	public void requestWhenMappedByPostInterceptUrlThenBehaviorMatchesNamespace() throws Exception {
 		this.spring.register(HttpInterceptUrlConfig.class, BaseController.class).autowire();
 		MockHttpServletRequestBuilder getWithUser = get("/admin/post").with(authentication(user("ROLE_USER")));
