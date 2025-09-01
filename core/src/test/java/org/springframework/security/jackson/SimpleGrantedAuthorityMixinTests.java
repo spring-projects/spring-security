671c5942d904af197753093c814f8f70eb81f247
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2025-09-01 18:23:31 +0200

    Add Jackson 3 support
    This commit adds support for Jackson 3 which has the following
    major differences with the Jackson 2 one:
     - jackson subpackage instead of jackson2
     - Jackson type prefix instead of Jackson2
     - JsonMapper instead of ObjectMapper
     - For configuration, JsonMapper.Builder instead of ObjectMapper
       since the latter is now immutable
     - Remove custom Jackson 3 support for unmodifiable collections
     - AllowlistTypeResolverBuilder in new a public type in order
       to be used easily with the JsonMapper.Builder API
    
    Jackson 3 changes compared to Jackson 2 are documented on
    https://github.com/FasterXML/jackson-future-ideas/discussions/72.
    
    This commit does not cover webauthn which is a special case (uses
    jackson sub-package for Jackson 2 support) which will be handled in
    a distinct commit.
    
    See gh-17832
    Signed-off-by: Sébastien Deleuze <sdeleuze@users.noreply.github.com>

---
+++ core/src/test/java/org/springframework/security/jackson/SimpleGrantedAuthorityMixinTests.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.jackson;
+
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import tools.jackson.databind.exc.ValueInstantiationException;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+/**
+ * @author Jitendra Singh
+ * @since 4.2
+ */
+public class SimpleGrantedAuthorityMixinTests extends AbstractMixinTests {
+
+	// @formatter:off
+	public static final String AUTHORITY_JSON = "{\"@class\": \"org.springframework.security.core.authority.SimpleGrantedAuthority\", \"authority\": \"ROLE_USER\"}";
+	public static final String AUTHORITIES_ARRAYLIST_JSON = "[\"java.util.Collections$UnmodifiableRandomAccessList\", [" + AUTHORITY_JSON + "]]";
+	public static final String AUTHORITIES_SET_JSON = "[\"java.util.Collections$UnmodifiableSet\", [" + AUTHORITY_JSON + "]]";
+	public static final String NO_AUTHORITIES_ARRAYLIST_JSON = "[\"java.util.Collections$UnmodifiableRandomAccessList\", []]";
+	public static final String EMPTY_AUTHORITIES_ARRAYLIST_JSON = "[\"java.util.Collections$EmptyList\", []]";
+	public static final String NO_AUTHORITIES_SET_JSON = "[\"java.util.Collections$UnmodifiableSet\", []]";
+	// @formatter:on
+	@Test
+	public void serializeSimpleGrantedAuthorityTest() throws JsonProcessingException, JSONException {
+		SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
+		String serializeJson = this.mapper.writeValueAsString(authority);
+		JSONAssert.assertEquals(AUTHORITY_JSON, serializeJson, true);
+	}
+
+	@Test
+	public void deserializeGrantedAuthorityTest() throws IOException {
+		SimpleGrantedAuthority authority = this.mapper.readValue(AUTHORITY_JSON, SimpleGrantedAuthority.class);
+		assertThat(authority).isNotNull();
+		assertThat(authority.getAuthority()).isNotNull().isEqualTo("ROLE_USER");
+	}
+
+	@Test
+	public void deserializeGrantedAuthorityWithoutRoleTest() throws IOException {
+		String json = "{\"@class\": \"org.springframework.security.core.authority.SimpleGrantedAuthority\"}";
+		assertThatExceptionOfType(ValueInstantiationException.class)
+			.isThrownBy(() -> this.mapper.readValue(json, SimpleGrantedAuthority.class));
+	}
+
+}
