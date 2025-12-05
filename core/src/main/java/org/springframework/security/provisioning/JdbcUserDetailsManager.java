0a182f1f20ef7abe743f63d717a8ca60b54bd541
Andrey Litvitski <andrey1010102008@gmail.com>
2025-12-05 21:17:49 +0300

    Add `@Nullable` to changePassword parameters in UserDetailsManager
    Closes: gh-18257
    
    Signed-off-by: Andrey Litvitski <andrey1010102008@gmail.com>

--- core/src/main/java/org/springframework/security/provisioning/JdbcUserDetailsManager.java
+++ core/src/main/java/org/springframework/security/provisioning/JdbcUserDetailsManager.java
@@ -69,6 +69,7 @@ import org.springframework.util.Assert;
  *
  * @author Luke Taylor
  * @author Junhyeok Lee
+ * @author Andrey Litvitski
  * @since 2.0
  */
 public class JdbcUserDetailsManager extends JdbcDaoImpl
@@ -310,7 +311,8 @@ public class JdbcUserDetailsManager extends JdbcDaoImpl
 	}
 
 	@Override
-	public void changePassword(String oldPassword, String newPassword) throws AuthenticationException {
+	public void changePassword(@Nullable String oldPassword, @Nullable String newPassword)
+			throws AuthenticationException {
 		Authentication currentUser = this.securityContextHolderStrategy.getContext().getAuthentication();
 		if (currentUser == null) {
 			// This would indicate bad coding somewhere
@@ -337,7 +339,7 @@ public class JdbcUserDetailsManager extends JdbcDaoImpl
 		this.userCache.removeUserFromCache(username);
 	}
 
-	protected Authentication createNewAuthentication(Authentication currentAuth, String newPassword) {
+	protected Authentication createNewAuthentication(Authentication currentAuth, @Nullable String newPassword) {
 		UserDetails user = loadUserByUsername(currentAuth.getName());
 		UsernamePasswordAuthenticationToken newAuthentication = UsernamePasswordAuthenticationToken.authenticated(user,
 				null, user.getAuthorities());
