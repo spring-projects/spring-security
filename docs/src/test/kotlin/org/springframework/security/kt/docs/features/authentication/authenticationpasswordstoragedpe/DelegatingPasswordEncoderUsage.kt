8bafd94b1fd898b8afa7e62ebd9b44fade455022
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2026-01-27 20:10:24 +0700

    Add `compile-warnings-error`
    Closes gh-18424
    
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

--- docs/src/test/kotlin/org/springframework/security/kt/docs/features/authentication/authenticationpasswordstoragedpe/DelegatingPasswordEncoderUsage.kt
+++ docs/src/test/kotlin/org/springframework/security/kt/docs/features/authentication/authenticationpasswordstoragedpe/DelegatingPasswordEncoderUsage.kt
@@ -4,10 +4,8 @@ import org.springframework.security.crypto.argon2.Argon2PasswordEncoder
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder
-import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
-import org.springframework.security.crypto.password.StandardPasswordEncoder
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder
 
 class DelegatingPasswordEncoderUsage {
@@ -18,19 +16,20 @@ class DelegatingPasswordEncoderUsage {
         return passwordEncoder
     }
 
+    @Suppress("DEPRECATION")
     fun customDelegatingPasswordEncoder(): PasswordEncoder {
         // tag::createCustomPasswordEncoder[]
         val idForEncode = "bcrypt"
         val encoders: MutableMap<String, PasswordEncoder> = mutableMapOf()
         encoders[idForEncode] = BCryptPasswordEncoder()
-        encoders["noop"] = NoOpPasswordEncoder.getInstance()
+        encoders["noop"] = org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance()
         encoders["pbkdf2"] = Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_5()
         encoders["pbkdf2@SpringSecurity_v5_8"] = Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8()
         encoders["scrypt"] = SCryptPasswordEncoder.defaultsForSpringSecurity_v4_1()
         encoders["scrypt@SpringSecurity_v5_8"] = SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8()
         encoders["argon2"] = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_2()
         encoders["argon2@SpringSecurity_v5_8"] = Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8()
-        encoders["sha256"] = StandardPasswordEncoder()
+        encoders["sha256"] = org.springframework.security.crypto.password.StandardPasswordEncoder()
 
         val passwordEncoder: PasswordEncoder = DelegatingPasswordEncoder(idForEncode, encoders)
         // end::createCustomPasswordEncoder[]
