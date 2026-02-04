7da34ec3ff7cd967afdcf7f9e20d3e9d2959babd
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2026-02-04 20:15:06 +0700

    Update Remember-Me example
    Closes gh-18639
    
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

---
+++ docs/src/test/java/org/springframework/security/docs/servlet/authentication/tokenbasedremembermeservices/DefaultAlgorithmRememberMeServicesConfiguration.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2026-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.docs.servlet.authentication.tokenbasedremembermeservices;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.RememberMeAuthenticationProvider;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.RememberMeAuthenticationFilter;
+import org.springframework.security.web.authentication.rememberme.TokenBasedRememberMeServices;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * Demonstrates default algorithm for remember me configuration.
+ *
+ * @author Ngoc Nhan
+ */
+@EnableWebMvc
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class DefaultAlgorithmRememberMeServicesConfiguration {
+
+	// tag::snippet[]
+	@Bean
+	RememberMeServices rememberMeServices(UserDetailsService userDetailsService) {
+		return new TokenBasedRememberMeServices("myKey", userDetailsService);
+	}
+
+	@Bean
+	RememberMeAuthenticationFilter rememberMeFilter(AuthenticationManager authenticationManager,
+			TokenBasedRememberMeServices rememberMeServices) {
+		return new RememberMeAuthenticationFilter(authenticationManager, rememberMeServices);
+	}
+
+	@Bean
+	RememberMeAuthenticationProvider rememberMeAuthenticationProvider() {
+		return new RememberMeAuthenticationProvider("myKey");
+	}
+	// end::snippet[]
+
+}
