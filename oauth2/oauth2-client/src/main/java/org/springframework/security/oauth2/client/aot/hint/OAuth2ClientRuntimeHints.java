7f5e1c728e69bacfd5683f929903b4b9bf7b8a85
Josh Long <54473+joshlong@users.noreply.github.com>
2026-02-18 16:09:21 -0600

    aot refinements in saml, acl, core, reactive, etc.

--- oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/aot/hint/OAuth2ClientRuntimeHints.java
+++ oauth2/oauth2-client/src/main/java/org/springframework/security/oauth2/client/aot/hint/OAuth2ClientRuntimeHints.java
@@ -16,8 +16,13 @@
 
 package org.springframework.security.oauth2.client.aot.hint;
 
+import org.jspecify.annotations.Nullable;
+
+import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
+import org.springframework.util.ClassUtils;
 
 /**
  * {@link RuntimeHintsRegistrar} for OAuth2 Client
@@ -27,15 +32,47 @@ import org.springframework.aot.hint.RuntimeHintsRegistrar;
  */
 class OAuth2ClientRuntimeHints implements RuntimeHintsRegistrar {
 
+	private static final boolean r2dbcPresent;
+
+	static {
+		ClassLoader classLoader = ClassUtils.getDefaultClassLoader();
+		r2dbcPresent = ClassUtils.isPresent("io.r2dbc.spi.Row", classLoader)
+				&& ClassUtils.isPresent("org.springframework.r2dbc.core.DatabaseClient", classLoader);
+	}
+
 	@Override
-	public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+	public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
 		registerOAuth2ClientSchemaFilesHints(hints);
+		if (r2dbcPresent) {
+			registerR2dbcHints(hints);
+		}
 	}
 
 	private void registerOAuth2ClientSchemaFilesHints(RuntimeHints hints) {
 		hints.resources()
 			.registerPattern("org/springframework/security/oauth2/client/oauth2-client-schema.sql")
 			.registerPattern("org/springframework/security/oauth2/client/oauth2-client-schema-postgres.sql");
+	}
+
+	private void registerR2dbcHints(RuntimeHints hints) {
+		// Register R2DBC OAuth2 client service types
+		hints.reflection()
+			.registerType(
+					TypeReference
+						.of("org.springframework.security.oauth2.client.R2dbcReactiveOAuth2AuthorizedClientService"),
+					(builder) -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+							MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.ACCESS_DECLARED_FIELDS));
+
+		// Register OAuth2 client types that may be serialized in R2DBC scenarios
+		hints.reflection()
+			.registerTypes(
+					java.util.List.of(
+							TypeReference
+								.of("org.springframework.security.oauth2.client.OAuth2AuthorizedClient"),
+							TypeReference
+								.of("org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken")),
+					(builder) -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS,
+							MemberCategory.INVOKE_DECLARED_METHODS, MemberCategory.ACCESS_DECLARED_FIELDS));
 	}
 
 }
