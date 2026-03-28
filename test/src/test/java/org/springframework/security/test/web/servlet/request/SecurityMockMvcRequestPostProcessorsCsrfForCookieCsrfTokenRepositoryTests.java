df146d7856687d18ef21b7e9ce989b4eeb08428c
Andre Kappes <16115678+indy5858@users.noreply.github.com>
2026-03-28 21:12:25 +0100

    use existing token repository if present
    do not overwrite repository set in production code;
    instead only wrap existing repository
    
    Closes gh-17082
    
    Signed-off-by: Andre Kappes <16115678+indy5858@users.noreply.github.com>

---
+++ test/src/test/java/org/springframework/security/test/web/servlet/request/SecurityMockMvcRequestPostProcessorsCsrfForCookieCsrfTokenRepositoryTests.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.test.web.servlet.request;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class SecurityMockMvcRequestPostProcessorsCsrfForCookieCsrfTokenRepositoryTests {
+
+	@Autowired
+	WebApplicationContext wac;
+
+	@Test
+	void withCsrfDoesNotResetTokenRepositorySetInProductionCode_shouldReturnCsrfCookie() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders
+				.webAppContextSetup(wac)
+				.apply(springSecurity())
+				.build();
+		mockMvc.perform(post("/").with(csrf()))
+				.andExpect(status().isOk());
+
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(cookie().httpOnly("XSRF-TOKEN", false));
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	public static class SpaConfig {
+
+		@Bean
+		public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+			http.csrf(CsrfConfigurer::spa);
+			return http.build();
+		}
+
+		@RestController
+		static class TheController {
+
+			@RequestMapping("/")
+			String index() {
+				return "Hi";
+			}
+
+		}
+	}
+}
