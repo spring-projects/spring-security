939d1b8c5bdc21e43da14ae4a6966dddff731b5d
sunghyun1999 <32184099@dankook.ac.kr>
2026-07-29 09:53:04 +0900

    Update cryptography.adoc to use include-code
    Closes gh-16226
    
    Signed-off-by: sunghyun1999 <32184099@dankook.ac.kr>

---
+++ docs/src/test/java/org/springframework/security/docs/features/integrations/cryptography/CryptoPasswordEncodingTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.docs.features.integrations.cryptography;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CryptoPasswordEncodingTests {
+
+	// tag::bcrypt[]
+	@Test
+	void bcryptPasswordEncoder() {
+		// Create an encoder with strength 16
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(16);
+		String result = encoder.encode("myPassword");
+		assertTrue(encoder.matches("myPassword", result));
+	}
+	// end::bcrypt[]
+
+	// tag::pbkdf2[]
+	@Test
+	void pbkdf2PasswordEncoder() {
+		// Create an encoder with all the defaults
+		Pbkdf2PasswordEncoder encoder = Pbkdf2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+		String result = encoder.encode("myPassword");
+		assertTrue(encoder.matches("myPassword", result));
+	}
+	// end::pbkdf2[]
+
+}
