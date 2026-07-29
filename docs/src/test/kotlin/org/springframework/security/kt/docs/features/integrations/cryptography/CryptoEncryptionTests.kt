12c5e28538e9e684c5672fefa190d5147221bd1d
sunghyun1999 <32184099@dankook.ac.kr>
2026-07-29 09:53:04 +0900

    Update cryptography.adoc to use include-code
    Closes gh-16226
    
    Signed-off-by: sunghyun1999 <32184099@dankook.ac.kr>

---
+++ docs/src/test/kotlin/org/springframework/security/kt/docs/features/integrations/cryptography/CryptoEncryptionTests.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.kt.docs.features.integrations.cryptography
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.security.crypto.encrypt.Encryptors
+import org.springframework.security.crypto.keygen.KeyGenerators
+
+class CryptoEncryptionTests {
+
+    // tag::bytes-encryptor[]
+    @Test
+    fun bytesEncryptor() {
+        val salt = KeyGenerators.string().generateKey()
+        Encryptors.stronger("password", salt)
+    }
+    // end::bytes-encryptor[]
+
+    // tag::generate-salt[]
+    @Test
+    fun generateSalt() {
+        val salt = KeyGenerators.string().generateKey() // generates a random 8-byte salt that is then hex-encoded
+        assertThat(salt).isNotEmpty()
+    }
+    // end::generate-salt[]
+
+    // tag::text-encryptor[]
+    @Test
+    fun textEncryptor() {
+        val salt = KeyGenerators.string().generateKey()
+        Encryptors.text("password", salt)
+    }
+    // end::text-encryptor[]
+
+}
