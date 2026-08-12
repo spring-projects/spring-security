783fe81d3b573a9169af75eb5661cb24f0c3bda6
Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>
2026-08-12 20:58:11 +0700

    Include code example for reactive onetimetoken
    Signed-off-by: Tran Ngoc Nhan <ngocnhan.tran1996@gmail.com>

---
+++ docs/src/test/kotlin/org/springframework/security/kt/docs/reactive/authentication/customizegeneratetokenrequest/SecurityConfig.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2004-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.security.kt.docs.reactive.authentication.customizegeneratetokenrequest
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.web.server.authentication.ott.DefaultServerGenerateOneTimeTokenRequestResolver
+import org.springframework.security.web.server.authentication.ott.ServerGenerateOneTimeTokenRequestResolver
+import java.time.Duration
+
+open class SecurityConfig {
+
+    // tag::config[]
+    @Bean
+    fun generateOneTimeTokenRequestResolver() : ServerGenerateOneTimeTokenRequestResolver {
+        return DefaultServerGenerateOneTimeTokenRequestResolver().apply {
+            this.setExpiresIn(Duration.ofMinutes(10))
+        }
+    }
+    // end::config[]
+
+}
